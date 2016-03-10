95db97ef3a1222333514d9362efd1b776e052a6c
liamdawe <liamdawe@gmail.com>
2016-03-10 11:26:25 +0000

    email us link in articles block was hard-coded to pretty url, shtop dat

--- blocks/block_article_categorys.php
+++ blocks/block_article_categorys.php
@@ -45,3 +45,14 @@ while ($top_articles = $db->fetch())
 }
 
 $templating->set('top_articles', $hot_articles);
+
+if ($config['pretty_urls'] == 1)
+{
+	$email_link = "/email-us/";
+}
+else
+{
+	$email_link = core::config('website_url') . 'index.php?module=email_us';
+}
+
+$templating->set('email_us_link', $email_us);

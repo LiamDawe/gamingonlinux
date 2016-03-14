92d7ad0b2daf8696425aec8c185616dacd470c9b
liamdawe <liamdawe@gmail.com>
2016-03-14 20:23:54 +0000

    fix register activation links

--- modules/register.php
+++ modules/register.php
@@ -166,16 +166,16 @@ if (core::config('allow_registrations') == 1)
 							<title>Welcome email for GamingOnLinux.com, activation needed!</title>
 							</head>
 							<body>
-							<img src="'.core::config('website_url').core::config('path').'templates/default/images/icon.png" alt="Gaming On Linux">
+							<img src="'.core::config('website_url').'templates/default/images/icon.png" alt="Gaming On Linux">
 							<br />
 							<p>Hello '.$_POST['username'].',</p>
-							<p>Thanks for registering on <a href="'.core::config('website_url').core::config('path').'" target="_blank">'.core::config('website_url').core::config('path').'</a>, The best source for linux games and news.</p>
-							<p><strong><a href="'.core::config('website_url').core::config('path').'index.php?module=activate_user&user_id='.$last_id.'&code='.$code.'">You need to activate your account before you can post! Click here to activate!</a></strong></p>
-							<p>If you&#39;re new, consider saying hello in the <a href="'.core::config('website_url').core::config('path').'forum/" target="_blank">forum</a>.</p>
+							<p>Thanks for registering on <a href="'.core::config('website_url').'" target="_blank">'.core::config('website_url').'</a>, The best source for linux games and news.</p>
+							<p><strong><a href="'.core::config('website_url').'index.php?module=activate_user&user_id='.$last_id.'&code='.$code.'">You need to activate your account before you can post! Click here to activate!</a></strong></p>
+							<p>If you&#39;re new, consider saying hello in the <a href="'.core::config('website_url').'forum/" target="_blank">forum</a>.</p>
 							<br style="clear:both">
 							<div>
 							<hr>
-							<p>If you haven&#39;t registered at <a href="'.core::config('website_url').core::config('path').'" target=\"_blank\">'.core::config('website_url').core::config('path').'</a>, Forward this mail to <a href=\"mailto:'.core::config('contact_email').'" target="_blank">'.core::config('contact_email').'</a> with some info about what you want us to do about it.</p>
+							<p>If you haven&#39;t registered at <a href="'.core::config('website_url').'" target=\"_blank\">'.core::config('website_url').'</a>, Forward this mail to <a href=\"mailto:'.core::config('contact_email').'" target="_blank">'.core::config('contact_email').'</a> with some info about what you want us to do about it.</p>
 							<p>Please, Don&#39;t reply to this automated message, We do not read any emails recieved on this email address.</p>
 							</div>
 							</body>

9af3cff7a6fb8c32ef3fa02a82958c4d3c043ddc
liamdawe <liamdawe@gmail.com>
2016-03-18 22:03:08 +0000

    don't try to post to telegram if the key isn't set

--- includes/telegram_poster.php
+++ includes/telegram_poster.php
@@ -1,11 +1,14 @@
 <?php
 function telegram($link)
 {
-    $botToken = core::config('telegram_bot_key');
-    $chat_id = "@linuxgaming";
-    $message = $link;
-    $bot_url    = "https://api.telegram.org/bot$botToken/";
-    $url = $bot_url."sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
-    file_get_contents($url);
+    if (!empty(core::config('telegram_bot_key')))
+    {
+        $botToken = core::config('telegram_bot_key');
+        $chat_id = "@linuxgaming";
+        $message = $link;
+        $bot_url    = "https://api.telegram.org/bot$botToken/";
+        $url = $bot_url."sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
+        file_get_contents($url);
+    }
 }
 ?>

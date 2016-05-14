8f6f4a3fdcbf1ecee0b57512e1a4428ede95c0d0
liamdawe <liamdawe@gmail.com>
2016-05-14 17:33:43 +0100

    move random picker to js to prevent refresh and form submits

--- random_picker.php
+++ random_picker.php
@@ -1,34 +1,44 @@
-<?php
-$options = '';
-for ($i = 1; $i <= 10; $i++)
-{
-  $options .= '<option value="'.$i.'">'.$i.'</a>';
-}
-?>
 <strong>GamingOnLinux random username picker for key giveaways:</strong><br />
 <br />
-<form method="post" action="random_picker.php">
-  Names, put each name on a new line: <br />
-  <textarea name="names" rows="15" cols="100"><?php echo $_POST['names']; ?></textarea><br />
-  <select name="amount"><?php echo $options; ?></select>
-  <button type="submit">Pick</button>
-</form>
-<?php if (isset($_POST['names']))
+Names, put each name on a new line: <br />
+<textarea id="names" rows="15" cols="100"></textarea><br />
+<select id="amount"></select>
+<button id="pick" onclick="random_click();">Pick</button><br />
+<strong>Results</strong><br />
+<div id="result"></div>
+<script>
+var select = document.getElementById("amount");
+
+for(var i = 1; i < 11; i++)
 {
-  $lines = explode("\n", $_POST['names']);
-  //print_r($lines);
-  $rand_keys = array_rand($lines, $_POST['amount']);
-  //print_r($rand_keys);
-  if ($_POST['amount'] == 1)
-  {
-    echo $lines[$rand_keys];
-  }
-  else
-  {
-    foreach ($rand_keys as $key)
-    {
-      echo $lines[$key] . '<br />';
-    }
-  }
+    var el = document.createElement("option");
+    el.textContent = i;
+    el.value = i;
+    select.appendChild(el);
 }
-?>
+
+function getRandom(arr, how_many)
+{
+    var result = new Array(how_many),len = arr.length,taken = new Array(len);
+    if (how_many > len)
+    {
+        throw new RangeError("getRandom: more elements taken than available");
+    }
+    while (how_many--)
+    {
+        var x = Math.floor(Math.random() * len);
+        result[how_many] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len;
+    }
+    return result;
+}
+
+function random_click()
+{
+  var names_array = document.getElementById('names').value.split('\n');
+
+  var random = getRandom(names_array, document.getElementById('amount').value)
+
+  document.getElementById('result').innerHTML = random;
+}
+</script>

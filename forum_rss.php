8d70e0f52c70382775aef5090cb79380e86f0420
liamdawe <liamdawe@gmail.com>
2016-06-10 13:43:12 +0100

    fix the long broken forum rss

--- forum_rss.php
+++ forum_rss.php
@@ -26,46 +26,46 @@ if ($config['articles_rss'] == 1)
 	if (isset($_GET['limit']) && is_numeric($_GET['limit']))
 	{
 		$limit = $_GET['limit'];
-		
+
 		if ($limit > 50)
 		{
 			$limit = 50;
 		}
 	}
-		
+
 	$now = date("D, d M Y H:i:s O");
-		
+
 	$output = "<?xml version=\"1.0\"?>
 	<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">
 		<channel>
 			<title>Latest forum posts</title>
-			<link>http://www.gamingonlinux.com/articles_rss.php</link>
-			<atom:link href=\"{$config['path']}/forum_rss.php\" rel=\"self\" type=\"application/rss+xml\" />
+			<link>https://www.gamingonlinux.com/forum_rss.php</link>
+			<atom:link href=\"". core::config('website_url') . "forum_rss.php\" rel=\"self\" type=\"application/rss+xml\" />
 			<language>en-us</language>
 			<description></description>
 			<pubDate>$now</pubDate>
 			<lastBuildDate>$now</lastBuildDate>";
-	
+
 	$db->sqlquery("SELECT `topic_id`, `topic_title`, `last_post_date` FROM `forum_topics` ORDER BY `last_post_date` DESC limit ?", array($limit));
 
 	while ($line = $db->fetch())
 	{
 		// make date human readable
 		$date = date("D, d M Y H:i:s O", $line['last_post_date']);
-			
+
 		$output .= "
 			<item>
 				<title>{$line['topic_title']}</title>
-				<link>{$config['path']}/forum/topic/{$line['topic_id']}/</link>
+				<link>". core::config('website_url') . "forum/topic/{$line['topic_id']}/</link>
 				<pubDate>{$date}</pubDate>
-				<guid>{$config['path']}/forum/topic/{$line['topic_id']}/</guid>
+				<guid>". core::config('website_url') . "forum/topic/{$line['topic_id']}/</guid>
 			</item>";
 	}
-		
+
 	$output .= "
 		</channel>
 	</rss>";
-		
+
 	header("Content-Type: application/rss+xml");
 	echo $output;
 }

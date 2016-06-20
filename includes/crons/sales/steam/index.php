e4de6350f9d2d4cdec930dfd31e2a50f099635eb
Liam Dawe <liamdawe@gmail.com>
2016-06-20 11:20:02 +0100

    add in currency loop for steam sales importing, not finished, need to fix dollars prices

--- includes/crons/sales/steam/index.php
+++ includes/crons/sales/steam/index.php
@@ -1,10 +1,10 @@
 <?php
 // TODO
-// ?cc=us for dollars, lets get pounds working first eh boyos!
+// need to add-in checking if the price is at zero, to re-check it or something?
 
 // With thanks to http://steamsales.rhekua.com/, they explain directly on their website how they do it
 
-echo "Steam Store importer started on " .date('d-m-Y H:m:s'). "\n";
+echo "Steam Store importer started on " .date('d-m-Y H:m:s'). "<br />\n";
 
 // http://simplehtmldom.sourceforge.net/
 include('simple_html_dom.php');
@@ -19,95 +19,118 @@ $db = new mysql($database_host, $database_username, $database_password, $databas
 include(path . 'includes/class_core.php');
 $core = new core();
 
-$page = 1;
 $stop = 0;
 $new_games = array();
+$currencies = array("fr", "us", "gb");
 
-$url = "http://store.steampowered.com/search/?specials=1&os=linux&cc=GB&page=";
-
-do
+foreach ($currencies as $currency)
 {
-  $html = file_get_html($url . $page);
+  $page = 1;
+  $url = "http://store.steampowered.com/search/?specials=1&os=linux&cc=$currency&page=";
 
-  $get_sales = $html->find('a.search_result_row');
+  do
+  {
+    $html = file_get_html($url . $page);
 
-  if (empty($get_sales))
-  {
-    $stop = 1;
-  }
-  else
-  {
-    foreach($get_sales as $element)
+    $get_sales = $html->find('a.search_result_row');
+
+    if (empty($get_sales))
     {
-      $link = $element->href;
-      echo $element->href . '<br />';
-
-      foreach($element->find('span.title') as $span)
-      {
-        $title = trim($span->plaintext);
-        echo $span->plaintext . '<br />';
-      }
-
-      foreach ($element->find('div.discounted') as $price)
-      {
-        $prices = trim($price->plaintext);
-        $prices = explode(' ', $prices);
-
-        // wtf?
-        $original_price = substr($prices[0], 2);
-        $price_now = substr($prices[1], 4);
-
-        echo 'Original price: ' . $original_price . '<br />';
-        echo 'Price now: ' . $price_now  . '<br />';
-
-        // ADD IT TO THE GAMES DATABASE
-        $db->sqlquery("SELECT `local_id`, `name` FROM `game_list` WHERE `name` = ?", array($title));
-        if ($db->num_rows() == 0)
-        {
-          $db->sqlquery("INSERT INTO `game_list` SET `name` = ?", array($title));
-
-          // okay now get the info for it to use later
-          $db->sqlquery("SELECT `local_id`, `name` FROM `game_list` WHERE `name` = ?", array($title));
-          $game_list = $db->fetch();
-        }
-        // it exists, so get the info for it to use later
-        else
-        {
-          $game_list = $db->fetch();
-        }
-        $new_games[] = $game_list['local_id'];
-
-        // need to check if we already have it to insert it
-        // search if that title exists
-        $db->sqlquery("SELECT `id` FROM `game_sales` WHERE `list_id` = ? AND `provider_id` = 1", array($game_list['local_id']));
-
-        // if it does exist, make sure it's not from Steam already
-        if ($db->num_rows() == 0)
-        {
-          $db->sqlquery("INSERT INTO `game_sales` SET `list_id` = ?, `website` = ?, `date` = ?, `accepted` = 1, `provider_id` = 1, `pounds` = ?, `pounds_original` = ?, `steam` = 1", array($game_list['local_id'], $link, core::$date, $price_now, $original_price));
-
-          $sale_id = $db->grab_id();
-
-          echo "\tAdded this game to the sales DB with id: " . $sale_id . "<br />.\n";
-
-          $games .= $title . '<br />';
-
-          $email = 1;
-        }
-
-        // if we already have it, just update it
-        else
-        {
-          $current_sale = $db->fetch();
-          $db->sqlquery("UPDATE `game_sales` SET `pounds` = ?, `pounds_original` = ? WHERE `provider_id` = 1 AND id = ?", array($price_now, $original_price, $current_sale['id']));
-
-          echo "Updated {$title} with the latest information<br />";
-        }
-      }
+      $stop = 1;
     }
-    $page++;
-  }
-} while ($stop == 0);
+    else
+    {
+      foreach($get_sales as $element)
+      {
+        $link = $element->href;
+        echo  'Link: ' . $link . '<br />';
+
+        foreach($element->find('span.title') as $span)
+        {
+          $title = trim($span->plaintext);
+          echo 'Title: ' . $title . '<br />';
+        }
+
+        foreach ($element->find('div.discounted') as $price)
+        {
+          $prices = trim($price->plaintext);
+          $prices = explode(' ', $prices);
+
+          // wtf?
+          if ($currency == 'gb')
+          {
+            $original_price = substr($prices[0], 2);
+            $price_now = substr($prices[1], 4);
+            $sql_currency = 'pounds';
+          }
+          else if ($currency == 'fr')
+          {
+            $original_price = str_replace(',', '.', substr($prices[0], 0, -3));
+            $price_now = str_replace(',', '.', substr($prices[1], 0, -3));
+            $sql_currency = 'euros';
+          }
+          else if ($currency == 'us')
+          {
+            $original_price = substr($prices[0], 1);
+            $price_now = substr($prices[1], 3);
+            $sql_currency = 'dollars';
+          }
+
+          echo 'Original price: ' . $original_price . '<br />';
+          echo 'Price now: ' . $price_now  . '<br />';
+
+          // ADD IT TO THE GAMES DATABASE
+          $db->sqlquery("SELECT `local_id`, `name` FROM `game_list` WHERE `name` = ?", array($title));
+          if ($db->num_rows() == 0)
+          {
+            $db->sqlquery("INSERT INTO `game_list` SET `name` = ?", array($title));
+
+            // okay now get the info for it to use later
+            $db->sqlquery("SELECT `local_id`, `name` FROM `game_list` WHERE `name` = ?", array($title));
+            $game_list = $db->fetch();
+          }
+          // it exists, so get the info for it to use later
+          else
+          {
+            $game_list = $db->fetch();
+          }
+          $new_games[] = $game_list['local_id'];
+
+          echo 'Local ID: ' .  $game_list['local_id'] . '<br />';
+
+          // need to check if we already have it to insert it
+          // search if that title exists
+          $db->sqlquery("SELECT `id` FROM `game_sales` WHERE `list_id` = ? AND `provider_id` = 1", array($game_list['local_id']));
+
+          // if it does exist, make sure it's not from Steam already
+          if ($db->num_rows() == 0)
+          {
+            $db->sqlquery("INSERT INTO `game_sales` SET `list_id` = ?, `website` = ?, `date` = ?, `accepted` = 1, `provider_id` = 1, `$sql_currency` = ?, `{$sql_currency}_original` = ?, `steam` = 1", array($game_list['local_id'], $link, core::$date, $price_now, $original_price));
+
+            $sale_id = $db->grab_id();
+
+            echo "\tAdded this game to the sales DB with id: " . $sale_id . "<br />\n";
+
+            $games .= $title . '<br />';
+
+            $email = 1;
+          }
+
+          // if we already have it, just update it
+          else
+          {
+            $current_sale = $db->fetch();
+            $db->sqlquery("UPDATE `game_sales` SET `pounds` = ?, `pounds_original` = ? WHERE `provider_id` = 1 AND id = ?", array($price_now, $original_price, $current_sale['id']));
+
+            echo "Updated {$title} with the latest information<br />";
+          }
+        }
+        echo '<br />';
+      }
+      $page++;
+    }
+  } while ($stop == 0);
+}
 
 $db->sqlquery("SELECT s.`list_id`, l.`name` FROM `game_sales` s INNER JOIN `game_list` l ON l.local_id = s.list_id WHERE s.`provider_id` = 1");
 $in_db = $db->fetch_all_rows();

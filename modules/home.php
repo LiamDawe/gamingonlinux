7275f6206dc30b364234e518e90727b4c6e90218
Liam Dawe <liamdawe@gmail.com>
2016-08-15 19:46:57 +0100

    adjust article tags to be names not numbers for SEO

--- modules/home.php
+++ modules/home.php
@@ -105,7 +105,7 @@ if (!isset($_GET['view']))
 
 	$db->sqlquery("SELECT count(article_id) as count FROM `articles` WHERE `show_in_menu` = 1");
 	$featured_ctotal = $db->fetch();
-	
+
 	// latest news
 	$db->sqlquery("SELECT a.article_id, a.author_id, a.guest_username, a.title, a.tagline, a.text, a.date, a.comment_count, a.article_top_image, a.article_top_image_filename, a.tagline_image, a.show_in_menu, a.slug, u.username FROM `articles` a LEFT JOIN `users` u on a.author_id = u.user_id WHERE a.active = 1 ORDER BY a.`date` DESC LIMIT ?, {$_SESSION['articles-per-page']}", array($core->start));
 	$articles_get = $db->fetch_all_rows();
@@ -196,12 +196,12 @@ if (!isset($_GET['view']))
 		{
 			if ($get_categories['category_id'] == 60)
 			{
-				$categories_list .= " <li class=\"ea\"><a href=\"/articles/category/{$get_categories['category_id']}\">{$get_categories['category_name']}</a></li> ";
+				$categories_list .= " <li class=\"ea\"><a href=\"/articles/category/{$get_categories['category_name']}\">{$get_categories['category_name']}</a></li> ";
 			}
 
 			else
 			{
-				$categories_list .= " <li><a href=\"/articles/category/{$get_categories['category_id']}\">{$get_categories['category_name']}</a></li> ";
+				$categories_list .= " <li><a href=\"/articles/category/{$get_categories['category_name']}\">{$get_categories['category_name']}</a></li> ";
 			}
 		}
 

ee27efd237a4738c63cc395c8870ae93aaabb1ed
liamdawe <liamdawe@gmail.com>
2016-10-24 20:03:16 +0100

    fix undefined error

--- admin_modules/articles.php
+++ admin_modules/articles.php
@@ -30,7 +30,7 @@ if (isset($_GET['view']))
 
 			$article = $db->fetch();
 
-			if ($article['locked'] == 1 && $_GET['unlock'] == 1 && $article['locked_by'] == $_SESSION['user_id'])
+			if (isset($_GET['unlock']) && $article['locked'] == 1 && $_GET['unlock'] == 1 && $article['locked_by'] == $_SESSION['user_id'])
 			{
 				$db->sqlquery("UPDATE `articles` SET `locked` = 0, `locked_by` = 0, `locked_date` = 0 WHERE `article_id` = ?", array($article['article_id']));
 

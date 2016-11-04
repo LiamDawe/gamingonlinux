bd8975c747c6a75f70ef3f9e6b83665dd93aa620
liamdawe <liamdawe@gmail.com>
2016-11-04 16:14:57 +0000

    add avatars to who likes ajax

--- includes/ajax/who_likes.php
+++ includes/ajax/who_likes.php
@@ -13,6 +13,9 @@ include('../class_template.php');
 
 $templating = new template('default');
 
+include('../class_user.php');
+$user = new user();
+
 if(isset($_GET['comment_id']) || isset($_GET['article_id']))
 {
   if (isset($_GET['comment_id']))
@@ -25,7 +28,7 @@ if(isset($_GET['comment_id']) || isset($_GET['article_id']))
     $table = 'article_likes';
     $field = 'article_id';
   }
-  $db->sqlquery("SELECT u.`username`, u.`user_id`, l.like_id FROM `users` u INNER JOIN `$table` l ON u.`user_id` = l.`user_id` WHERE l.`$field` = ? ORDER BY u.`username` ASC LIMIT 50", array($_GET[$field]));
+  $db->sqlquery("SELECT u.`username`, u.`user_id`, u.`avatar_gravatar`, u.`gravatar_email`, u.`avatar_gallery`, u.`avatar`, u.`avatar_gravatar`, u.`avatar_uploaded`, l.like_id FROM `users` u INNER JOIN `$table` l ON u.`user_id` = l.`user_id` WHERE l.`$field` = ? ORDER BY u.`username` ASC LIMIT 50", array($_GET[$field]));
   if ($db->num_rows() == 0)
 	{
 		$core->message('That does not exist!');
@@ -47,9 +50,12 @@ if(isset($_GET['comment_id']) || isset($_GET['article_id']))
         $profile_link = '/index.php?module=profile&user_id=' . $grab_users['user_id'];
       }
 
+      $avatar = $user->sort_avatar($grab_users);
+
       $templating->block('user_row');
       $templating->set('username', $grab_users['username']);
       $templating->set('profile_link', $profile_link);
+      $templating->set('avatar', $avatar);
     }
 
     $templating->block('end');

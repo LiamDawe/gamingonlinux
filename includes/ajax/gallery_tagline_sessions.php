5d7b59bad38a7e588fcf6b8d1fd10e6ef3f2d85b
liamdawe <liamdawe@gmail.com>
2016-11-04 14:27:45 +0000

    new file to handle session changeover for gallery tagline images

---
+++ includes/ajax/gallery_tagline_sessions.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+include('../config.php');
+
+include('../class_mysql.php');
+$db = new mysql($database_host, $database_username, $database_password, $database_db);
+
+include('../class_core.php');
+$core = new core();
+
+if (isset($_POST['id']))
+{
+  // see if there's a previous image uploaded, this is set by ajax_tagline_upload.php
+  if (isset($_SESSION['uploads_tagline']))
+  {
+    // remove the image
+    unlink(core::config('path') . "uploads/articles/tagline_images/temp/" . $_SESSION['uploads_tagline']['image_name']);
+    unlink(core::config('path') . "uploads/articles/tagline_images/temp/thumbnails/" . $_SESSION['uploads_tagline']['image_name']);
+
+    // destroy the session for any other images
+    unset($_SESSION['uploads_tagline']);
+  }
+
+  // set the id of the image in the session to detect and use when finishing the article
+  $_SESSION['gallery_tagline'] = $_POST['id'];
+
+  echo json_encode(array("result" => 'done'));
+  return;
+}
+echo json_encode(array("result" => 'error'));
+return;

2e69cb6ad83c8daa1f33b3d8f3ffcd61c3bd791c
liamdawe <liamdawe@gmail.com>
2016-11-07 23:54:22 +0000

    KISS livestream fetcher

--- blocks/block_livestreams.php
+++ blocks/block_livestreams.php
@@ -4,7 +4,7 @@ $templating->merge('blocks/block_livestreams');
 $templating->block('list');
 
 // count how many there is due this month and today
-$count_query = "SELECT `title`, `date` FROM `livestreams` WHERE YEAR(date) = YEAR(CURDATE()) AND MONTH(date) = MONTH(CURDATE()) AND DAY(date) > DAY(CURDATE()) AND HOUR(date) >= HOUR(CURDATE()) ORDER BY `date` ASC LIMIT 1";
+$count_query = "SELECT `title`, `date` FROM `livestreams` ORDER BY `date` ASC LIMIT 1";
 $db->sqlquery($count_query);
 if ($db->num_rows() == 1)
 {

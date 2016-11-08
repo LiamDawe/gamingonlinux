ad3b8b22d2201d4d0189fa465abd668b511769b6
liamdawe <liamdawe@gmail.com>
2016-11-08 09:54:55 +0000

    only put UTC after a date not countdown

--- blocks/block_livestreams.php
+++ blocks/block_livestreams.php
@@ -11,8 +11,8 @@ if ($db->num_rows() == 1)
 	$get_info = $db->fetch();
 	$templating->set('title', '<li><a href="https://www.twitch.tv/gamingonlinux">' . $get_info['title'] . '</a></li>');
 
-	$countdown = '<noscript>'.$get_info['date'].'</noscript><span id="livestream'.$get_info['row_id'].'"></span><script type="text/javascript">var livestream' . $get_info['row_id'] . ' = moment.tz("'.$get_info['date'].'", "UTC"); $("#livestream'.$get_info['row_id'].'").countdown(livestream'.$get_info['row_id'].'.toDate(),function(event) {$(this).text(event.strftime(\'%D days %H:%M:%S\'));});</script>';
-	$templating->set('date', '<li>Date: ' . $countdown . ' UTC</li>');
+	$countdown = '<noscript>'.$get_info['date'].' UTC</noscript><span id="livestream'.$get_info['row_id'].'"></span><script type="text/javascript">var livestream' . $get_info['row_id'] . ' = moment.tz("'.$get_info['date'].'", "UTC"); $("#livestream'.$get_info['row_id'].'").countdown(livestream'.$get_info['row_id'].'.toDate(),function(event) {$(this).text(event.strftime(\'%D days %H:%M:%S\'));});</script>';
+	$templating->set('date', '<li>Date: ' . $countdown . '</li>');
 }
 else
 {

54ad83c4a0e8e819338908a71709b45ee934125e
liamdawe <liamdawe@gmail.com>
2016-11-08 09:53:47 +0000

    give a countdown if JS is enabled

--- blocks/block_livestreams.php
+++ blocks/block_livestreams.php
@@ -4,13 +4,15 @@ $templating->merge('blocks/block_livestreams');
 $templating->block('list');
 
 // count how many there is due this month and today
-$count_query = "SELECT `title`, `date` FROM `livestreams` ORDER BY `date` ASC LIMIT 1";
+$count_query = "SELECT `row_id`, `title`, `date` FROM `livestreams` ORDER BY `date` ASC LIMIT 1";
 $db->sqlquery($count_query);
 if ($db->num_rows() == 1)
 {
 	$get_info = $db->fetch();
 	$templating->set('title', '<li><a href="https://www.twitch.tv/gamingonlinux">' . $get_info['title'] . '</a></li>');
-	$templating->set('date', '<li>Date: ' . $get_info['date'] . ' UTC</li>');
+
+	$countdown = '<noscript>'.$get_info['date'].'</noscript><span id="livestream'.$get_info['row_id'].'"></span><script type="text/javascript">var livestream' . $get_info['row_id'] . ' = moment.tz("'.$get_info['date'].'", "UTC"); $("#livestream'.$get_info['row_id'].'").countdown(livestream'.$get_info['row_id'].'.toDate(),function(event) {$(this).text(event.strftime(\'%D days %H:%M:%S\'));});</script>';
+	$templating->set('date', '<li>Date: ' . $countdown . ' UTC</li>');
 }
 else
 {

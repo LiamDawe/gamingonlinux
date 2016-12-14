6a2102b235af04abc5db8c2ba5fd291b7ba95261
liamdawe <liamdawe@gmail.com>
2016-12-14 14:25:03 +0000

    update stats page to make it clearer

--- modules/website_stats.php
+++ modules/website_stats.php
@@ -7,24 +7,25 @@ $templating->merge('website_stats');
 $templating->block('top');
 
 $templating->block('users');
-$templating->set('total_users', core::config('total_users'));
+$templating->set('total_users', number_format(core::config('total_users')));
 
-$db->sqlquery("SELECT COUNT( DISTINCT user_id ) AS `counter` FROM `users` WHERE MONTH(FROM_UNIXTIME(`register_date`)) >= MONTH(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AND YEAR(FROM_UNIXTIME(`register_date`)) = YEAR(CURRENT_DATE)");
+$db->sqlquery("SELECT COUNT( DISTINCT user_id ) AS `counter` FROM `users` WHERE MONTH(FROM_UNIXTIME(`register_date`)) >= MONTH(NOW()) AND YEAR(FROM_UNIXTIME(`register_date`)) = YEAR(CURRENT_DATE)");
 $count_monthly_users = $db->fetch();
 
-$templating->set('users_month', $count_monthly_users['counter']);
+$templating->set('users_month', number_format($count_monthly_users['counter']));
 
 $db->sqlquery("SELECT COUNT(article_id) as `total` FROM `articles` WHERE `active` = 1");
 $total_articles = $db->fetch();
 
-$templating->set('total_articles', $total_articles['total']);
+$templating->set('total_articles', number_format($total_articles['total']));
 
+// count comments posted in the last 24 hours
 $last_24_hours = time() - 86400;
 
 $db->sqlquery("SELECT COUNT(comment_id) as total FROM `articles_comments` WHERE `time_posted` > ?", array($last_24_hours));
 $comments_24 = $db->fetch();
 
-$templating->set('total_comments', $comments_24['total']);
+$templating->set('total_comments', number_format($comments_24['total']));
 
 $db->sqlquery("SELECT COUNT( DISTINCT a.article_id ) AS `counter`, u.username, u.user_id, (SELECT `date` FROM `articles` WHERE author_id = a.author_id ORDER BY `article_id` DESC LIMIT 1) as last_date FROM `articles` a LEFT JOIN `users` u ON u.user_id = a.author_id LEFT JOIN `article_category_reference` c ON a.`article_id` = c.`article_id`
 WHERE MONTH(FROM_UNIXTIME(a.`date`)) >= MONTH(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AND YEAR(FROM_UNIXTIME(a.`date`)) = YEAR(CURRENT_DATE) AND a.`active` = 1 AND c.`category_id` NOT IN (63) GROUP BY u.`username` ORDER BY `counter` DESC");

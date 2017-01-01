59c3d8e3f8bd8d7bf4147cfe93c269bb88979f00
liamdawe <liamdawe@gmail.com>
2017-01-01 23:12:14 +0000

    new css for masonry and adjust support us page to use it

--- modules/support_us.php
+++ modules/support_us.php
@@ -9,29 +9,22 @@ $templating->block('list_top');
 
 // get supporter list Sorted by last login
 $res = $db->sqlquery("SELECT `username`, `user_id`, `avatar`, `gravatar_email`, `avatar_uploaded`,`avatar_gravatar`, `avatar_gallery`, `supporter_link` FROM `users` WHERE `secondary_user_group` IN (6,7) AND `user_group` != 1 AND `user_group` != 2 ORDER BY RAND() DESC LIMIT 9");
-
-//Chop the results up in arrays of 3 users per row
-$chucks = array_chunk($res->fetch_all_rows(PDO::FETCH_ASSOC), 3);
-
-foreach ($chucks as $row)
+$templating->block('list_row_start');
+while ($rowuser = $res->fetch())
 {
-	$templating->block('list_row_start');
-	foreach ($row as $bb => $rowuser)
+	$templating->block('person');
+
+	$templating->set('user_id', $rowuser['user_id']);
+	$templating->set('username', $rowuser['username']);
+
+	$avatar = user::sort_avatar($rowuser);
+	$templating->set('avatarurl', $avatar);
+
+	$supporter_link = '';
+	if (!empty($rowuser['supporter_link']))
 	{
-		$templating->block('person');
-
-		$templating->set('user_id', $rowuser['user_id']);
-		$templating->set('username', $rowuser['username']);
-
-		$avatar = user::sort_avatar($rowuser);
-		$templating->set('avatarurl', $avatar);
-
-		$supporter_link = '';
-		if (!empty($rowuser['supporter_link']))
-		{
-			$supporter_link = "<a href=\"{$rowuser['supporter_link']}\">{$rowuser['supporter_link']}</a>";
-		}
-		$templating->set('supporter_link', $supporter_link);
+		$supporter_link = "<a href=\"{$rowuser['supporter_link']}\">{$rowuser['supporter_link']}</a>";
 	}
-	$templating->block('list_row_end');
+	$templating->set('supporter_link', $supporter_link);
 }
+$templating->block('list_row_end');

485d779aa54b18540ff60d62b410fde77e642d9a
liamdawe <liamdawe@gmail.com>
2017-01-01 21:00:54 +0000

    working code for itch new games importer

--- includes/crons/itch_games_import.php
+++ includes/crons/itch_games_import.php
@@ -1,8 +1,8 @@
 <?php
 echo "Itch importer started on " .date('d-m-Y H:m:s'). "\n";
 
-//define('path', '/home/gamingonlinux/public_html/includes/');
-define('path', '/mnt/storage/public_html/includes/');
+define('path', '/home/gamingonlinux/public_html/includes/');
+//define('path', '/mnt/storage/public_html/includes/');
 
 include(path . 'config.php');
 
@@ -34,13 +34,7 @@ $get_url = $core->file_get_contents_curl($url);
 $get_url = preg_replace("^&(?!#38;)^", "&amp;", $get_url);
 $xml = simplexml_load_string($get_url);
 
-// for seeing what we have available
-/*
-echo '<pre>';
-print_r($xml);
-echo '</pre>';*/
-
-$games = '';
+$games_added = '';
 $email = 0;
 foreach ($xml->channel->item as $game)
 {
@@ -51,31 +45,51 @@ foreach ($xml->channel->item as $game)
 	print_r($game);
 	echo '</pre>';*/
 
-	// put Operating Systems into an array so we can loop through the Linux ones
 	if ($game->{'platforms'}->linux == 'yes')
 	{
 		$game->plainTitle = html_entity_decode($game->plainTitle, ENT_QUOTES);
-		echo $game->plainTitle . '<br />';
-		/*
-		$db->sqlquery("SELECT `name` FROM `game_list` WHERE `name` = ?", array($game->plainTitle));
-		if ($db->num_rows() == 0)
+
+		$name = preg_replace("/(™|®|©|&trade;|&reg;|&copy;|&#8482;|&#174;|&#169;)/", "", $game->plainTitle);
+
+		$parsed_release_date = strtotime($game->pubDate);
+		$released_date = date('Y-m-d', $parsed_release_date);
+
+		echo $name . ' ' . $game->link . ' ' . $released_date . '<br />';
+
+		$get_info = $db->sqlquery("SELECT `name`, `itch_link` FROM `calendar` WHERE `name` = ?", array($name));
+		$grab_info = $get_info->fetch();
+
+		$count_rows = $db->num_rows();
+
+		// if it does exist, make sure it's not from itch already
+		if ($count_rows == 0)
 		{
-			$db->sqlquery("INSERT INTO `game_list` SET `name` = ?", array($game->plainTitle));
-		}*/
+			$db->sqlquery("INSERT INTO `calendar` SET `name` = ?, `itch_link` = ?, `date` = ?, `approved` = 1", array($name, $game->link, $released_date));
+
+			$calendar_id = $db->grab_id();
+
+			echo "\tAdded this game to the calendar DB with id: " . $calendar_id . ".\n";
+
+			$games_added .= $name . '<br />';
+		}
+
+		// if we already have it, just update it
+		else if ($count_rows == 1 && $grab_info['itch_link'] == NULL)
+		{
+			$db->sqlquery("UPDATE `calendar` SET `itch_link` = ? WHERE `name` = ?", array($game->link, $name));
+
+			echo "Updated {$name} with the latest information<br />";
+		}
 	}
-echo "\n";
+
 }
-echo "\n\n";
-/*
-if ($email == 1)
+
+if (!empty($games_added))
 {
-	$to = 'liamdawe@gmail.com';
-	$subject = 'GOL Contact Us - Itch.io sales added';
-	// To send HTML mail, the Content-type header must be set
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$headers .= "From: GOL Contact Us <noreply@gamingonlinux.com>\r\n";
-	mail($to, $subject, "<a href=\"http://www.gamingonlinux.com/sales/\">Sales Page</a> - The auto Itch.io salesman has added<br />$games", $headers);
-	echo "Mail sent!";
-}*/
-echo "End of Itch.io import @ " . date('d-m-Y H:m:s') . ".\nHave a nice day.\n";
+  if (core::config('send_emails') == 1)
+  {
+    $mail = new mail('liamdawe@gmail.com', 'The itch new games importer has added new games', 'New games added to the <a href="https://www.gamingonlinux.com/index.php?module=calendar">calendar</a> from itch.io!<br />' . $games_added, '');
+    $mail->send();
+  }
+}
+echo "End of Itch.io import @ " . date('d-m-Y H:m:s');

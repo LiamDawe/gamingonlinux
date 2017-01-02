1f73f1dfae63ee7499a5588e2aa2aaa4c9559b26
liamdawe <liamdawe@gmail.com>
2017-01-02 10:57:59 +0000

    fix up monthly review article, don't auto publish in case of errors, put it into admin review

--- includes/crons/last_month_review.php
+++ includes/crons/last_month_review.php
@@ -14,17 +14,26 @@ include('/home/gamingonlinux/public_html/includes/class_template.php');
 
 $templating = new template();
 
-$db->sqlquery("SELECT count(DISTINCT a.article_id) as `counter` FROM `articles` a LEFT JOIN `article_category_reference` c ON a.article_id = c.article_id WHERE MONTH(FROM_UNIXTIME(a.`date`)) = MONTH(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AND YEAR(FROM_UNIXTIME(`date`)) = YEAR(CURRENT_DATE) AND c.category_id NOT IN (63) AND a.active = 1");
+$prev_month = date('n', strtotime('-1 months'));
+$year_selector = date('Y');
+if ($prev_month = 12)
+{
+	$time = strtotime("-1 year", time());
+	$year_selector = date("Y", $time);
+}
+$first_minute = mktime(0, 0, 0, $prev_month, 1, $year_selector);
+$last_minute = mktime(23, 59, 59, $prev_month, date("t"), $year_selector);
+
+$db->sqlquery("SELECT count(DISTINCT a.article_id) as `counter` FROM `articles` a LEFT JOIN `article_category_reference` c ON a.article_id = c.article_id WHERE a.`date` >= $first_minute AND a.`date` <= $last_minute AND c.category_id NOT IN (63) AND a.active = 1");
 
 $counter = $db->fetch();
 
-$prevmonth = date('F Y', strtotime('-1 months'));
+$prevdate = date('F Y', strtotime('-1 months'));
+$title = "The most popular Linux & SteamOS gaming articles for $prevdate, {$counter['counter']} in total";
 
-$title = "The most popular Linux & SteamOS gaming articles for $prevmonth, {$counter['counter']} in total";
+$tagline = "Here is a look back at the 15 most popular articles on GamingOnLinux for $prevdate, an easy way to for you to keep up to date on what has happened in the past month for Linux & SteamOS Gaming!";
 
-$tagline = "Here is a look back at the 15 most popular articles on GamingOnLinux for $prevmonth, an easy way to for you to keep up to date on what has happened in the past month for Linux & SteamOS Gaming!";
-
-$text = "Here is a look back at the 15 most popular articles on GamingOnLinux for $prevmonth, an easy way to for you to keep up to date on what has happened in the past month for Linux & SteamOS Gaming! If you wish to keep track of these overview posts you can with our <a href=\"http://www.gamingonlinux.com/article_rss.php?section=overviews\">Overview RSS</a>.<br /><br />We published a total of <strong>{$counter['counter']} articles last month</strong>! You can see who <a href=\"https://www.gamingonlinux.com/index.php?module=website_stats\">contributed articles on this page.</a><br />";
+$text = "Here is a look back at the 15 most popular articles on GamingOnLinux for $prevdate, an easy way to for you to keep up to date on what has happened in the past month for Linux & SteamOS Gaming! If you wish to keep track of these overview posts you can with our <a href=\"http://www.gamingonlinux.com/article_rss.php?section=overviews\">Overview RSS</a>.<br /><br />We published a total of <strong>{$counter['counter']} articles last month</strong>! You can see who <a href=\"https://www.gamingonlinux.com/index.php?module=website_stats\">contributed articles on this page.</a><br />";
 
 // sub query = grab the highest ones, then the outer query sorts them in ascending order, so we get the highest viewed articles, and then sorted from lowest to highest
 $db->sqlquery("SELECT a.*
@@ -34,7 +43,7 @@ FROM (SELECT a.article_id, a.date, a.tagline_image, a.article_top_image_filename
 	LEFT JOIN
 		`article_category_reference` c ON a.article_id = c.article_id
 	WHERE
-		MONTH(FROM_UNIXTIME(a.`date`)) = MONTH(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AND YEAR(FROM_UNIXTIME(`date`)) = YEAR(CURRENT_DATE) AND c.category_id NOT IN (63, 92) AND a.active = 1 group by `a`.`article_id`
+		a.`date` >= $first_minute AND a.`date` <= $last_minute AND c.category_id NOT IN (63, 92) AND a.active = 1 group by `a`.`article_id`
 	ORDER BY
 		a.views DESC
 	LIMIT 15
@@ -62,22 +71,22 @@ while ($articles = $db->fetch())
 	$text .= "<br /><em>{$articles['tagline']}</em><br /><br />";
 }
 
-
 $text .= "<br />All of this is possible thanks to <a href=\"http://patreon.com/liamdawe\">my Patreon campaign</a>, and our Supporters!<br />";
 
-$text .= "<br />What was your favourite Linux Gaming news from $prevmonth?";
+$text .= "<br />What was your favourite Linux Gaming news from $prevdate?";
 
 // DEBUG
 //echo $text;
 
 $slug = $core->nice_title($title);
 
-$db->sqlquery("INSERT INTO `articles` SET `author_id` = 1844, `date` = ?, `title` = ?, `slug` = ?, `tagline` = ?, `text` = ?, `show_in_menu` = 0, `tagline_image` = 'monthlyoverview.png'", array(core::$date, $title, $slug, $tagline, $text));
+$db->sqlquery("INSERT INTO `articles` SET `author_id` = 1844, `date` = ?, `title` = ?, `slug` = ?, `tagline` = ?, `text` = ?, `show_in_menu` = 0, `tagline_image` = 'monthlyoverview.png', `active` = 0, `admin_review` = 1", array(core::$date, $title, $slug, $tagline, $text));
 
 $article_id = $db->grab_id();
 
 $db->sqlquery("INSERT INTO `article_category_reference` SET `article_id` = ?, `category_id` = 63", array($article_id));
 
-include(core::config('path') . 'includes/telegram_poster.php');
-telegram($title . ' ' . core::config('website_url') . "articles/" . $slug . '.' . $article_id);
+// update admin notifications
+$db->sqlquery("INSERT INTO `admin_notifications` SET `user_id` = 1844, `completed` = 0, `type` = ?, `created_date` = ?, `data` = ?", array('article_admin_queue', core::$date, $article_id));
+
 ?>

62a7ff30865f6c3b4915b242593d3e75ee6cfd63
liam dawe <liamdawe@gmail.com>
2017-01-12 20:05:50 +0000

    the working patreon auto-mailer tool

--- tools/patreon.php
+++ tools/patreon.php
@@ -1,16 +1,52 @@
- <?php
+<?php
+$path = '/home/gamingonlinux/public_html/';
+//$path = '/mnt/storage/public_html/';
+include($path . 'includes/config.php');
+
+include($path . 'includes/class_mysql.php');
+$db = new mysql($database_host, $database_username, $database_password, $database_db);
+
+include($path . 'includes/class_core.php');
+$core = new core();
+
+include($path . 'includes/class_mail.php');
+
 $csv = array_map('str_getcsv', file('patreon.csv'));
 
-//$csvFile = file('patreon.csv');
 $data = [];
 array_splice($csv, 0, 2);
-//echo '<pre>';
-//print_r($csvFile);
-//echo '</pre>';
-foreach ($csv as $line) {
-    echo '<pre>';
-    print_r($line);
-    echo '</pre>';
-}
+foreach ($csv as $line)
+{
+  // make it a proper decimal number to compare against
+  $pledge = (float) $line[3];
 
+  // if they pledge at least 5 dollars a month
+  if ($pledge >= 5)
+  {
+    $db->sqlquery("SELECT `username` FROM `users` WHERE `email` = ?", array($line[2]));
+    if ($db->num_rows() != 1)
+    {
+      if (core::config('send_emails') == 1)
+      {
+        $html_message = "Hello from Liam at <a href=\"https://www.gamingonlinux.com\">GamingOnLinux.com</a>! Thank you for supporting me on Patreon.<br />
+        <br />
+        I have tried to match your email up to a username, but I didn't find anything. <br />
+        <br />
+        <strong>Don't worry</strong>, if you already have your GOL Supporter badge you can ignore this email! <br />
+        <br />
+        If you haven't, please reply with your username or email attached to a GOL account. You're likely using a different email address on Patreon to what you use on GOL.<br />
+        <br />
+        Thank you.<br />
+        <hr />
+        Ps. Don't worry if you have never seen this before, this email was generated from a new script I wrote to help me automate Patreon stuff! It will only be sent to you once a month, just to confirm and so I don't miss anyone.";
+
+
+        $mail = new mail($line[2], 'Thank you for supporting GamingOnLinux, more info may be needed', $html_message, '', 'Reply-To: liamdawe@gmail.com');
+        $mail->send();
+
+        echo "Email sent to " . $line[2] . '<br />';
+      }
+    }
+  }
+}
 ?>

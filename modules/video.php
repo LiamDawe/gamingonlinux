4694be9b02e1d3d9cfede8767c2530a3475f6006
liam dawe <liamdawe@gmail.com>
2017-01-14 17:17:45 +0000

    add sanity checks in a standard loop

--- modules/video.php
+++ modules/video.php
@@ -30,33 +30,54 @@ while ($user_list = $db->fetch())
   $templating->block('user');
   $templating->set('username', $user_list['username'] . '<br />');
 
-  $twitch = '';
-  if (!empty($user_list['twitch']))
+  // loop over each one and do some basic sanity checks to make sure it's a proper URL
+  $to_check = array('twitch', 'youtube');
+  $total_array = count($to_check);
+  $counter = 0;
+  foreach ($to_check as $check)
   {
-    if(strpos($user_list['twitch'], "http://") === false && strpos($user_list['twitch'], "https://") === false )
+    $check_output = '';
+    if (!empty($user_list[$check]))
     {
-      $user_list['twitch'] = 'https://' . $user_list['twitch'];
-    }
-    if(strpos($user_list['twitch'], "www.") === false)
-    {
-      $user_list['twitch'] = 'www.' . $user_list['twitch'];
-    }
-    $url = parse_url($user_list['twitch']);
-    if (isset($url['path']))
-    {
-      $twitch_username = str_replace('/', '', $url['path']);
-    }
-    $twitch = '<a class="ltwitch" data-tnick="'.$twitch_username.'" href="' . $user_list['twitch'] . '">Twitch</a> <span></span><br />';
-  }
-  $templating->set('twitch', $twitch);
+      $counter++;
 
-  $youtube = '';
-  if (!empty($user_list['youtube']))
-  {
-    $youtube = '<a href="'.$user_list['youtube'].'">Youtube</a><br />';
-  }
-  $templating->set('youtube', $youtube);
+      $check_output = $user_list[$check];
 
+      /* just to make sure the link is a proper URL, remove any combination of them, and then set it properly manually
+       this way we don't have to work out what has what vs what doesn't have what
+       it seems silly, but it's the only real sane way to make sure
+      */
+      $remove_array = array('www.', 'http://', 'https://');
+      $check_output = str_replace($remove_array, '', $check_output);
+      $check_output = 'https://www.' . $check_output;
+
+      // make the first letter a capital, as we don't use capitals in the DB
+      $service_name = ucfirst($check);
+
+      // grab the twitch username for the online checker
+      $additional_classes = '';
+      if ($check == 'twitch')
+      {
+        // their username will be the last thing in the path after the slash (we remove the slash to get it)
+        $url = parse_url($check_output);
+        if (isset($url['path']))
+        {
+          $twitch_username = str_replace('/', '', $url['path']);
+        }
+        $additional_classes = 'class="ltwitch" data-tnick="'.$twitch_username.'"';
+      }
+
+      // add a break if it's not the last link in the loop
+      $check_output = '<a '.$additional_classes.' href="'.$check_output.'">'.$service_name.'</a> <span></span>';
+      if ($counter != $total_array)
+      {
+        $check_output = $check_output . '<br />';
+      }
+    }
+
+    // additional stuff per-type of field for special content
+    $templating->set($check, $check_output);
+  }
 }
 $templating->block('bottom');
 $templating->set('pagination', $pagination);

d20f4450719894e3e32c93610899c6e10f09dc99
liam dawe <liamdawe@gmail.com>
2017-01-18 13:54:49 +0000

    when approving, check it's not already approved

--- admin_modules/mod_queue.php
+++ admin_modules/mod_queue.php
@@ -7,6 +7,10 @@ if (isset($_GET['message']))
 	{
 		$core->message('You have approved that message');
 	}
+	if ($_GET['message'] == 'already-approved')
+	{
+		$core->message('Slowpoke! Someone else has already approved it!');
+	}
 
 	if ($_GET['message'] == 'removed')
 	{
@@ -77,102 +81,126 @@ if (isset($_POST['action']))
 	{
 		if ($_POST['is_topic'] == 1)
 		{
-			$db->sqlquery("UPDATE `forum_topics` SET `approved` = 1, `creation_date` = ? WHERE `topic_id` = ?", array(core::$date, $_POST['topic_id']));
+			$db->sqlquery("SELECT `approved` FROM `forum_topics` WHERE `topic_id` = ?", array($_POST['topic_id']));
+			$find_approval = $db->fetch();
+			if ($find_approval['approved'] == 0)
+			{
+				$db->sqlquery("UPDATE `forum_topics` SET `approved` = 1, `creation_date` = ? WHERE `topic_id` = ?", array(core::$date, $_POST['topic_id']));
 
-			$db->sqlquery("UPDATE `forums` SET `last_post_time` = ?, `last_post_user_id` = ?, `last_post_topic_id` = ? WHERE `forum_id` = ?", array(core::$date, $_POST['author_id'], $_POST['topic_id'], $_POST['forum_id']));
+				$db->sqlquery("UPDATE `forums` SET `last_post_time` = ?, `last_post_user_id` = ?, `last_post_topic_id` = ? WHERE `forum_id` = ?", array(core::$date, $_POST['author_id'], $_POST['topic_id'], $_POST['forum_id']));
 
-			$db->sqlquery("UPDATE `admin_notifications` SET `completed` = 1, `completed_date` = ? WHERE `data` = ? AND `type` = 'mod_queue'", array(core::$date, $_POST['topic_id']));
-			$db->sqlquery("INSERT INTO `admin_notifications` SET `user_id` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?, `type` = 'mod_queue_approved', `data` = ?", array($_SESSION['user_id'], core::$date, core::$date, $_POST['topic_id']));
+				$db->sqlquery("UPDATE `admin_notifications` SET `completed` = 1, `completed_date` = ? WHERE `data` = ? AND `type` = 'mod_queue'", array(core::$date, $_POST['topic_id']));
+				$db->sqlquery("INSERT INTO `admin_notifications` SET `user_id` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?, `type` = 'mod_queue_approved', `data` = ?", array($_SESSION['user_id'], core::$date, core::$date, $_POST['topic_id']));
+
+				header("Location: /admin.php?module=mod_queue&view=manage&message=approved");
+				die();
+			}
+			else if ($find_approval['approved'] == 1)
+			{
+				header("Location: /admin.php?module=mod_queue&view=manage&message=already-approved");
+				die();
+			}
 		}
 
 		else if ($_POST['is_topic'] == 0)
 		{
-			$db->sqlquery("UPDATE `forum_replies` SET `approved` = 1, `creation_date` = ? WHERE `post_id` = ?", array(core::$date, $_POST['post_id']));
-
-			$db->sqlquery("UPDATE `forum_topics` SET `last_post_date` = ?, `last_post_id` = ?, `replys` = (replys + 1) WHERE `topic_id` = ?", array(core::$date, $_POST['author_id'], $_POST['topic_id']));
-
-			$db->sqlquery("UPDATE `forums` SET `last_post_time` = ?, `last_post_user_id` = ?, `last_post_topic_id` = ? WHERE `forum_id` = ?", array(core::$date, $_POST['author_id'], $_POST['topic_id'], $_POST['forum_id']));
-
-			$db->sqlquery("UPDATE `admin_notifications` SET `completed` = 1, `completed_date` = ? WHERE `data` = ? AND `type` = 'mod_queue_reply'", array(core::$date, $_POST['post_id']));
-			$db->sqlquery("INSERT INTO `admin_notifications` SET `user_id` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?, `type` = 'mod_queue_reply_approved', `data` = ?", array($_SESSION['user_id'], core::$date, core::$date, $_POST['post_id']));
-
-			// get article name for the email and redirect
-			$db->sqlquery("SELECT `topic_title` FROM `forum_topics` WHERE `topic_id` = ?", array($_POST['topic_id']));
-			$title = $db->fetch();
-
-			// email anyone subscribed which isn't you
-			$db->sqlquery("SELECT s.`user_id`, s.`emails`, u.`email`, u.`username` FROM `forum_topics_subscriptions` s INNER JOIN `users` u ON s.`user_id` = u.`user_id` WHERE s.`topic_id` = ? AND s.`send_email` = 1 AND s.`emails` = 1", array($_POST['topic_id']));
-			$users_array = array();
-			while ($users = $db->fetch())
+			$db->sqlquery("SELECT `approved` FROM `forum_replies` WHERE `post_id` = ?", array($_POST['post_id']));
+			$find_approval = $db->fetch();
+			if ($find_approval['approved'] == 0)
 			{
-				if ($users['user_id'] != $_POST['author_id'] && $users['emails'] == 1)
+				$db->sqlquery("UPDATE `forum_replies` SET `approved` = 1, `creation_date` = ? WHERE `post_id` = ?", array(core::$date, $_POST['post_id']));
+
+				$db->sqlquery("UPDATE `forum_topics` SET `last_post_date` = ?, `last_post_id` = ?, `replys` = (replys + 1) WHERE `topic_id` = ?", array(core::$date, $_POST['author_id'], $_POST['topic_id']));
+
+				$db->sqlquery("UPDATE `forums` SET `last_post_time` = ?, `last_post_user_id` = ?, `last_post_topic_id` = ? WHERE `forum_id` = ?", array(core::$date, $_POST['author_id'], $_POST['topic_id'], $_POST['forum_id']));
+
+				$db->sqlquery("UPDATE `admin_notifications` SET `completed` = 1, `completed_date` = ? WHERE `data` = ? AND `type` = 'mod_queue_reply'", array(core::$date, $_POST['post_id']));
+				$db->sqlquery("INSERT INTO `admin_notifications` SET `user_id` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?, `type` = 'mod_queue_reply_approved', `data` = ?", array($_SESSION['user_id'], core::$date, core::$date, $_POST['post_id']));
+
+				// get article name for the email and redirect
+				$db->sqlquery("SELECT `topic_title` FROM `forum_topics` WHERE `topic_id` = ?", array($_POST['topic_id']));
+				$title = $db->fetch();
+
+				// email anyone subscribed which isn't you
+				$db->sqlquery("SELECT s.`user_id`, s.`emails`, u.`email`, u.`username` FROM `forum_topics_subscriptions` s INNER JOIN `users` u ON s.`user_id` = u.`user_id` WHERE s.`topic_id` = ? AND s.`send_email` = 1 AND s.`emails` = 1", array($_POST['topic_id']));
+				$users_array = array();
+				while ($users = $db->fetch())
 				{
-					$users_array[$users['user_id']]['user_id'] = $users['user_id'];
-					$users_array[$users['user_id']]['email'] = $users['email'];
-					$users_array[$users['user_id']]['username'] = $users['username'];
-				}
-			}
-
-			$db->sqlquery("SELECT `username` FROM `users` WHERE `user_id` = ?", array($_POST['author_id']));
-			$author_username = $db->fetch();
-
-			// send the emails
-			foreach ($users_array as $email_user)
-			{
-				$email_message = email_bbcode($_POST['text']);
-
-				// subject
-				$subject = "New reply to forum post {$title['topic_title']} on GamingOnLinux.com";
-
-				// message
-				$html_message = "
-				<p>Hello <strong>{$email_user['username']}</strong>,</p>
-				<p><strong>{$author_username['username']}</strong> has replied to a forum topic you follow on titled \"<strong><a href=\"" . core::config('website_url') . "forum/topic/{$topic_id}/post_id={$post_id}\">{$title['topic_title']}</a></strong>\". There may be more replies after this one, and you may not get any more emails depending on your email settings in your UserCP.</p>
-				<div>
-				<hr>
-				{$email_message}
-				<hr>
-				You can unsubscribe from this topic by <a href=\"" . core::config('website_url') . "unsubscribe.php?user_id={$email_user['user_id']}&topic_id={$topic_id}&email={$email_user['email']}\">clicking here</a>, you can manage your subscriptions anytime in your <a href=\"" . core::config('website_url') . "usercp.php\">User Control Panel</a>.
-				<hr>
-					<p>If you haven&#39;t registered at <a href=\"" . core::config('website_url') . "\" target=\"_blank\">" . core::config('website_url') . "</a>, Forward this mail to <a href=\"mailto:liamdawe@gmail.com\" target=\"_blank\">liamdawe@gmail.com</a> with some info about what you want us to do about it or if you logged in and found no message let us know!</p>
-					<p>Please, Don&#39;t reply to this automated message, We do not read any mails recieved on this email address.</p>
-					<p>-----------------------------------------------------------------------------------------------------------</p>
-				</div>";
-
-				$plain_message = "Hello {$email_user['username']}, {$author_username['username']} has replied to a forum topic you follow on titled \"{$title['topic_title']}\". There may be more replies after this one, and you may not get any more emails depending on your email settings in your UserCP. See this new message here: " . core::config('website_url') . "forum/topic/{$topic_id}/post_id={$post_id}";
-
-				// Mail it
-				if (core::config('send_emails') == 1)
-				{
-					$mail = new mail($email_user['email'], $subject, $html_message, $plain_message);
-					$mail->send();
+					if ($users['user_id'] != $_POST['author_id'] && $users['emails'] == 1)
+					{
+						$users_array[$users['user_id']]['user_id'] = $users['user_id'];
+						$users_array[$users['user_id']]['email'] = $users['email'];
+						$users_array[$users['user_id']]['username'] = $users['username'];
+					}
 				}
 
-				// remove anyones send_emails subscription setting if they have it set to email once
-				$db->sqlquery("SELECT `email_options` FROM `users` WHERE `user_id` = ?", array($email_user['user_id']));
-				$update_sub = $db->fetch();
+				$db->sqlquery("SELECT `username` FROM `users` WHERE `user_id` = ?", array($_POST['author_id']));
+				$author_username = $db->fetch();
 
-				if ($update_sub['email_options'] == 2)
+				// send the emails
+				foreach ($users_array as $email_user)
 				{
-					$db->sqlquery("UPDATE `forum_topics_subscriptions` SET `send_email` = 0 WHERE `topic_id` = ? AND `user_id` = ?", array($topic_id, $email_user['user_id']));
+					$email_message = email_bbcode($_POST['text']);
+
+					// subject
+					$subject = "New reply to forum post {$title['topic_title']} on GamingOnLinux.com";
+
+					// message
+					$html_message = "
+					<p>Hello <strong>{$email_user['username']}</strong>,</p>
+					<p><strong>{$author_username['username']}</strong> has replied to a forum topic you follow on titled \"<strong><a href=\"" . core::config('website_url') . "forum/topic/{$topic_id}/post_id={$post_id}\">{$title['topic_title']}</a></strong>\". There may be more replies after this one, and you may not get any more emails depending on your email settings in your UserCP.</p>
+					<div>
+					<hr>
+					{$email_message}
+					<hr>
+					You can unsubscribe from this topic by <a href=\"" . core::config('website_url') . "unsubscribe.php?user_id={$email_user['user_id']}&topic_id={$topic_id}&email={$email_user['email']}\">clicking here</a>, you can manage your subscriptions anytime in your <a href=\"" . core::config('website_url') . "usercp.php\">User Control Panel</a>.
+					<hr>
+						<p>If you haven&#39;t registered at <a href=\"" . core::config('website_url') . "\" target=\"_blank\">" . core::config('website_url') . "</a>, Forward this mail to <a href=\"mailto:liamdawe@gmail.com\" target=\"_blank\">liamdawe@gmail.com</a> with some info about what you want us to do about it or if you logged in and found no message let us know!</p>
+						<p>Please, Don&#39;t reply to this automated message, We do not read any mails recieved on this email address.</p>
+						<p>-----------------------------------------------------------------------------------------------------------</p>
+					</div>";
+
+					$plain_message = "Hello {$email_user['username']}, {$author_username['username']} has replied to a forum topic you follow on titled \"{$title['topic_title']}\". There may be more replies after this one, and you may not get any more emails depending on your email settings in your UserCP. See this new message here: " . core::config('website_url') . "forum/topic/{$topic_id}/post_id={$post_id}";
+
+					// Mail it
+					if (core::config('send_emails') == 1)
+					{
+						$mail = new mail($email_user['email'], $subject, $html_message, $plain_message);
+						$mail->send();
+					}
+
+					// remove anyones send_emails subscription setting if they have it set to email once
+					$db->sqlquery("SELECT `email_options` FROM `users` WHERE `user_id` = ?", array($email_user['user_id']));
+					$update_sub = $db->fetch();
+
+					if ($update_sub['email_options'] == 2)
+					{
+						$db->sqlquery("UPDATE `forum_topics_subscriptions` SET `send_email` = 0 WHERE `topic_id` = ? AND `user_id` = ?", array($topic_id, $email_user['user_id']));
+					}
 				}
+
+				// update their post counter
+				$db->sqlquery("UPDATE `users` SET `forum_posts` = (forum_posts + 1) WHERE `user_id` = ?", array($_POST['author_id']));
+
+				// add 1 to their approval rating
+				$db->sqlquery("SELECT `mod_approved` FROM `users` WHERE `user_id` = ?", array($_POST['author_id']));
+				$user_get = $db->fetch();
+
+				// remove them from the mod queue if we need to
+				if ($user_get['mod_approved'] >= 2)
+				{
+					$db->sqlquery("UPDATE `users` SET `in_mod_queue` = 0 WHERE `user_id` = ?", array($_POST['author_id']));
+				}
+				$db->sqlquery("UPDATE `users` SET `mod_approved` = (mod_approved + 1), `forum_posts` = (forum_posts + 1) WHERE `user_id` = ?", array($_POST['author_id']));
+
+				header("Location: /admin.php?module=mod_queue&view=manage&message=approved");
+				die();
 			}
-
-			// update their post counter
-			$db->sqlquery("UPDATE `users` SET `forum_posts` = (forum_posts + 1) WHERE `user_id` = ?", array($_POST['author_id']));
-
-			// add 1 to their approval rating
-			$db->sqlquery("SELECT `mod_approved` FROM `users` WHERE `user_id` = ?", array($_POST['author_id']));
-			$user_get = $db->fetch();
-
-			// remove them from the mod queue if we need to
-			if ($user_get['mod_approved'] >= 2)
-			{
-				$db->sqlquery("UPDATE `users` SET `in_mod_queue` = 0 WHERE `user_id` = ?", array($_POST['author_id']));
-			}
-			$db->sqlquery("UPDATE `users` SET `mod_approved` = (mod_approved + 1), `forum_posts` = (forum_posts + 1) WHERE `user_id` = ?", array($_POST['author_id']));
-
-			header("Location: /admin.php?module=mod_queue&view=manage&message=approved");
+		}
+		else if ($find_approval['approved'] == 1)
+		{
+			header("Location: /admin.php?module=mod_queue&view=manage&message=already-approved");
+			die();
 		}
 	}
 
@@ -195,6 +223,7 @@ if (isset($_POST['action']))
 		$db->sqlquery("INSERT INTO `admin_notifications` SET `user_id` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?, `type` = 'mod_queue_removed', `data` = ?", array($_SESSION['user_id'], core::$date, core::$date, $_POST['topic_id']));
 
 		header("Location: /admin.php?module=mod_queue&view=manage&message=removed");
+		die();
 	}
 
 	// ban them and remove the topic
@@ -224,6 +253,7 @@ if (isset($_POST['action']))
 		$db->sqlquery("INSERT INTO `admin_notifications` SET `user_id` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?, `type` = 'mod_queue_removed_ban', `data` = ?", array($_SESSION['user_id'], core::$date, core::$date, $_POST['topic_id']));
 
 		header("Location: /admin.php?module=mod_queue&view=manage&message=removed");
+		die();
 	}
 }
 ?>

fe5c591833e680d84e659786f49826162d027be2
liam dawe <liamdawe@gmail.com>
2017-01-30 19:32:12 +0000

    switch forum rss to use newer methods too

--- forum_rss.php
+++ forum_rss.php
@@ -12,43 +12,60 @@ $templating = new template(core::config('template'));
 
 if (core::config('articles_rss') == 1)
 {
+	header('Content-Type: text/xml; charset=utf-8', true);
+	header("Cache-Control: max-age=3600");
+	
 	$now = date("D, d M Y H:i:s O");
+	
+	$xml = new XMLWriter();
+	$xml->openMemory();
+	$xml->startDocument( '1.0', 'UTF-8' );
+	
+	$xml->startElement( 'rss' );
+	$xml->writeAttribute( 'version', '2.0' );
+	$xml->writeAttribute( 'xmlns:atom', 'http://www.w3.org/2005/Atom' );
 
-	$output = "<?xml version=\"1.0\"?>
-	<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">
-		<channel>
-			<title>Latest forum posts</title>
-			<link>https://www.gamingonlinux.com/forum_rss.php</link>
-			<atom:link href=\"". core::config('website_url') . "forum_rss.php\" rel=\"self\" type=\"application/rss+xml\" />
-			<language>en-us</language>
-			<description></description>
-			<pubDate>$now</pubDate>
-			<lastBuildDate>$now</lastBuildDate>";
+	$xml->startElement('channel');
+	$xml->writeElement('title', 'GamingOnLinux.com Latest forum posts');
+	$xml->writeElement('link', 'https://www.gamingonlinux.com');
+	$xml->writeElement('description', 'The latest news from GamingOnLinux.com');
+	$xml->writeElement('pubDate', $now);
+	$xml->writeElement('language', 'en-us');
+	$xml->writeElement('lastBuildDate', $now);
+
+	$xml->startElement('atom:link');
+	$xml->writeAttribute('href', 'https://www.gamingonlinux.com/forum_rss.php');
+	$xml->writeAttribute('rel', 'self');
+	$xml->writeAttribute('type', 'application/rss+xml');
+	$xml->endElement();
 
 	$db->sqlquery("SELECT `topic_id`, `topic_title`, `last_post_date` FROM `forum_topics` WHERE `approved` = 1 ORDER BY `last_post_date` DESC LIMIT 15");
 
 	while ($line = $db->fetch())
 	{
+		$xml->startElement('item');
+		
+		$xml->startElement('title');
+		$xml->writeCData($line['topic_title']);
+		$xml->endElement();
+		
 		// make date human readable
 		$date = date("D, d M Y H:i:s O", $line['last_post_date']);
-
-		$title = htmlspecialchars($line['topic_title'], ENT_QUOTES, 'UTF-8');
-
-		$output .= "
-			<item>
-				<title>{$title}</title>
-				<link>". core::config('website_url') . "forum/topic/{$line['topic_id']}/</link>
-				<pubDate>{$date}</pubDate>
-				<guid>". core::config('website_url') . "forum/topic/{$line['topic_id']}/</guid>
-			</item>";
+		$xml->writeElement('pubDate', $date);
+		
+		$link = core::config('website_url') . "forum/topic/{$line['topic_id']}/";
+		$xml->writeElement('link', $link);
+		$xml->writeElement('guid', $link);
+		
+		// close this item
+		$xml->endElement();
 	}
 
-	$output .= "
-		</channel>
-	</rss>";
+	// close the channel and then the rss tags and then output the RSS feed
+	$xml->endElement();
+	$xml->endElement();
 
-	header("Content-Type: application/rss+xml");
-	echo $output;
+	echo $xml->outputMemory();
 }
 
 else

2a10a0ee2dbf78a1cc9ceccb001ef189c60993b3
liam dawe <liamdawe@gmail.com>
2017-01-30 19:22:55 +0000

    much cleaner article rss using built in functions to build it

--- article_rss.php
+++ article_rss.php
@@ -25,21 +25,32 @@ LIMIT 1");
 
 $last_time = $db->fetch();
 
-header("Content-Type: application/rss+xml");
+header('Content-Type: text/xml; charset=utf-8', true);
 header("Cache-Control: max-age=3600");
 
 $last_date = gmdate("D, d M Y H:i:s O", $last_time['date']);
 
-$output = "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">
-	<channel>
-		<title>GamingOnLinux.com Latest Articles</title>
-		<link>http://www.gamingonlinux.com/</link>
-		<atom:link href=\"https://www.gamingonlinux.com/article_rss.php\" rel=\"self\" type=\"application/rss+xml\" />
-		<language>en-us</language>
-		<description></description>
-		<pubDate>$last_date</pubDate>
-		<lastBuildDate>$last_date</lastBuildDate>";
+$xml = new XMLWriter();
+$xml->openMemory();
+$xml->startDocument( '1.0', 'UTF-8' );
+ 
+$xml->startElement( 'rss' );
+$xml->writeAttribute( 'version', '2.0' );
+$xml->writeAttribute( 'xmlns:atom', 'http://www.w3.org/2005/Atom' );
+
+$xml->startElement('channel');
+$xml->writeElement('title', 'GamingOnLinux.com Latest Articles');
+$xml->writeElement('link', 'https://www.gamingonlinux.com');
+$xml->writeElement('description', 'The latest news from GamingOnLinux.com');
+$xml->writeElement('pubDate', $last_date);
+$xml->writeElement('language', 'en-us');
+$xml->writeElement('lastBuildDate', $last_date);
+
+$xml->startElement('atom:link');
+$xml->writeAttribute('href', 'https://www.gamingonlinux.com/article_rss_new.php');
+$xml->writeAttribute('rel', 'self');
+$xml->writeAttribute('type', 'application/rss+xml');
+$xml->endElement();
 
 $db->sqlquery("SELECT a.*, u.username
 FROM `articles` a LEFT JOIN `users` u ON a.author_id = u.user_id $sql_join
@@ -51,10 +62,38 @@ $articles = $db->fetch_all_rows();
 
 foreach ($articles as $line)
 {
-	// make date human readable
-	$date = date("D, d M Y H:i:s O", $line['date']);
-	$nice_title = preg_replace('/<[^>]+>/', '', $core->nice_title($line['title']) ); // ~~ Piratelv @ 28/08/13
+	$xml->startElement('item');
+	
+	$xml->startElement('title');
+	$xml->writeCData($line['title']);
+	$xml->endElement();
 
+	$username = '';
+	if (isset($line['username']))
+	{
+		$username = $line['username'];
+	}
+	else if (isset($line['guest_username']))
+	{
+		$username = $line['username'];
+	}
+	$xml->writeElement('author', "contact@gamingonlinux.com ($username)");
+	
+	$nice_title = $core->nice_title($line['title']);
+	$xml->writeElement('link', "https://www.gamingonlinux.com/articles/$nice_title.{$line['article_id']}");
+	$xml->writeElement('guid', "https://www.gamingonlinux.com/articles/$nice_title.{$line['article_id']}");
+	
+	// sort out the categories (tags)
+	$tag_list = [];
+	$db->sqlquery("SELECT c.`category_name`, c.`category_id` FROM `articles_categorys` c INNER JOIN `article_category_reference` r ON c.category_id = r.category_id WHERE r.article_id = ?", array($line['article_id']));
+	while ($get_categories = $db->fetch())
+	{
+		$xml->startElement('category');
+		$xml->writeCData($get_categories['category_name']);
+		$xml->endElement();
+		$tag_list[] = $get_categories['category_name'];
+	}
+	
 	$tagline_bbcode = '';
 	$bbcode_tagline_gallery = 0;
 	if (!empty($line['tagline_image']))
@@ -78,51 +117,21 @@ foreach ($articles as $line)
 
 		$text = bbcode($text, 1, 1);
 	}
-
-	$title = str_replace("&#039;", '\'', $line['title']);
-	$title = str_replace("&", "&amp;", $title);
-	$title = $title;
-
-	$categories_list = array();
-	// sort out the categories (tags)
-	$db->sqlquery("SELECT c.`category_name`, c.`category_id` FROM `articles_categorys` c INNER JOIN `article_category_reference` r ON c.category_id = r.category_id WHERE r.article_id = ?", array($line['article_id']));
-	while ($get_categories = $db->fetch())
-	{
-		$categories_list[] = htmlspecialchars($get_categories['category_name']);
-	}
-
-	$cats = implode(', ', $categories_list);
 	
-	$username = '';
-	if (isset($line['username']))
-	{
-		$username = $line['username'];
-	}
-	else if (isset($line['guest_username']))
-	{
-		$username = $line['username'];
-	}
-
-	$output .= "
-		<item>
-			<title>{$title}</title>
-			<author>contact@gamingonlinux.com ($username)</author>
-			<link>http://www.gamingonlinux.com/articles/$nice_title.{$line['article_id']}</link>
-			<description><![CDATA[<p>Tags:$cats</p><p>{$text}</p><br />Content from <a href=\"https://www.gamingonlinux.com\">GamingOnLinux.com</a>]]></description>
-			<pubDate>{$date}</pubDate>
-			<guid>http://www.gamingonlinux.com/articles/$nice_title.{$line['article_id']}</guid>\n";
-
-	foreach ($categories_list as $cat)
-	{
-		$output .= "<category domain=\"\">$cat</category>\n";
-	}
-
-	$output .= "</item>";
+	$xml->startElement('description');
+	$xml->writeCData('<p>Tags: ' . implode(', ', $tag_list) . '</p><p>' . $text . '</p>');
+	$xml->endElement();
+	
+	$date = date("D, d M Y H:i:s O", $line['date']);
+	$xml->writeElement('pubDate', $date);
+	
+	// close this item
+	$xml->endElement();
 }
 
-$output .= "
-	</channel>
-</rss>";
+// close the channel and then the rss tags and then output the RSS feed
+$xml->endElement();
+$xml->endElement();
 
-echo $output;
+echo $xml->outputMemory();
 ?>

565c6cd0edd401006486cd62ff581cdbc2097fa5
liam dawe <liamdawe@gmail.com>
2017-01-31 09:33:13 +0000

    allow messages on forum index, also dont trust session, make sure the forum type exists

--- modules/forum.php
+++ modules/forum.php
@@ -1,8 +1,28 @@
 <?php
+if (isset($_GET['message']))
+{
+  $extra = NULL;
+  if (isset($_GET['extra']))
+  {
+    $extra = $_GET['extra'];
+  }
+  $message = $message_map->get_message($_GET['message'], $extra);
+  $core->message($message['message'], NULL, $message['error']);
+}
+
+$forum_types = ['normal_forum', 'flat_forum'];
+
 $forum_type = 'normal_forum';
 if (isset($_SESSION['forum_type']))
 {
 	$forum_type = $_SESSION['forum_type'];
 }
-include(core::config('path') . 'modules/forum/'.$forum_type.'.php')
+if (in_array($_SESSION['forum_type'], $forum_types))
+{
+	include(core::config('path') . 'modules/forum/'.$forum_type.'.php');
+}
+else
+{
+	include(core::config('path') . 'modules/forum/normal_forum.php');
+}
 ?>

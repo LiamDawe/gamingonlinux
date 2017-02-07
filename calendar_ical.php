445c30575c14edf0a56decc75ea604eebe275a95
liam dawe <liamdawe@gmail.com>
2017-02-07 23:49:32 +0000

    totally forgot to update this for new methods

--- calendar_ical.php
+++ calendar_ical.php
@@ -2,13 +2,13 @@
 header('Content-type: text/calendar; charset=utf-8');
 header('Content-Disposition: attachment; filename=calendar.ical');
 
-include('includes/config.php');
+$file_dir = dirname(__FILE__);
 
-include('includes/class_mysql.php');
-$db = new mysql($database_host, $database_username, $database_password, $database_db);
+include($file_dir . '/includes/class_core.php');
+$core = new core($file_dir);
 
-include('includes/class_core.php');
-$core = new core();
+include($file_dir. '/includes/class_mysql.php');
+$db = new mysql(core::$database['host'], core::$database['username'], core::$database['password'], core::$database['database']);
 
 // the iCal date format. Note the Z on the end indicates a UTC timestamp.
 define('DATE_ICAL', 'Ymd\THis\Z');

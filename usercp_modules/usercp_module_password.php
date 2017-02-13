6f3afcee7be9b408cf65d2feb52710e058a7b072
liam dawe <liamdawe@gmail.com>
2017-02-13 16:30:19 +0000

    only allow an auto generated password to be emailed if they have none, for social logins that dont set a password, that way if someone gains access they cant poach an account

--- usercp_modules/usercp_module_password.php
+++ usercp_modules/usercp_module_password.php
@@ -19,91 +19,137 @@ if (isset($_GET['message']))
 	{
 		$core->message('The new password didn\'t match the checker, try again!', NULL, 1);
 	}
+	if ($_GET['message'] == 'existing-password')
+	{
+		$core->message('You already have a password!', NULL, 1);
+	}
 	if ($_GET['message'] == 'done')
 	{
 		$core->message("Your password has been updated!");
 	}
+	if ($_GET['message'] == 'password-sent')
+	{
+		$core->message("Your new password has been emailed to you!");
+	}
 }
-$templating->merge('usercp_modules/usercp_module_password');
-$templating->block('main');
 
 // find current password
-$db->sqlquery("SELECT `password` FROM `users` WHERE `user_id` = ?", array($_SESSION['user_id']));
+$db->sqlquery("SELECT `username`, `password`, `email` FROM `users` WHERE `user_id` = ?", array($_SESSION['user_id']));
 $grab_current_password = $db->fetch();
 
-// steam or twitter login
-$current_password = '';
-if (!empty($grab_current_password['password']))
+$templating->merge('usercp_modules/usercp_module_password');
+
+if (empty($grab_current_password['password']))
 {
-	$current_password = 'Current Password: <input type="password" name="current_password" /><br />';
+	$templating->block('no_password');
 }
-$templating->set('current_password', $current_password);
-
-if (isset($_POST['Update']))
+else
 {
-	if (empty($_POST['current_password']))
-	{
-		header("Location: /usercp.php?module=password&message=nocurrent");
-		die();
-	}
+	$templating->block('main');
+}
 
-	// find current password
-	$db->sqlquery("SELECT `username`, `password`, `steam_id`, `oauth_uid`, `email` FROM `users` WHERE `user_id` = ?", array($_SESSION['user_id']));
-	$grab_current_password = $db->fetch();
-
-	// if they have no password, they simply must have a steamid or a twitter oauth id
-	if (empty($grab_current_password['password']))
+if (isset($_POST['act']))
+{
+	if ($_POST['act'] == 'request')
 	{
-		if (empty($grab_current_password['steam_id']) && empty($grab_current_password['oauth_uid']))
+		if (!empty($_POST['current_password']))
 		{
-			$user->logout();
+			header("Location: /usercp.php?module=password&message=existing-password");
 			die();
 		}
-	}
+		
+		$new_password = core::random_id(15);
+		$safe_password = password_hash($new_password, PASSWORD_BCRYPT);
+		
+		$db->sqlquery("UPDATE `users` SET `password` = ? WHERE `user_id` = ?", array($safe_password, $_SESSION['user_id']));
+		
+		// send an email to their old address to let them know
+		$subject = "Password requested on GamingOnLinux.com";
 
-	if (!empty($grab_current_password['password']))
-	{
-		// check the original matches
-		if (!password_verify($_POST['current_password'], $grab_current_password['password']))
+		// message
+		$html_message = "<p>Hello <strong>{$grab_current_password['username']}</strong>,</p>
+		<p>Someone, hopefully you, has requested a password for your account on <a href=\"".core::config('website_url')."\">gamingonlinux.com</a>. If this was you, please ignore this email as it's just a security measure.</p>
+		<p>Your new password is: ".$new_password.", please keep a note of it!</p>
+		<hr>";
+
+		$plain_message = PHP_EOL."Hello {$grab_current_password['username']}! Someone, hopefully you, has requested a password for your account on ".core::config('website_url').". If this was you, please ignore this email as it's just a security measure. Your new password is: ".$new_password . ", please keep a note of it!";
+
+		// Mail it
+		if (core::config('send_emails') == 1)
 		{
-			header("Location: /usercp.php?module=password&message=nomatchoriginal");
+			$mail = new mail($grab_current_password['email'], $subject, $html_message, $plain_message);
+			$mail->send();
+		}
+		
+		header("Location: /usercp.php?module=password&message=password-sent");
+	}
+	
+	if ($_POST['act'] == 'Update')
+	{
+		if (empty($_POST['current_password']))
+		{
+			header("Location: /usercp.php?module=password&message=nocurrent");
 			die();
 		}
+
+		// find current password
+		$db->sqlquery("SELECT `username`, `password`, `steam_id`, `oauth_uid`, `email` FROM `users` WHERE `user_id` = ?", array($_SESSION['user_id']));
+		$grab_current_password = $db->fetch();
+
+		// if they have no password, they simply must have a steamid or a twitter oauth id
+		if (empty($grab_current_password['password']))
+		{
+			if (empty($grab_current_password['steam_id']) && empty($grab_current_password['oauth_uid']))
+			{
+				$user->logout();
+				die();
+			}
+		}
+
+		if (!empty($grab_current_password['password']))
+		{
+			// check the original matches
+			if (!password_verify($_POST['current_password'], $grab_current_password['password']))
+			{
+				header("Location: /usercp.php?module=password&message=nomatchoriginal");
+				die();
+			}
+		}
+
+		$new_password_safe = password_hash($_POST['new_password'], PASSWORD_BCRYPT);
+
+		if (empty($_POST['new_password']) || empty($_POST['new_password_check']))
+		{
+			header("Location: /usercp.php?module=password&message=newcheck");
+			die();
+		}
+
+		// check the new ones match
+		if ($_POST['new_password'] != $_POST['new_password_check'])
+		{
+			header("Location: /usercp.php?module=password&message=nochecknew");
+			die();
+		}
+
+		// send an email to their old address to let them know
+		$subject = "Password changed on GamingOnLinux.com";
+
+		// message
+		$html_message = "<p>Hello <strong>{$grab_current_password['username']}</strong>,</p>
+		<p>Someone, hopefully you, has changed your password on <a href=\"".core::config('website_url')."\">gamingonlinux.com</a>. If this was you, please ignore this email as it's just a security measure.</p>
+		<hr>";
+
+		$plain_message = PHP_EOL."Hello {$grab_current_password['username']}! Someone, hopefully you, has changed your password on ".core::config('website_url').". If this was you, please ignore this email as it's just a security measure.";
+
+		// Mail it
+		if (core::config('send_emails') == 1)
+		{
+			$mail = new mail($grab_current_password['email'], $subject, $html_message, $plain_message);
+			$mail->send();
+		}
+
+		$db->sqlquery("UPDATE `users` SET `password` = ? WHERE `user_id` = ?", array($new_password_safe, $_SESSION['user_id']));
+		header("Location: /usercp.php?module=password&message=done");
 	}
-
-	$new_password_safe = password_hash($_POST['new_password'], PASSWORD_BCRYPT);
-
-	if (empty($_POST['new_password']) || empty($_POST['new_password_check']))
-	{
-		header("Location: /usercp.php?module=password&message=newcheck");
-		die();
-	}
-
-	// check the new ones match
-	if ($_POST['new_password'] != $_POST['new_password_check'])
-	{
-		header("Location: /usercp.php?module=password&message=nochecknew");
-		die();
-	}
-
-	// send an email to their old address to let them know
-	$subject = "Password changed on GamingOnLinux.com";
-
-	// message
-	$html_message = "<p>Hello <strong>{$grab_current_password['username']}</strong>,</p>
-	<p>Someone, hopefully you, has changed your password on <a href=\"".core::config('website_url')."\">gamingonlinux.com</a>. If this was you, please ignore this email as it's just a security measure.</p>
-	<hr>";
-
-	$plain_message = PHP_EOL."Hello {$grab_current_password['username']}! Someone, hopefully you, has changed your password on ".core::config('website_url').". If this was you, please ignore this email as it's just a security measure.";
-
-	// Mail it
-	if (core::config('send_emails') == 1)
-	{
-		$mail = new mail($grab_current_password['email'], $subject, $html_message, $plain_message);
-		$mail->send();
-	}
-
-	$db->sqlquery("UPDATE `users` SET `password` = ? WHERE `user_id` = ?", array($new_password_safe, $_SESSION['user_id']));
-	header("Location: /usercp.php?module=password&message=done");
 }
 ?>

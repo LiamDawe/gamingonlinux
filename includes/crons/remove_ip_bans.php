bf2401085852eb3b0a55c9086c17036c11fadea3
liam dawe <liamdawe@gmail.com>
2017-03-07 14:47:49 +0000

    new cron for removing IP bans after 1 month has lapsed since the ban

---
+++ includes/crons/remove_ip_bans.php
@@ -0,0 +1,12 @@
+<?php
+$file_dir = dirname( dirname( dirname(__FILE__) ) );
+
+include($file_dir . '/includes/class_core.php');
+$core = new core($file_dir);
+
+include($file_dir . '/includes/class_mysql.php');
+$db = new mysql(core::$database['host'], core::$database['username'], core::$database['password'], core::$database['database']);
+
+$timeout = 30;
+
+$db->sqlquery("DELETE FROM `ipbans` WHERE `ban_date` < NOW() - INTERVAL $timeout DAY");

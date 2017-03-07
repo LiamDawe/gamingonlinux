09132202ebef5bf987059822b0d7935a4127c02c
liam dawe <liamdawe@gmail.com>
2017-03-07 14:38:40 +0000

    add new needed messaging files

---
+++ includes/messages/admin/add_article_messages.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	"taglinetoolong" => 
+	[
+		"text" => "The tagline was too long, it needs to be %d characters or less!",
+		"additions" => 1,
+		"error" => 1
+	],
+];
+?>
---
+++ includes/messages/admin/comments_messages.php
@@ -0,0 +1,9 @@
+<?php
+return [
+	"double_comment" => 
+	[
+		"text"	=> 'You cannot comment the same thing twice!',
+		"error" => 1
+	]
+];
+?>

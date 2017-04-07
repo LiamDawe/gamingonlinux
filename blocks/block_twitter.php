0e5c92b396dcd5e858d515400dc448b25e1af34e
liam dawe <liamdawe@gmail.com>
2017-04-07 20:53:32 +0100

    give the twitter block a sane height, default went on forever WHUT

--- blocks/block_twitter.php
+++ blocks/block_twitter.php
@@ -4,12 +4,12 @@ $templating->block('menu');
 
 if (isset($_SESSION['theme']) && $_SESSION['theme'] == 'default' || !isset($_SESSION['theme']))
 {
-	$embed = "<a class=\"twitter-timeline\" href=\"https://twitter.com/".core::config('twitter_username')."\">Tweets by ".core::config('twitter_username')."</a> <script async src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>";
+	$embed = "<a class=\"twitter-timeline\" data-height=\"500\" href=\"https://twitter.com/".core::config('twitter_username')."\">Tweets by ".core::config('twitter_username')."</a> <script async src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>";
 }
 
 else
 {
-	$embed = "<a class=\"twitter-timeline\" data-theme=\"dark\" href=\"https://twitter.com/".core::config('twitter_username')."\">Tweets by ".core::config('twitter_username')."</a> <script async src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>";
+	$embed = "<a class=\"twitter-timeline\" data-height=\"500\" data-theme=\"dark\" href=\"https://twitter.com/".core::config('twitter_username')."\">Tweets by ".core::config('twitter_username')."</a> <script async src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>";
 }
 
 $templating->set('embed', $embed);

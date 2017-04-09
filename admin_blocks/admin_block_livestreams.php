a7392414328cfbc1fe8b59e2d8b98624a93f7043
liam dawe <liamdawe@gmail.com>
2017-04-09 23:14:07 +0100

    dont hard-code livestream stuff into admin main menu, create block for it

---
+++ admin_blocks/admin_block_livestreams.php
@@ -0,0 +1,16 @@
+<?php
+$templating->merge('admin_blocks/admin_block_livestreams');
+$templating->block('main');
+
+$db->sqlquery("SELECT COUNT(`row_id`) as counter FROM `livestreams` WHERE `accepted` = 0");
+$livestream_counter = $db->fetch();
+
+if ($livestream_counter['counter'] == 0)
+{
+	$livestream_indicator = '(0)';
+}
+else if ($livestream_counter['counter'] > 0)
+{
+	$livestream_indicator = '<span class="badge badge-important">'.$livestream_counter['counter'].'</span>';
+}
+$templating->set('livestream_indicator', $livestream_indicator);

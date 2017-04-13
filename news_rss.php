5d2ddfee2b24be6e4a2d3abd3fb5c2b7ca52f2bc
liam dawe <liamdawe@gmail.com>
2017-04-13 14:03:29 +0100

    remove really old redirect, people should have updated by now

--- news_rss.php
+++
@@ -1,6 +0,0 @@
-<?php
-header($_SERVER['SERVER_PROTOCOL'] . ' 301 Moved Permanently');
-header('Status: 301 Moved Permanently');
-header('Location: http://www.gamingonlinux.com/article_rss.php');
-exit();
-?>

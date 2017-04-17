7afe268b66ce3075be1017028cf82e85beddae21
liam dawe <liamdawe@gmail.com>
2017-04-17 15:35:38 +0100

    new plugin for showing distro icon next to username, stop hard-coding it

---
+++ plugins/distro_username/plugin_distro_username.php
@@ -0,0 +1,11 @@
+<?php
+plugins::register_hook('into_post_username', 'distro_in_username');
+
+function hook_distro_in_username($user_info)
+{
+	if (!empty($user_info['distro']) && $user_info['distro'] != 'Not Listed')
+	{
+		$into_username .= '<img title="' . $user_info['distro'] . '" class="distro tooltip-top"  alt="" src="' . core::config('website_url') . 'templates/'.core::config('template').'/images/distros/' . $user_info['distro'] . '.svg" />';
+	}
+	return $into_username;
+}

34f4616b49940b282ab4b08f4fd4d77d4f6ea0a2
liam dawe <liamdawe@gmail.com>
2017-04-18 21:06:04 +0100

    dont force default template

--- includes/ajax/gallery_tagline.php
+++ includes/ajax/gallery_tagline.php
@@ -9,7 +9,7 @@ $db = new mysql(core::$database['host'], core::$database['username'], core::$dat
 
 include($file_dir . '/includes/class_template.php');
 
-$templating = new template('default');
+$templating = new template(core::config('template'));
 
 $templating->load('/admin_modules/gallery_tagline');
 

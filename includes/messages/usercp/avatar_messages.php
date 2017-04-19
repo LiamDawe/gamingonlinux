a3d346515b9ee8c7531935fe1bd000d6ca922062
liam dawe <liamdawe@gmail.com>
2017-04-19 23:24:26 +0100

    missing message file

---
+++ includes/messages/usercp/avatar_messages.php
@@ -0,0 +1,23 @@
+<?php
+return [
+	"too_big" => 
+	[
+		"text" => "The file size was too big!",
+		"error" => 1
+	],
+	"not_image" =>
+	[
+		"text" => "That was not an image!",
+		"error" => 1
+	],
+	"cant_upload" =>
+	[
+		"text" => "We couldn't upload the file!",
+		"error" => 1
+	],
+	"no_file" =>
+	[
+		"text" => "No file selected to upload, dummy!",
+		"error" => 1
+	]
+];

180e8825f7547282a50b5219120f9ad0ace89eb1
liam dawe <liamdawe@gmail.com>
2017-04-20 11:34:17 +0100

    new plugin to grab latest LOL article

---
+++ plugins/latest_lifeonlinux_article/plugin_latest_lifeonlinux_article.php
@@ -0,0 +1,26 @@
+<?php
+plugins::register_hook('top_of_home_hook', 'latest_rss_item');
+
+function hook_latest_rss_item($article_id)
+{
+	global $db, $templating;
+	
+	$templating->merge_plugin('latest_lifeonlinux_article/template');
+	$rss_block = $templating->block_store('main', 'latest_lifeonlinux_article/template');
+	
+	$content = core::file_get_contents_curl('https://www.lifeonlinux.com/article_rss.php');
+    $x = new SimpleXmlElement($content);
+	
+	$count_items = 0;
+    foreach($x->channel->item as $entry) 
+    {
+		if($count_items == 1 ) 
+		{
+			break;
+		} 
+        $rss_block = $templating->store_replace($rss_block, ['link' => $entry->link, 'title' => $entry->title]);
+        $count_items++;
+    }
+
+    return $rss_block;
+}

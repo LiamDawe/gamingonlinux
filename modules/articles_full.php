c5990f36216004fb4b58a9c48b5aa2573e93df77
Liam Dawe <liamdawe@gmail.com>
2017-05-22 15:45:58 +0100

    move normal article comments over to phpmailer wrapper

--- modules/articles_full.php
+++ modules/articles_full.php
@@ -1225,8 +1225,8 @@ else if (isset($_GET['go']))
 									// Mail it
 									if ($core->config('send_emails') == 1)
 									{
-										$mail = new mail($email_user['email'], $subject, $html_message, $plain_message);
-										$mail->send();
+										$mail = new mailer($core);
+										$mail->sendMail($email_user['email'], $subject, $html_message, $plain_message);
 									}
 
 									// remove anyones send_emails subscription setting if they have it set to email once

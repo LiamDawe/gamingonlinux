5b2e35314813abcadda99a9c4d8f4d0fb5a0e342
Liam Dawe <liamdawe@gmail.com>
2017-06-28 15:10:55 +0100

    we dont use this

--- tools/make_user.php
+++ tools/make_user.php
@@ -4,7 +4,7 @@ $file_dir = dirname( dirname(__FILE__) );
 $db_conf = include $file_dir . '/includes/config.php';
 
 include($file_dir. '/includes/class_db_mysql.php');
-$dbl = new db_mysql("mysql:host=".$db_conf['host'].";dbname=".$db_conf['database'],$db_conf['username'],$db_conf['password'], $db_conf['table_prefix']);
+$dbl = new db_mysql("mysql:host=".$db_conf['host'].";dbname=".$db_conf['database'],$db_conf['username'],$db_conf['password']);
 
 include($file_dir . '/includes/class_core.php');
 $core = new core($dbl, $file_dir);

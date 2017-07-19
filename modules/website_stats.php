9e6462b71b791ce627a92f5418c9f8d3378ffcea
Liam Dawe <liamdawe@gmail.com>
2017-07-19 19:51:46 +0100

    add top liked articles to stats page

--- modules/website_stats.php
+++ modules/website_stats.php
@@ -127,11 +127,21 @@ $templating->set('hot_articles', $hot_articles);
 $templating->block('top_articles');
 
 $article_list = '';
-// top 10 articles of all time by views
 $query_top = $dbl->run("SELECT `article_id`, `title`, `views` FROM `articles` WHERE `active` = 1 ORDER BY `views` DESC LIMIT 5")->fetch_all();
 foreach ($query_top as $top_articles)
 {
 	$article_list .= '<li><a href="'.$article_class->get_link($top_articles['article_id'], $top_articles['title']).'">'.$top_articles['title'].'</a> ('.number_format($top_articles['views']).')</li>';
 }
 $templating->set('article_list', $article_list);
+
+// most "liked" articles
+$templating->block('most_likes');
+
+$liked_list = '';
+$query_liked = $dbl->run("SELECT COUNT(l.like_id) as `total_likes`, a.`article_id`, a.`title` FROM `article_likes` l INNER JOIN `articles` a ON a.`article_id` = l.`article_id` GROUP BY l.`article_id` ORDER BY `total_likes` DESC LIMIT 5")->fetch_all();
+foreach ($query_liked as $top_liked)
+{
+	$liked_list .= '<li><a href="'.$article_class->get_link($top_liked['article_id'], $top_liked['title']).'">'.$top_liked['title'].'</a> ('.number_format($top_liked['total_likes']).')</li>';
+}
+$templating->set('article_list', $liked_list);
 ?>

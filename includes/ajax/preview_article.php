9a33bc594a3ef6001728ee7fd17e75b030cc0c97
Liam Dawe <liamdawe@gmail.com>
2017-08-01 22:43:26 +0100

    new ajax files for admin sections

---
+++ includes/ajax/preview_article.php
@@ -0,0 +1,160 @@
+<?php
+session_start();
+
+define("APP_ROOT", dirname ( dirname ( dirname(__FILE__) ) ) );
+
+require APP_ROOT . "/includes/bootstrap.php";
+
+$templating = new template($core, $core->config('template'));
+
+$user = new user($dbl, $core);
+
+if(isset($_POST))
+{
+	if (isset($_POST['article_id']) && $_POST['article_id'] > 0)
+	{
+		$check_article_sql = "SELECT
+		a.`title`,
+		a.`tagline`,
+		a.`text`,
+		a.`article_id`,
+		a.`author_id`,
+		a.`guest_username`,
+		a.`tagline_image`,
+		a.`locked`,
+		a.`locked_by`,
+		a.`locked_date`,
+		a.`gallery_tagline`,
+		t.`filename` as `gallery_tagline_filename`,
+		u1.`username`,
+		u1.`user_id`,
+		u2.`username` as username_lock
+		FROM `articles` a
+		LEFT JOIN
+		`users` u1 ON u1.`user_id` = a.`author_id`
+		LEFT JOIN
+		`users` u2 ON a.`locked_by` = u2.`user_id`
+		LEFT JOIN
+		`articles_tagline_gallery` t ON t.`id` = a.`gallery_tagline`
+		WHERE `article_id` = ?";
+		$article = $dbl->run($check_article_sql, array($_POST['article_id']))->fetch();
+		
+		if ($article['locked'] == 0)
+		{
+			$title = $article['title'];
+			$tagline = $article['tagline'];
+			$text = $article['text'];
+		}
+		else
+		{
+			$title = strip_tags($_POST['title']);
+			$tagline = $_POST['tagline'];
+			$text = $_POST['text'];
+		}
+	}
+	else
+	{
+		$title = strip_tags($_POST['title']);
+		$tagline = $_POST['tagline'];
+		$text = $_POST['text'];
+	}
+	
+	$templating->load('admin_modules/admin_module_articles');
+	
+	// make date human readable
+	$date = $core->format_date(core::$date);
+
+	// get the article row template
+	$templating->block('preview_row', 'admin_modules/admin_module_articles');
+	$templating->set('url', $core->config('website_url'));
+
+	$templating->set('categories_list_preview', '<span class="label label-info">Categories Here</span>');
+
+	$templating->set('title', $title);
+	
+	$top_image = '';
+	$top_image_nobbcode='';
+	if (isset($article))
+	{
+		if ($article['author_id'] == 0)
+		{
+			if (empty($article['guest_username']))
+			{
+				$username = 'Guest';
+			}
+
+			else
+			{
+				$username = $article['guest_username'];
+			}
+		}
+
+		// if it's a registered user show their username with a profile link
+		else if (isset($article['username']))
+		{
+			$username = "<a href=\"/profiles/{$article['user_id']}\">{$article['username']}</a>";
+		}
+		
+		if (!empty($article['tagline_image']))
+		{
+			$top_image_nobbcode = "<img src=\"" . $core->config('website_url') . "uploads/articles/tagline_images/thumbnails/{$article['tagline_image']}\" alt=\"[articleimage]\" class=\"imgList\">";
+		}
+		if ($article['gallery_tagline'] > 0 && !empty($article['gallery_tagline_filename']))
+		{
+			$top_image_nobbcode = "<img src=\"" . $core->config('website_url') . "uploads/tagline_gallery/{$article['gallery_tagline_filename']}\" alt=\"[articleimage]\" class=\"imgList\">";
+		}
+	}
+
+	// else we are probably just previewing a new post by an editor
+	else if (!isset($article))
+	{
+		$username = "<a href=\"/profiles/{$_SESSION['user_id']}\">{$_SESSION['username']}</a>";
+	}
+
+	$templating->set('username', $username);
+	
+	$templating->set('date', $date);
+	$templating->set('submitted_date', 'Submitted ' . $date);
+	
+	if (isset($_SESSION['gallery_tagline_id']) && $_SESSION['gallery_tagline_rand'] == $_SESSION['image_rand'])
+	{
+		$db->sqlquery("SELECT `filename` FROM `articles_tagline_gallery` WHERE `id` = ?", array($_SESSION['gallery_tagline_id']));
+		$gallery_image = $db->fetch();
+		$top_image_nobbcode = "<img src=\"" . $core->config('website_url') . "uploads/tagline_gallery/{$gallery_image['filename']}\" alt=\"[articleimage]\" class=\"imgList\">";
+	}
+	if (isset($_SESSION['uploads_tagline']) && $_SESSION['uploads_tagline']['image_rand'] == $_SESSION['image_rand'])
+	{
+		$top_image_nobbcode = '<img src="' . $core->config('website_url') . 'uploads/articles/tagline_images/temp/thumbnails/' . $_SESSION['uploads_tagline']['image_name'] . '" alt="[articleimage]">';
+	}
+	
+	$tagline_bbcode = '';
+	$bbcode_tagline_gallery = NULL;
+	if (isset($_SESSION['uploads_tagline']) && $_SESSION['uploads_tagline']['image_rand'] == $_SESSION['image_rand'])
+	{
+		$tagline_bbcode = '/temp/' . $_SESSION['uploads_tagline']['image_name'];
+	}
+
+	else if (isset($_SESSION['gallery_tagline_rand']) && $_SESSION['gallery_tagline_rand'] == $_SESSION['image_rand'])
+	{
+		$tagline_bbcode = $_SESSION['gallery_tagline_filename'];
+		$bbcode_tagline_gallery = 1;
+	}
+	
+	else if ((!isset($_SESSION['uploads_tagline']) || $_SESSION['uploads_tagline']['image_rand'] != $_SESSION['image_rand']) || (!isset($_SESSION['gallery_tagline_rand']) || $_SESSION['gallery_tagline_rand'] != $_SESSION['image_rand']) && isset($article))
+	{
+		if (!empty($article['tagline_image']))
+		{
+			$tagline_bbcode  = $article['tagline_image'];
+		}
+		if (!empty($article['gallery_tagline']))
+		{
+			$tagline_bbcode = $article['gallery_tagline_filename'];
+			$bbcode_tagline_gallery = 1;
+		}
+	}
+	$templating->set('top_image_nobbcode', $top_image_nobbcode);
+	$templating->set('tagline', $tagline);
+	$templating->set('text_full', $bbcode->parse_bbcode($text, 1, $tagline_bbcode, $bbcode_tagline_gallery));
+	
+	echo $templating->output();
+}
---
+++ includes/ajax/quote_comment.php
@@ -0,0 +1,40 @@
+<?php
+define("APP_ROOT", dirname ( dirname ( dirname(__FILE__) ) ) );
+
+require APP_ROOT . "/includes/bootstrap.php";
+
+if(isset($_POST))
+{
+	if ($_POST['type'] == 'article_comment')
+	{
+		$get_comment = $dbl->run("SELECT c.`comment_text`, u.`username` FROM `articles_comments` c LEFT JOIN `users` u ON u.user_id = c.author_id WHERE c.comment_id = ?", array($_POST['id']))->fetch();
+		
+		echo json_encode(array("result" => 'done', 'username' => $get_comment['username'], 'text' => $get_comment['comment_text']));
+		return;
+	}
+	
+	if ($_POST['type'] == 'forum_topic')
+	{
+		$get_comment = $dbl->run("SELECT t.`topic_text`, u.`username` FROM `forum_topics` t LEFT JOIN `users` u ON u.user_id = t.author_id WHERE t.topic_id = ?", array($_POST['id']))->fetch();
+		
+		echo json_encode(array("result" => 'done', 'username' => $get_comment['username'], 'text' => $get_comment['topic_text']));
+		return;
+	}
+	
+	if ($_POST['type'] == 'forum_reply')
+	{
+		$get_comment = $dbl->run("SELECT r.`reply_text`, u.`username` FROM `forum_replies` r LEFT JOIN `users` u ON u.user_id = r.author_id WHERE r.post_id = ?", array($_POST['id']))->fetch();
+		
+		echo json_encode(array("result" => 'done', 'username' => $get_comment['username'], 'text' => $get_comment['reply_text']));
+		return;
+	}	
+	
+	if ($_POST['type'] == 'pm')
+	{
+		$get_comment = $dbl->run("SELECT m.`message`, u.`username` FROM `user_conversations_messages` m LEFT JOIN `users` u ON u.user_id = m.author_id WHERE m.message_id = ?", array($_POST['id']))->fetch();
+		
+		echo json_encode(array("result" => 'done', 'username' => $get_comment['username'], 'text' => $get_comment['message']));
+		return;
+	}
+}
+?>

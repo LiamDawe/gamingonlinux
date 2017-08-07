d682cf71684eef277844c8d92fa8e44d6081b944
Liam Dawe <liamdawe@gmail.com>
2017-08-07 21:42:00 +0100

    make it smarter, only email if there are articles

--- includes/crons/daily_article_overview.php
+++ includes/crons/daily_article_overview.php
@@ -11,31 +11,33 @@ $endOfDay   = strtotime("midnight today") - 1;
 
 // gather a list of articles in the last day
 $article_list = $dbl->run("SELECT `title`, `article_id` FROM `articles` WHERE `date` >= $beginOfDay AND `date` <= $endOfDay")->fetch_all();
-
-$email_article_list = '';
-$email_article_list_plain = '';
-foreach ($article_list as $article)
+if ($article_list)
 {
-	$link = $article_class->get_link($article['article_id'], $article['title']);
-	$email_article_list .= '<p><a href="'.$link.'">'.$article['title'].'</a></p>';
-	$email_article_list_plain .= $article['title'] .': ' . $link . "\r\n\r\n";
-}
-
-foreach ($email_users as $email)
-{
-	// subject
-	$subject = "Your daily Linux gaming news fix from GamingOnLinux";
-
-	// message
-	$html_message = "<p>Hello <strong>{$email['username']}</strong>,</p>
-	<p>Here is your daily news digest from GamingOnLinux!</p>" . $email_article_list;
-	
-	$plain_message = PHP_EOL."Hello {$email['username']}, here is your daily news digest from GamingOnLinux!" . $email_article_list_plain;
-
-	// Mail it
-	if ($core->config('send_emails') == 1)
+	$email_article_list = '';
+	$email_article_list_plain = '';
+	foreach ($article_list as $article)
 	{
-		$mail = new mailer($core);
-		$mail->sendMail($email['email'], $subject, $html_message, $plain_message);
-	}	
+		$link = $article_class->get_link($article['article_id'], $article['title']);
+		$email_article_list .= '<p><a href="'.$link.'">'.$article['title'].'</a></p>';
+		$email_article_list_plain .= $article['title'] .': ' . $link . "\r\n\r\n";
+	}
+
+	foreach ($email_users as $email)
+	{
+		// subject
+		$subject = "Your daily Linux gaming news fix from GamingOnLinux";
+
+		// message
+		$html_message = "<p>Hello <strong>{$email['username']}</strong>,</p>
+		<p>Here is your daily news digest from GamingOnLinux!</p>" . $email_article_list;
+		
+		$plain_message = PHP_EOL."Hello {$email['username']}, here is your daily news digest from GamingOnLinux!" . $email_article_list_plain;
+
+		// Mail it
+		if ($core->config('send_emails') == 1)
+		{
+			$mail = new mailer($core);
+			$mail->sendMail($email['email'], $subject, $html_message, $plain_message);
+		}	
+	}
 }

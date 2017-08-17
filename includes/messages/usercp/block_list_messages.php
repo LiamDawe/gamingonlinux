c577657d7b841ad0e2c8c5106f6c53de8497c476
Liam Dawe <liamdawe@gmail.com>
2017-08-17 12:41:19 +0100

    user blocking usercp module

---
+++ includes/messages/usercp/block_list_messages.php
@@ -0,0 +1,18 @@
+<?php
+return [
+	"blocked" => 
+	[
+		"text" => "The user %s has now been blocked.",
+		"additions" => 1
+	],
+	"unblocked" =>
+	[
+		"text" => 'The user %s has now been unblocked.',
+		"additions" => 1
+	],
+	"already_blocked" =>
+	[
+		"text" => 'The user %s is already blocked, if you are still seeing their content then you\'ve found a bug!. Please let us know about it.',
+		"additions" => 1
+	],	
+];

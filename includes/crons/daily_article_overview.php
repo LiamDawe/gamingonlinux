98763b636b01fd04efa70c2fd3b98f7efddfcf17
Liam Dawe <liamdawe@gmail.com>
2017-08-21 16:57:58 +0100

    add unsub link for regged users in the mailing list

--- includes/crons/daily_article_overview.php
+++ includes/crons/daily_article_overview.php
@@ -4,7 +4,7 @@ define("APP_ROOT", dirname( dirname( dirname(__FILE__) ) ) );
 require APP_ROOT . "/includes/bootstrap.php";
 
 // gather a list of users who want a daily email of articles
-$email_users = $dbl->run("SELECT `username`, `email` FROM `users` WHERE `email_articles` = 'daily'")->fetch_all();
+$email_users = $dbl->run("SELECT `username`, `email`, `mailing_list_key` FROM `users` WHERE `email_articles` = 'daily'")->fetch_all();
 $guest_subs = $dbl->run("SELECT `email`,`id`,`unsub_key` FROM `mailing_list` WHERE `activated` = 1")->fetch_all();
 
 $beginOfDay = strtotime("midnight yesterday");
@@ -25,14 +25,16 @@ if ($article_list)
 
 	foreach ($email_users as $email)
 	{
+		$unsub_link = $core->config('website_url') . 'index.php?module=mailing_list&type=remove_user&key=' . $email['mailing_list_key'] . '&id=' . $email['id'];
+		
 		// subject
 		$subject = "Your daily Linux gaming news fix from GamingOnLinux";
 
 		// message
 		$html_message = "<p>Hello <strong>{$email['username']}</strong>,</p>
-		<p>Here is your daily news digest from GamingOnLinux!</p>" . $email_article_list . '<p><em>Please be aware we generally post less on weekends as we take a little time off!</em></p>';
+		<p>Here is your daily news digest from GamingOnLinux!</p>" . $email_article_list . '<p><em>Please be aware we generally post less on weekends as we take a little time off!</em></p><p>You can unsubscribe by <a href="'.$unsub_link.'">clicking here</a>.';
 		
-		$plain_message = PHP_EOL."Hello {$email['username']}, here is your daily news digest from GamingOnLinux!" . $email_article_list_plain . PHP_EOL . 'Please be aware we generally post less on weekends as we take a little time off!';
+		$plain_message = PHP_EOL."Hello {$email['username']}, here is your daily news digest from GamingOnLinux!" . $email_article_list_plain . PHP_EOL . 'Please be aware we generally post less on weekends as we take a little time off! You can unsubscribe any time go going here: ' . $unsub_link;
 
 		// Mail it
 		if ($core->config('send_emails') == 1)
@@ -51,7 +53,7 @@ if ($article_list)
 
 		// message
 		$html_message = "<p>Hello,</p>
-		<p>Here is your daily news digest from GamingOnLinux!</p>" . $email_article_list . '<p><em>Please be aware we generally post less on weekends as we take a little time off!</em></p><p>You can unsubscribe any time by just <a href="'.$unsub_link.'">clicking here</a>.';
+		<p>Here is your daily news digest from GamingOnLinux!</p>" . $email_article_list . '<p><em>Please be aware we generally post less on weekends as we take a little time off!</em></p><p>You can unsubscribe any time by just <a href="'.$unsub_link.'">clicking here</a>.</p>';
 		
 		$plain_message = PHP_EOL."Hello, here is your daily news digest from GamingOnLinux!" . $email_article_list_plain . PHP_EOL . 'Please be aware we generally post less on weekends as we take a little time off! You can unsubscribe any time go going here: ' . $unsub_link;
 

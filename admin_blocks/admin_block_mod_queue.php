79f69514349fdaf239691f21f1efd4a05b25ce03
Liam Dawe <liamdawe@gmail.com>
2017-08-23 16:24:21 +0100

    move to new mysql

--- admin_blocks/admin_block_mod_queue.php
+++ admin_blocks/admin_block_mod_queue.php
@@ -1,17 +1,16 @@
 <?php
 $templating->load('admin_blocks/admin_block_mod_queue');
 
-$db->sqlquery("SELECT COUNT(`id`) as count FROM `admin_notifications` WHERE `type` IN('mod_queue','mod_queue_reply', 'mod_queue_comment') AND `completed` = 0");
-$post_counter = $db->fetch();
+$post_counter = $dbl->run("SELECT COUNT(`id`) as count FROM `admin_notifications` WHERE `type` IN('mod_queue','mod_queue_reply', 'mod_queue_comment') AND `completed` = 0")->fetchOne();
 
 $templating->block('main');
 
-if ($post_counter['count'] > 0)
+if ($post_counter > 0)
 {
-	$templating->set('post_counter', "<span class=\"badge badge-important\">{$post_counter['count']}</span>");
+	$templating->set('post_counter', "<span class=\"badge badge-important\">{$post_counter}</span>");
 }
 
-else if ($post_counter['count'] == 0)
+else if ($post_counter == 0)
 {
-	$templating->set('post_counter', "({$post_counter['count']})");
+	$templating->set('post_counter', "(0)");
 }

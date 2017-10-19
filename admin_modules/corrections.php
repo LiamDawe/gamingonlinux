c4f6aacb55f3b4f0dce74227c85ec529071e15b2
liam dawe <liamdawe@gmail.com>
2017-10-19 14:26:07 +0100

    adjust a bit

--- admin_modules/corrections.php
+++ admin_modules/corrections.php
@@ -17,51 +17,50 @@ else if (is_numeric($_GET['page']))
 $templating->block('top', 'admin_modules/corrections');
 
 // count how many there is in total
-$db->sqlquery("SELECT `row_id` FROM `article_corrections`");
-$total_pages = $db->num_rows();
+$total_pages = $dbl->run("SELECT COUNT(`row_id`) FROM `article_corrections`")->fetchOne();
 
 /* get any spam reported comments in a paginated list here */
 $pagination = $core->pagination_link(9, $total_pages, "admin.php?module=corrections", $page);
 
-  $db->sqlquery("SELECT c.row_id, c.`article_id`, c.`user_id`, c.`correction_comment`, u.username, a.title FROM `article_corrections` c LEFT JOIN `users` u ON u.user_id = c.user_id LEFT JOIN `articles` a ON a.article_id = c.article_id ORDER BY c.`row_id` ASC LIMIT ?, 9", array($core->start));
-  if ($db->num_rows() > 0)
-  {
-    while ($corrections = $db->fetch())
+$res = $dbl->run("SELECT c.row_id, c.`article_id`, c.`user_id`, c.`correction_comment`, u.username, a.title FROM `article_corrections` c LEFT JOIN `users` u ON u.user_id = c.user_id LEFT JOIN `articles` a ON a.article_id = c.article_id ORDER BY c.`row_id` ASC LIMIT ?, 9", array($core->start))->fetch_all();
+if ($res)
+{
+	foreach ($res as $corrections)
     {
-      if (empty($corrections['username']))
-      {
-        $username = 'Guest';
-      }
-      else
-      {
-        $username = "<a href=\"/profiles/{$corrections['user_id']}\">{$corrections['username']}</a>";
-      }
+		if (empty($corrections['username']))
+		{
+			$username = 'Guest';
+		}
+		else
+		{
+			$username = "<a href=\"/profiles/{$corrections['user_id']}\">{$corrections['username']}</a>";
+		}
 
-      $nice_title = core::nice_title($corrections['title']);
+		$nice_title = core::nice_title($corrections['title']);
 
-      if ($core->config('pretty_urls') == 1)
-      {
-        $article_link = '/articles/' . $nice_title . '.' . $corrections['article_id'];
-      }
-      else 
-      {
-        $article_link = '/index.php?module=articles_full&aid=' . $corrections['article_id'] . '&title=' . $nice_title;
-      }
+		if ($core->config('pretty_urls') == 1)
+		{
+			$article_link = '/articles/' . $nice_title . '.' . $corrections['article_id'];
+		}
+		else 
+		{
+			$article_link = '/index.php?module=articles_full&aid=' . $corrections['article_id'] . '&title=' . $nice_title;
+		}
 
-      $templating->block('row', 'admin_modules/corrections');
-      $templating->set('username', $username);
-      $templating->set('title', $corrections['title']);
-      $templating->set('article_link', $article_link);
-      $templating->set('correction', $bbcode->parse_bbcode($corrections['correction_comment']));
-      $templating->set('correction_id', $corrections['row_id']);
-    }
+		$templating->block('row', 'admin_modules/corrections');
+		$templating->set('username', $username);
+		$templating->set('title', $corrections['title']);
+    	$templating->set('article_link', $article_link);
+    	$templating->set('correction', $bbcode->parse_bbcode($corrections['correction_comment']));
+		$templating->set('correction_id', $corrections['row_id']);
+	}
 
     $templating->block('bottom', 'admin_modules/corrections');
     $templating->set('pagination', $pagination);
-  }
+}
 else
 {
-  $core->message('Nothing to display! There are no suggestions.');
+	$core->message('Nothing to display! There are no suggestions.');
 }
 
 
@@ -77,10 +76,9 @@ if (isset($_POST['act']) && $_POST['act'] == 'delete')
 
 	else
 	{
-		$db->sqlquery("UPDATE `admin_notifications` SET `completed` = 1, `completed_date` = ? WHERE `type` = 'article_correction' AND `data` = ?", array(core::$date, $_POST['correction_id']));
-		$db->sqlquery("INSERT INTO `admin_notifications` SET `user_id` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?, `type` = ?, `data` = ?", array($_SESSION['user_id'], core::$date, core::$date, 'deleted_correction', $_POST['correction_id']));
-
-		$db->sqlquery("DELETE FROM `article_corrections` WHERE `row_id` = ?", array($_POST['correction_id']));
+		$dbl->run("UPDATE `admin_notifications` SET `completed` = 1, `completed_date` = ? WHERE `type` = 'article_correction' AND `data` = ?", array(core::$date, $_POST['correction_id']));
+		$dbl->run("INSERT INTO `admin_notifications` SET `user_id` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?, `type` = ?, `data` = ?", array($_SESSION['user_id'], core::$date, core::$date, 'deleted_correction', $_POST['correction_id']));
+		$dbl->run("DELETE FROM `article_corrections` WHERE `row_id` = ?", array($_POST['correction_id']));
 
 		$_SESSION['message'] = 'deleted';
 		$_SESSION['message_extra'] = 'correction';

de9eb62410026d86ae174fce4524654d7017851d
liam dawe <liamdawe@gmail.com>
2017-10-25 16:19:19 +0100

    new sql

--- usercp_modules/usercp_module_topic_subscriptions.php
+++ usercp_modules/usercp_module_topic_subscriptions.php
@@ -26,16 +26,15 @@ if (!isset($_GET['go']))
 	$templating->block('main_top', 'usercp_modules/usercp_module_topic_subscriptions');
 
 	// count how many there is in total
-	$db->sqlquery("SELECT s.user_id, t.`topic_id` FROM `forum_topics` t INNER JOIN `forum_topics_subscriptions` s ON t.topic_id = s.topic_id WHERE s.`user_id` = ?", array($_SESSION['user_id']));
-	$total_pages = $db->num_rows();
+	$total_pages = $dbl->run("SELECT COUNT(t.`topic_id`) FROM `forum_topics` t INNER JOIN `forum_topics_subscriptions` s ON t.topic_id = s.topic_id WHERE s.`user_id` = ?", array($_SESSION['user_id']))->fetchOne();
 
 	// sort out the pagination link
 	$pagination = $core->pagination_link(9, $total_pages, "usercp.php?module=topic_subscriptions&", $page);
 
 	// get the posts for this forum
-	$db->sqlquery("SELECT t.*, s.user_id, u.`username`, u.avatar, u.gravatar_email, u.avatar_gravatar, u.avatar_uploaded, u.avatar_gallery, u2.`username` as `username_last`, u2.`user_id` as `user_id_last` FROM `forum_topics` t INNER JOIN `forum_topics_subscriptions` s ON t.topic_id = s.topic_id INNER JOIN `users` u ON t.`author_id` = u.`user_id` LEFT JOIN `users` u2 ON t.`last_post_id` = u2.`user_id` WHERE s.`user_id`= ? ORDER BY t.`last_post_date` DESC LIMIT ?, 9", array($_SESSION['user_id'], $core->start));
+	$post_res = $dbl->run("SELECT t.*, s.user_id, u.`username`, u.avatar, u.gravatar_email, u.avatar_gravatar, u.avatar_uploaded, u.avatar_gallery, u2.`username` as `username_last`, u2.`user_id` as `user_id_last` FROM `forum_topics` t INNER JOIN `forum_topics_subscriptions` s ON t.topic_id = s.topic_id INNER JOIN `users` u ON t.`author_id` = u.`user_id` LEFT JOIN `users` u2 ON t.`last_post_id` = u2.`user_id` WHERE s.`user_id`= ? ORDER BY t.`last_post_date` DESC LIMIT ?, 9", array($_SESSION['user_id'], $core->start))->fetch_all();
 
-	while ($post = $db->fetch())
+	foreach ($post_res as $post)
 	{
 		$templating->block('post_row', 'usercp_modules/usercp_module_topic_subscriptions');
 
@@ -85,13 +84,13 @@ else if (isset($_GET['go']))
 
 			else if (isset($_POST['yes']))
 			{
-				$db->sqlquery("DELETE FROM `forum_topics_subscriptions` WHERE `user_id` = ?", array($_SESSION['user_id']));
+				$dbl->run("DELETE FROM `forum_topics_subscriptions` WHERE `user_id` = ?", array($_SESSION['user_id']));
 				header("Location: /usercp.php?module=topic_subscriptions&message=done");
 			}
 		}
 		else
 		{
-			$db->sqlquery("DELETE FROM `forum_topics_subscriptions` WHERE `user_id` = ? AND `topic_id` = ?", array($_SESSION['user_id'], $_GET['topic_id']));
+			$dbl->run("DELETE FROM `forum_topics_subscriptions` WHERE `user_id` = ? AND `topic_id` = ?", array($_SESSION['user_id'], $_GET['topic_id']));
 
 			header("Location: /usercp.php?module=topic_subscriptions&message=done");
 		}

96065ffdb5a73102062fe05b3d45fbfa6c4df1a2
liam dawe <liamdawe@gmail.com>
2017-10-25 22:46:03 +0100

    wipe notifications and profile info row when deleting user

--- admin_modules/users.php
+++ admin_modules/users.php
@@ -471,10 +471,12 @@ else
 				}
 
 				$dbl->run("DELETE FROM `users` WHERE `user_id` = ?", array($_GET['user_id']));
+				$dbl->run("DELETE FROM `user_profile_info` WHERE `user_id` = ?", [$_GET['user_id']]);
 				$dbl->run("DELETE FROM `forum_topics_subscriptions` WHERE `user_id` = ?", array($_GET['user_id']));
 				$dbl->run("DELETE FROM `articles_subscriptions` WHERE `user_id` = ?", array($_GET['user_id']));
 				$dbl->run("DELETE FROM `user_conversations_info` WHERE `owner_id` = ?", array($_GET['user_id']));
 				$dbl->run("DELETE FROM `user_conversations_participants` WHERE `participant_id` = ?", array($_GET['user_id']));
+				$dbl->run("DELETE FROM `user_notifications` WHERE `owner_id` = ?", [$_GET['user_id']]);
 				$dbl->run("UPDATE `articles_comments` SET `author_id` = 0 WHERE `author_id` = ?", [$_GET['user_id']]);
 				$dbl->run("UPDATE `config` SET `data_value` = (data_value - 1) WHERE `data_key` = 'total_users'");
 				$dbl->run("INSERT INTO `admin_notifications` SET `user_id` = ?, `type` = 'delete_user', `data` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?", array($_SESSION['user_id'], $deleted_info['username'], core::$date, core::$date));

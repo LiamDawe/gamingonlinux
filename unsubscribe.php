30c4240003c7d9731dc7edb8103aead9faceda65
liam dawe <liamdawe@gmail.com>
2017-10-25 14:53:11 +0100

    new mysql

--- unsubscribe.php
+++ unsubscribe.php
@@ -39,7 +39,7 @@ if (isset($_GET['user_id']) && is_numeric($_GET['user_id']) && isset($_GET['emai
 
 			if ($sub_exists)
 			{
-				$db->sqlquery("DELETE FROM `articles_subscriptions` WHERE `user_id` = ? AND `article_id` = ?", array($_GET['user_id'], $_GET['article_id']));
+				$dbl->run("DELETE FROM `articles_subscriptions` WHERE `user_id` = ? AND `article_id` = ?", array($_GET['user_id'], $_GET['article_id']));
 				$_SESSION['message'] = 'unsubscribed';
 				header("Location: home/");
 				die();
@@ -59,7 +59,7 @@ if (isset($_GET['user_id']) && is_numeric($_GET['user_id']) && isset($_GET['emai
 
 			if ($sub_exists)
 			{
-				$db->sqlquery("DELETE FROM `forum_topics_subscriptions` WHERE `user_id` = ? AND `topic_id` = ?", array($_GET['user_id'], $_GET['topic_id']));
+				$dbl->run("DELETE FROM `forum_topics_subscriptions` WHERE `user_id` = ? AND `topic_id` = ?", array($_GET['user_id'], $_GET['topic_id']));
 				$_SESSION['message'] = 'unsubscribed';
 				header("Location: home/");
 				die();

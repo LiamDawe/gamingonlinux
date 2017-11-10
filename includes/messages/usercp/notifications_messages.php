2bd999ae41436d73349aeef32386152fac0a070b
liam dawe <liamdawe@gmail.com>
2017-11-10 14:43:47 +0000

    notifications message file

---
+++ includes/messages/usercp/notifications_messages.php
@@ -0,0 +1,15 @@
+<?php
+return [
+	"mark_all_read" => 
+	[
+		"text" => "All current notifications marked as read!",
+	],
+	"removed_read" =>
+	[
+		"text" => "All read notifications removed!",
+	],
+	"removed_all" =>
+	[
+		"text" => "All notifications removed!",
+	]
+];

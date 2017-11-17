c91a6d3e6cd878d4fffa5323fa683770ba387093
liam dawe <liamdawe@gmail.com>
2017-11-17 14:08:34 +0000

    fanatical importer

---
+++ includes/crons/sales/fanatical_import.php
@@ -0,0 +1,105 @@
+<?php
+error_reporting(-1);
+
+$doc_root = dirname( dirname( dirname( dirname(__FILE__) ) ) );
+
+// we dont need the whole bootstrap
+require $doc_root . '/includes/loader.php';
+include $doc_root . '/includes/config.php';
+$dbl = new db_mysql();
+$core = new core($dbl);
+
+echo "Fanatical importer started on " .date('d-m-Y H:m:s'). "\n";
+
+$date = strtotime(gmdate("d-n-Y H:i:s"));
+
+$url = 'https://api.fanatical.com/api/feed?cc=US&auth=gamingonlinux';
+if (core::file_get_contents_curl($url) == true)
+{
+	// magic
+}
+else
+{
+	$to = $core->config('contact_email');
+	$subject = 'GOL ERROR - Cannot reach Fanatical sales importer';
+
+	// To send HTML mail, the Content-type header must be set
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= "From: GOL - Fanatical Sales Importer <noreply@gamingonlinux.com>\r\n";
+
+	mail($to, $subject, "Could not reach the importer!", $headers);
+	error_log("Couldn't reach the Fanatical sales XML");
+	die('Fanatical XML not available!');
+}
+
+$on_sale = [];
+
+
+$array = json_decode(core::file_get_contents_curl($url), true);
+$count = count($array['games']);
+printf("Page #%d: %d product(s)\n", $page, $count);
+
+// DEBUG: Check whole array
+//print_r($array['data']);
+
+foreach ($array['data'] as $games)
+{
+	if (in_array('linux', $games['operating_systems']) && $games['discount_percent'] > 0 && substr($games['title'], -6) != 'Bundle')
+	{
+		$website = str_replace('https://', '', $games['url']);
+		$current_price =  $games['current_price']['USD'];
+		$original_price = $games['regular_price']['USD'];
+
+		echo $games['title'] . "\n";
+		echo "* Original Price: $". $original_price ."\n";
+		echo "* Price Now: $" . $current_price . "\n";
+
+		$release_date = date('Y-m-d', $games['release_date']);
+
+		// ADD IT TO THE GAMES DATABASE
+		$game_list = $dbl->run("SELECT `id`, `also_known_as` FROM `calendar` WHERE `name` = ?", array($games['title']))->fetch();
+
+		if (!$game_list)
+		{
+			$dbl->run("INSERT INTO `calendar` SET `name` = ?, `date` = ?, `on_sale` = 1", array($games['title'], $release_date));
+
+			// need to grab it again
+			$game_list = $dbl->run("SELECT `id` FROM `calendar` WHERE `name` = ?", array($games['title']))->fetch();
+
+			$game_id = $game_list['id'];
+		}
+		else
+		{
+			// check for a parent game, if this game is also known as something else, and the detected name isn't the one we use
+			$game_id = $game_list['id'];
+			if ($game_list['also_known_as'] != NULL && $game_list['also_known_as'] != 0)
+			{
+				$game_id = $game_list['also_known_as'];
+			}
+
+			$dbl->run("UPDATE `calendar` SET `on_sale` = 1 WHERE `id` = ?", array($game_id));
+		}
+
+		$on_sale[] = $game_id;
+
+		$check_sale = $dbl->run("SELECT 1 FROM `sales` WHERE `game_id` = ? AND `store_id` = 2", array($game_id))->fetch();
+
+		// if it does exist, make sure it's not from Fanatical already
+		if (!$check_sale)
+		{
+			$share_sale = 'http://www.shareasale.com/r.cfm?u=1644082&b=880704&m=66498&urllink='.$website;
+			$dbl->run("INSERT INTO `sales` SET `game_id` = ?, `store_id` = 2, `accepted` = 1, `sale_dollars` = ?, `original_dollars` = ?, `link` = ?", array($game_id, $current_price, $original_price, $share_sale));
+
+			$sale_id = $dbl->new_id();
+
+			echo "\tAdded ".$games['title']." to the sales DB with id: " . $sale_id . ".\n";
+		}
+	}
+}
+// remove any not found on sale
+$in  = str_repeat('?,', count($on_sale) - 1) . '?';
+$dbl->run("DELETE FROM `sales` WHERE `game_id` NOT IN ($in) AND `store_id` = 2", $on_sale);
+
+echo "\n\n";
+echo "End of Fanatical import @ " . date('d-m-Y H:m:s') . ".\nHave a nice day.\n";

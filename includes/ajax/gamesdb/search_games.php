2d490c848480131d3c3674d035ef3a73785a5d49
liam dawe <liamdawe@gmail.com>
2017-11-28 12:48:08 +0000

    new combined ajax file for games db search

---
+++ includes/ajax/gamesdb/search_games.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: application/json');
+
+session_start();
+
+define("APP_ROOT", dirname ( dirname ( dirname ( dirname(__FILE__) ) ) ) );
+
+require APP_ROOT . "/includes/bootstrap.php";
+
+if(isset($_GET['q']))
+{
+	$game_search = '%' . $_GET['q'] . '%';
+
+	if (isset($_GET['sales']))
+	{
+		$sql = "SELECT DISTINCT c.`name` FROM `sales` s INNER JOIN `calendar` c ON c.id = s.game_id WHERE c.`name` LIKE ? ORDER BY c.`name` ASC";
+	}
+	if (isset($_GET['free']))
+	{
+		$sql = "SELECT `name` FROM `calendar` WHERE `name` LIKE ? AND `free_game` = 1 ORDER BY `name` ASC";		
+	}
+
+	$get_data = $dbl->run($sql, [$game_search])->fetch_all();
+
+	$data = [];
+	if($get_data)
+	{
+		foreach ($get_data as $key => $value)
+		{
+			$data[] = array('data' => $value['name']);
+		}
+	}
+
+	echo json_encode($data);
+}
+?>

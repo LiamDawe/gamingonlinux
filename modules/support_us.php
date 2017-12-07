14eec27c3eba0b6bdc464dbfe9dad87f6b8ba8f2
liam dawe <liamdawe@gmail.com>
2017-12-07 20:17:00 +0000

    fix avatars

--- modules/support_us.php
+++ modules/support_us.php
@@ -9,7 +9,7 @@ $templating->set('config_support_us_text', $core->config('support_us_text'));
 $templating->block('list_top');
 
 // get supporter list Sorted by last login
-$res = $dbl->run("SELECT u.`username`, u.`user_id`, u.`supporter_link` FROM `users` u INNER JOIN `user_group_membership` g ON u.user_id = g.user_id WHERE g.group_id = 6 ORDER BY RAND() DESC LIMIT 9")->fetch_all();
+$res = $dbl->run("SELECT u.`username`, u.`user_id`, u.`supporter_link`, u.`avatar`, u.`avatar_gravatar`, u.`gravatar_email`, u.`avatar_uploaded`, u.`avatar_gallery` FROM `users` u INNER JOIN `user_group_membership` g ON u.user_id = g.user_id WHERE g.group_id = 6 ORDER BY RAND() DESC LIMIT 9")->fetch_all();
 $templating->block('list_row_start');
 foreach ($res as $rowuser)
 {
@@ -18,7 +18,7 @@ foreach ($res as $rowuser)
 	$templating->set('user_id', $rowuser['user_id']);
 	$templating->set('username', $rowuser['username']);
 
-	$avatar = $user->sort_avatar($rowuser['user_id']);
+	$avatar = $user->sort_avatar($rowuser);
 	$templating->set('avatarurl', $avatar);
 
 	$supporter_link = '';

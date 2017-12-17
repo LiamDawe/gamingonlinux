311d296daa250dcc807aa18908d3768500624739
liam dawe <liamdawe@gmail.com>
2017-12-17 21:14:49 +0000

    messages file for usercp home

---
+++ includes/messages/usercp/home_messages.php
@@ -0,0 +1,17 @@
+<?php
+return [
+	"youtube-missing" => 
+	[
+		"text" => "That is not a correct Youtube URL format, please use a correct URL like: https://www.youtube.com/gamingonlinux",
+		"error" => 1
+	],
+	"twitch-missing" =>
+	[
+		"text" => "That is not a correct Twitch URL format, please use a correct URL like: http://www.twitch.tv/gamingonlinux",
+		"error" => 1
+	],
+	"profile_updated" =>
+	[
+		"text" => "Profile updated!"
+	]
+];

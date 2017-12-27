a408aceda8b4b561b062aeea7fbc4b14d74c10da
liam dawe <liamdawe@gmail.com>
2017-12-27 20:04:41 +0000

    goty message file

---
+++ includes/messages/goty_messages.php
@@ -0,0 +1,17 @@
+<?php
+// livestreams submission page
+return [ 
+	"game_exists" => [
+		"text" => "That game has already been submitted to that category!",
+		"error" => 1
+	],
+	"goty_added_editor" => [
+		"text" => "You have added that game to the list! It has been auto accepted as you're an Editor!"
+	],
+	"goty_game_submitted" => [
+		"text" => "You have added that game to the list! It will be reviewed by an editor first before it appears, so sit tight!"
+	],
+	"goty_vote_deleted" => [
+		"text" => "Your vote has been removed! You can now vote in that category again."
+	]
+];

75d25190b3ffd20bedc89052587b48afe108b345
liam dawe <liamdawe@gmail.com>
2017-12-27 23:40:07 +0000

    link and counter for submissions

--- admin_blocks/admin_block_games.php
+++ admin_blocks/admin_block_games.php
@@ -12,3 +12,14 @@ else if ($review_count == 0)
 {
 	$templating->set('tag_count', "(0)");
 }
+
+// count any submitted items for review
+$items_count = $dbl->run("SELECT COUNT(*) FROM `calendar` WHERE `approved` = 0")->fetchOne();
+if ($items_count > 0)
+{
+	$templating->set('items_count', "<span class=\"badge badge-important\">$items_count</span>");
+}
+else if ($items_count == 0)
+{
+	$templating->set('items_count', "(0)");
+}

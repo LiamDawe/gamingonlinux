16c844ff7458204a3af8b744587f6021e6899869
liam dawe <liamdawe@gmail.com>
2017-12-28 13:19:03 +0000

    also now count developers/publisher submissions

--- admin_blocks/admin_block_games.php
+++ admin_blocks/admin_block_games.php
@@ -15,11 +15,13 @@ else if ($review_count == 0)
 
 // count any submitted items for review
 $items_count = $dbl->run("SELECT COUNT(*) FROM `calendar` WHERE `approved` = 0")->fetchOne();
-if ($items_count > 0)
+$devs_count = $dbl->run("SELECT COUNT(*) FROM `developers` WHERE `approved` = 0")->fetchOne();
+$total_count = $items_count + $devs_count;
+if ($total_count > 0)
 {
-	$templating->set('items_count', "<span class=\"badge badge-important\">$items_count</span>");
+	$templating->set('items_count', "<span class=\"badge badge-important\">$total_count</span>");
 }
-else if ($items_count == 0)
+else if ($total_count == 0)
 {
 	$templating->set('items_count', "(0)");
 }

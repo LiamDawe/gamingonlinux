b736e8958e89267e9609bc0383d7a34643520e9c
liam dawe <liamdawe@gmail.com>
2017-12-28 01:21:31 +0000

    fix total counter not to count apps

--- free_games.php
+++ free_games.php
@@ -124,7 +124,7 @@ if (isset($_SESSION['message']))
 
 $templating->load('free_games');
 
-$total_free = $dbl->run("SELECT COUNT(*) FROM `calendar` WHERE `free_game` = 1 AND `approved` = 1")->fetchOne();
+$total_free = $dbl->run("SELECT COUNT(*) FROM `calendar` WHERE `free_game` = 1 AND `approved` = 1 AND `is_application` = 0 AND `also_known_as` IS NULL")->fetchOne();
 
 $templating->block('top', 'free_games');
 $templating->set('total', $total_free);

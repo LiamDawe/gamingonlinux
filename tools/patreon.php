61f479cca260c1ceaec76b8f6382d51c9d28afef
liam dawe <liamdawe@gmail.com>
2018-01-09 09:54:47 +0000

    add discord links, add test run ability to patreon script

--- tools/patreon.php
+++ tools/patreon.php
@@ -1,23 +1,30 @@
 <?php
 session_start();
 
+$file_dir = dirname( dirname(__FILE__) );
+	
+include $file_dir . '/includes/config.php';
+
+include($file_dir. '/includes/class_db_mysql.php');
+$dbl = new db_mysql();
+
+include($file_dir . '/includes/class_core.php');
+$core = new core($dbl, $file_dir);
+
+include($file_dir . '/includes/class_mail.php');
+
+include($file_dir . '/includes/class_user.php');
+$user = new user($dbl, $core);
+?>
+<form method="post" action="patreon.php">
+	<label>Send emails?<input type="checkbox" name="emails" <?php if (isset($_POST['emails'])){echo'checked';}?>/></label><br />
+	<label>Test run?<input type="checkbox" name="test_run" <?php if (isset($_POST['test_run'])){echo'checked';}?>/></label><br />
+	<button type="submit" name="go" value="1">Process</button>
+</form>
+
+<?php
 if (isset($_POST['go']))
 {
-	$file_dir = dirname( dirname(__FILE__) );
-	
-	include $file_dir . '/includes/config.php';
-
-	include($file_dir. '/includes/class_db_mysql.php');
-	$dbl = new db_mysql();
-
-	include($file_dir . '/includes/class_core.php');
-	$core = new core($dbl, $file_dir);
-
-	include($file_dir . '/includes/class_mail.php');
-	
-	include($file_dir . '/includes/class_user.php');
-	$user = new user($dbl, $core);
-
 	$csv = array_map('str_getcsv', file('patreon.csv'));
 
 	array_splice($csv, 0, 2);
@@ -27,9 +34,9 @@ if (isset($_POST['go']))
 		$pledge = (float) $line[3];
 
 		// if they pledge at least 5 dollars a month
-		if ($pledge >= 5)
+		if ($pledge >= 4)
 		{
-			$user_info = $dbl->run("SELECT `username`, `user_id` FROM `users` WHERE `email` = ?", array($line[2]))->fetch();
+			$user_info = $dbl->run("SELECT `username`, `user_id` FROM `users` WHERE `email` = ? OR `supporter_email` = ?", array($line[2], $line[2]))->fetch();
 			// it didn't find an account, email them
 			if (!$user_info)
 			{
@@ -46,9 +53,12 @@ if (isset($_POST['go']))
 					Thank you.";
 					
 					$plain_message = "Hello from Liam at GamingOnLinux.com! Thank you for supporting me on Patreon. I have tried to match your Patreon registered email up to a username on the website, but I didn't find anything.  Don't worry, if you already have your GOL Supporter badge you can ignore this email! If you haven't, please reply with your username or email attached to a GOL account. You're likely using a different email address on Patreon to what you use on GOL. Thank you.";
-					
-					$mail = new mailer($core);
-					$mail->sendMail($line[2], 'Thank you for supporting GamingOnLinux, more info may be needed', $html_message, $plain_message, ['name' => 'Liam Dawe', 'email' => 'contact@gamingonlinux.com']);
+											
+					if (!isset($_POST['test_run']))
+					{
+						$mail = new mailer($core);
+						$mail->sendMail($line[2], 'Thank you for supporting GamingOnLinux, more info may be needed', $html_message, $plain_message, ['name' => 'Liam Dawe', 'email' => 'contact@gamingonlinux.com']);
+					}
 
 					echo "Email sent to " . $line[2] . '<br />';
 				}
@@ -62,8 +72,11 @@ if (isset($_POST['go']))
 					echo 'Username: ' . $user_info['username'] . ' ' . $line[2] . ' | Pledge: '. $pledge .'<pre>';
 					print_r($their_groups);
 					echo '</pre>';
-					
-					$dbl->run("INSERT INTO `user_group_membership` SET `user_id` = ?, `group_id` = 6", [$user_info['user_id']]);
+
+					if (!isset($_POST['test_run']))
+					{					
+						$dbl->run("INSERT INTO `user_group_membership` SET `user_id` = ?, `group_id` = 6", [$user_info['user_id']]);
+					}
 					
 					echo "\nGiven Supporter status\n\n";
 				}
@@ -72,11 +85,10 @@ if (isset($_POST['go']))
 					$their_groups = $user->post_group_list([$user_info['user_id']]);
 					if (!in_array(9, $their_groups[$user_info['user_id']]))
 					{
-						echo 'Username: ' . $user_info['username'] . ' ' . $line[2] . ' | Pledge: '. $pledge .'<pre>';
-						print_r($their_groups);
-						echo '</pre>';
-						
-						$dbl->run("INSERT INTO `user_group_membership` SET `user_id` = ?, `group_id` = 9", [$user_info['user_id']]);
+						if (!isset($_POST['test_run']))
+						{
+							$dbl->run("INSERT INTO `user_group_membership` SET `user_id` = ?, `group_id` = 9", [$user_info['user_id']]);
+						}
 						
 						echo "\nGiven Supporter Plus status\n\n";
 					}
@@ -87,7 +99,3 @@ if (isset($_POST['go']))
 	}
 }
 ?>
-<form method="post" action="patreon.php">
-	<label>Send emails?<input type="checkbox" name="emails" /></label><br />
-	<button type="submit" name="go" value="1">Process</button>
-</form>

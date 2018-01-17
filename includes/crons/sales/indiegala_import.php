42c4ca00fdf4049f30b41604fb5c6a2d6ce27576
liam dawe <liamdawe@gmail.com>
2018-01-17 13:04:04 +0000

    indiegala sales importer

---
+++ includes/crons/sales/indiegala_import.php
@@ -0,0 +1,121 @@
+<?php
+error_reporting(E_ALL);
+
+define("APP_ROOT", dirname( dirname( dirname( dirname(__FILE__) ) ) ));
+
+require APP_ROOT . '/includes/bootstrap.php';
+
+$game_sales = new game_sales($dbl, $templating, $user, $core);
+
+echo "IndieGala importer started on " .date('d-m-Y H:m:s'). "\n";
+
+$url = 'https://www.indiegala.com/store_games_rss?sale=true';
+$get_data = core::file_get_contents_curl($url);
+if ($get_data)
+{
+	// magic
+}
+else
+{
+	$to = $core->config('contact_email');
+	$subject = 'GOL ERROR - Cannot reach the IndieGala sales importer';
+
+	// To send HTML mail, the Content-type header must be set
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= "From: GOL Contact Us <noreply@gamingonlinux.com>\r\n";
+
+	mail($to, $subject, "Could not reach the importer!", $headers);
+	error_log("Couldn't reach the IndieGala sales XML");	
+	die('IndieGala XML not available!');
+}
+
+$xml = simplexml_load_string($get_data);
+
+$on_sale = [];
+
+for ($i = 1; $i <= $xml->channel->totalPages; $i++) 
+{
+	if ($i > 1)
+	{
+		$next_page_url = 'https://www.indiegala.com/store_games_rss?sale=true&page='.$i;
+		$get_data = core::file_get_contents_curl($next_page_url);
+		$xml = simplexml_load_string($get_data);	
+	}
+
+	foreach ($xml->channel->browse->item as $game)
+	{
+		$on_linux = 0;
+		foreach ($game->platforms->platform as $platform)
+		{
+			//print_r($platform);
+			if ($platform == 'Linux')
+			{
+				$on_linux = 1;
+			}
+		}
+		if ($on_linux == 1)
+		{
+			// for seeing what we have available
+			/*echo '<pre>';
+			print_r($game);
+			echo '</pre>';
+
+			//for testing output
+			echo 'This is available for Linux!<br />';
+			echo "\n* Starting import of ".$new_title."\n";
+			echo "URL: ", $game->link, "\n";
+			echo "Price USD: ", $game->discountPriceUSD, "\n";
+			echo "Original Price: ", $game->priceUSD, "\n";*/
+			
+			$new_title = html_entity_decode($game->title, ENT_QUOTES);
+			$stripped_title = $game_sales->stripped_title($new_title);
+			$new_title = $game_sales->clean_title($new_title);
+		
+			// first check it exists based on the normal name
+			$game_id = $dbl->run("SELECT `id` FROM `calendar` WHERE `name` = ?", array($new_title))->fetchOne();
+					
+			if (!$game_id)
+			{
+				// not found, checked the stripped name
+				$game_list_stripped = $dbl->run("SELECT `id` FROM `calendar` WHERE `stripped_name` = ?", array($stripped_title))->fetchOne();
+				if (!$game_list_stripped)
+				{
+					$dbl->run("INSERT INTO `calendar` SET `name` = ?, `stripped_name` = ?, `date` = ?, `approved` = 1", array($new_title, $stripped_title, date('Y-m-d'))); // they don't give the release date, just add in today's date, we can fix manually later if/when we need to
+		
+					$game_id = $dbl->new_id();
+				}
+				else
+				{
+					$game_id = $game_list_stripped;
+				}
+			}
+					
+			$on_sale[] = $game_id;
+			
+			$check_sale = $dbl->run("SELECT 1 FROM `sales` WHERE `game_id` = ? AND `store_id` = 3", array($game_id))->fetch();
+		
+			// all sorted out - insert into the sales database
+			if (!$check_sale)
+			{
+				$dbl->run("INSERT INTO `sales` SET `game_id` = ?, `store_id` = 3, `accepted` = 1, `sale_dollars` = ?, `original_dollars` = ?, `sale_pounds` = ?, `original_pounds` = ?, sale_euro = ?, `original_euro` = ?, `link` = ?", array($game_id, $game->discountPriceUSD, $game->priceUSD, $game->discountPriceGBP, $game->priceGBP, $game->discountPriceEUR, $game->priceEUR, $game->link));
+					
+				$sale_id = $dbl->new_id();
+					
+				echo "\tAdded ".$new_title." to the sales DB with id: " . $sale_id . ".\n";
+			}
+		}
+		echo "\n"; //Just a bit of white space here.
+	}
+}
+
+$total_on_sale = count($on_sale);
+
+// remove any not found on sale
+if (isset($total_on_sale) && $total_on_sale > 0)
+{
+	$in  = str_repeat('?,', count($on_sale) - 1) . '?';
+	$dbl->run("DELETE FROM `sales` WHERE `game_id` NOT IN ($in) AND `store_id` = 3", $on_sale);
+}
+
+echo "End of IndieGala import @ " . date('d-m-Y H:m:s') . ".\nHave a nice day.\n";

f6c764e4cd5d08ee01c1e2391bbc117344dc2349
liam dawe <liamdawe@gmail.com>
2018-03-18 23:40:33 +0000

    when removing a user, we need to actually set their forum posts to guests

--- admin_modules/users.php
+++ admin_modules/users.php
@@ -503,6 +503,8 @@ else
 				$dbl->run("DELETE FROM `user_conversations_participants` WHERE `participant_id` = ?", array($_GET['user_id']));
 				$dbl->run("DELETE FROM `user_notifications` WHERE `owner_id` = ?", [$_GET['user_id']]);
 				$dbl->run("UPDATE `articles_comments` SET `author_id` = 0 WHERE `author_id` = ?", [$_GET['user_id']]);
+				$dbl->run("UPDATE `forum_topics` SET `author_id` = 0 WHERE `author_id` = ?", array($_GET['user_id']));
+				$dbl->run("UPDATE `forum_replies` SET `author_id` = 0 WHERE `author_id` = ?", array($_GET['user_id']));
 				$dbl->run("UPDATE `config` SET `data_value` = (data_value - 1) WHERE `data_key` = 'total_users'");
 				$dbl->run("INSERT INTO `admin_notifications` SET `user_id` = ?, `type` = 'delete_user', `data` = ?, `completed` = 1, `created_date` = ?, `completed_date` = ?", array($_SESSION['user_id'], $deleted_info['username'], core::$date, core::$date));
 

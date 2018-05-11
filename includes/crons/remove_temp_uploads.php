7ace5ac3ab507733e49b0f3670bf0a58983d8b24
liam dawe <liamdawe@gmail.com>
2018-05-11 12:54:44 +0100

    remove youtube cache images older than 60 days to save space, these aren't likely to be viewed often at all at this point

--- includes/crons/remove_temp_uploads.php
+++ includes/crons/remove_temp_uploads.php
@@ -72,3 +72,40 @@ if ($handle = opendir($file_dir.'/uploads/articles/tagline_images/temp/thumbnail
     }
     closedir($handle);
 }
+
+/* remove youtube cache images that are old, not likely to be viewed again */
+if ($handle = opendir($file_dir.'/cache/youtube_thumbs/')) 
+{
+    while (false !== ($entry = readdir($handle)))
+		{
+			$full_filename = $file_dir . '/cache/youtube_thumbs/' . $entry;
+			// if the file is older than 60 days
+			if (time()-filemtime($full_filename) > 60 * 24 * 3600)
+			{
+				$types = array('jpg', 'png', 'gif');
+				$image_info = @getimagesize($full_filename); // supress errors, as we don't get if it cant read folders
+				$image_type = $image_info[2];
+				$file_ext = '';
+				if( $image_type == IMAGETYPE_JPEG )
+				{
+					$file_ext = 'jpg';
+				}
+
+				else if( $image_type == IMAGETYPE_GIF )
+				{
+					$file_ext = 'gif';
+				}
+
+				else if( $image_type == IMAGETYPE_PNG )
+				{
+					$file_ext = 'png';
+				}
+				// if we managed to read it, it's an image, if the file extension matches (to be 100% certain it's an image) we can delete it
+				if (in_array($file_ext, $types))
+				{
+					unlink($full_filename);
+				}
+			}
+    }
+    closedir($handle);
+}

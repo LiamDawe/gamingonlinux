5b885f681a3bc236f35b3b43573c7b10c2678ccd
liam dawe <liamdawe@gmail.com>
2018-05-21 21:49:29 +0100

    new sales block

---
+++ blocks/block_sales.php
@@ -0,0 +1,24 @@
+<?php
+// Article categorys block
+$templating->load('blocks/block_sales');
+$templating->block('block');
+
+// bundles
+$total_bundles = $dbl->run("SELECT COUNT(*) FROM `sales_bundles` WHERE `approved` = 1 AND `end_date` > NOW()")->fetchOne();
+
+$bundles_text = '';
+if ($total_bundles > 0)
+{
+	$bundles_text = '<li class="list-group-item"><a href="/sales/">Bundles: '.$total_bundles.'</a></li>';
+}
+$templating->set('bundles', $bundles_text);
+
+// normal sales
+$total_sales = $dbl->run("SELECT COUNT(*) FROM `sales` WHERE `accepted` = 1")->fetchOne();
+
+$sales_text = '';
+if ($total_sales > 0)
+{
+	$sales_text = '<li class="list-group-item"><a href="/sales/">Games: '.$total_sales.'</a></li>';
+}
+$templating->set('games', $sales_text);

ab53124ee56a74fbbc9797b8fefc538f6cd1691d
liam dawe <liamdawe@gmail.com>
2018-05-25 09:50:38 +0100

    adjust pc info cron wording

--- includes/crons/update_pc_info.php
+++ includes/crons/update_pc_info.php
@@ -8,10 +8,14 @@ $title = "Reminder: Update your PC info for the next round of statistics updates
 $tagline = "This is your once a month reminder to make sure your PC information is correct on your user profiles. A fresh batch of statistics is generated on the 1st of each month.";
 
 $text = "<p>This is your once a month reminder to make sure your PC information is correct on your user profiles. A fresh batch of statistics is generated on the 1st of each month.</p>
-[pcinfo]
-<p>You can see the statistics any time <a href=\"https://www.gamingonlinux.com/users/statistics\">on this page</a>.</p>
-<p>PC Info is automatically purged if it hasn't been updated, or if you don't click the link to remain in for 2 years. This way we prevent too much stale data and don't hold onto your data for longer than required.</p>
-<p>If you want your details to actually be included in the monthly survey, be sure to tick the box labelled \"Include your PC details in our Monthly User Statistics?\" and hit the \"Update\" button at the bottom, it's opt-in by default for new users.";
+
+<p>[pcinfo]</p>
+
+<p>You can see the statistics any time <a href=\"https://www.gamingonlinux.com/users/statistics\" target=\"_blank\">on this page</a>.</p>
+
+<p>If you want your details to actually be included in the monthly survey, be sure to <a href=\"https://www.gamingonlinux.com/usercp.php?module=pcinfo\" target=\"_blank\">head here</a> and tick the box labelled &quot;Include your PC details in our Monthly User Statistics?&quot; and hit the &quot;Update&quot; button at the bottom, it&#39;s opt-in and you can uncheck it at any time not to be included in future statistics gathering.</p>
+
+<p>PC Info is automatically purged if it hasn&#39;t been updated, or if you don&#39;t click the link to remain in for 2 years. This way we prevent too much stale data and don&#39;t hold onto your data for longer than required.</p>";
 
 $slug = core::nice_title($title);
 

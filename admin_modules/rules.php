fab43548cea6066b552baafd005d9787b9408272
liam dawe <liamdawe@gmail.com>
2018-06-30 23:56:36 +0100

    actual admin module to edit the rules

---
+++ admin_modules/rules.php
@@ -0,0 +1,47 @@
+<?php
+define("POPULAR_COUNTER_DEFAULT", 1000);
+
+if (!$user->check_group(1))
+{
+	$core->message("You do not have permission to access this page!");
+}
+
+else
+{
+	if (!isset($_POST['Submit']))
+	{
+		$templating->load('admin_modules/rules');
+
+		$templating->block('main');
+		$templating->set('form_url', $core->config('website_url'));
+
+		$core->article_editor(['content' => $core->config('rules')]);
+
+		$templating->block('bottom', 'admin_modules/config');
+	}
+
+	// We have been asked to edit the config
+	else if (isset($_POST['Submit']))
+	{
+		$rules = trim($_POST['text']);
+		// check empty
+		if (empty($rules))
+		{
+			$_SESSION['message'] = 'empty';
+			$_SESSION['message_extra'] = 'rules text';
+			header("Location: /admin.php?module=rules");
+			die();
+		}
+
+		// do the update
+		else
+		{
+			$core->set_config($rules, 'rules');
+
+			$_SESSION['message'] = 'edited';
+			$_SESSION['message_extra'] = 'site rules';
+			header('Location: '.$core->config('website_url').'admin.php?module=rules');
+		}
+	}
+}
+?>

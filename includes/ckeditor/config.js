c038f3fb28b1e8a61a0ba798a18566bb266ecc77
liam dawe <liamdawe@gmail.com>
2018-09-18 15:11:09 +0100

    don't disable browser spelling checker

--- includes/ckeditor/config.js
+++ includes/ckeditor/config.js
@@ -30,6 +30,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
+	// i find it quite annoying ckeditor disables the browser spell checker by default
+	config.disableNativeSpellChecker = false;
+
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 

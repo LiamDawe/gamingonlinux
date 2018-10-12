818bcb4c8a8ee56c4722e5dbcff501a8241ca0f7
liam dawe <liamdawe@gmail.com>
2018-10-12 13:16:05 +0100

    crowdfunder tracking page, experiment atm

---
+++ modules/crowdfunders.php
@@ -0,0 +1,36 @@
+<?php
+$templating->set_previous('title', 'Crowdfunded Linux games', 1);
+$templating->set_previous('meta_description', 'Crowdfunded Linux games', 1);
+
+$total = $dbl->run("SELECT COUNT(*) FROM `kickstarters` ORDER BY `name` ASC")->fetchOne();
+$total_failed = $dbl->run("SELECT COUNT(*) FROM `kickstarters` WHERE `failed_linux` = 1 ORDER BY `name` ASC")->fetchOne();
+
+$templating->load('crowdfunders');
+$templating->block('list_top');
+$templating->set('total', $total);
+$templating->set('total_failed', $total_failed);
+
+$percentage = round($total_failed/$total*100);
+
+$templating->set('percentage', $percentage . '%');
+
+$crowdfunders = $dbl->run("SELECT * FROM `kickstarters` ORDER BY `name` ASC")->fetch_all();
+
+foreach ($crowdfunders as $item)
+{
+	$templating->block('row');
+	$templating->set('name', $item['name']);
+	$templating->set('link', $item['link']);
+	$failed = '';
+	if ($item['failed_linux'] == 1)
+	{
+		$failed = 'Failed.';
+	}
+	else if ($item['failed_linux'] == 0)
+	{
+		$failed = 'Linux build released.';
+	}
+	$templating->set('linux_status', $failed);
+}
+
+$templating->block('bottom');

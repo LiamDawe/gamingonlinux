859111549b40875b34f1fbea67f69f3f8d3a081e
liam dawe <liamdawe@gmail.com>
2019-01-12 13:49:47 +0000

    prevent XSS

--- includes/ajax/call_profile.php
+++ includes/ajax/call_profile.php
@@ -5,9 +5,16 @@ define("APP_ROOT", dirname ( dirname ( dirname(__FILE__) ) ) );
 
 require APP_ROOT . "/includes/bootstrap.php";
 
-if(isset($_GET['user_id']))
+$user_id = strip_tags($_GET['user_id']);
+
+if (!is_numeric($user_id))
 {
-	$grab_fields = $dbl->run("SELECT `username`, `pc_info_public`, `distro` FROM `users` WHERE `user_id` = ?", array($_GET['user_id']))->fetch();
+	die('The user ID has to be a number!');
+}
+
+if(isset($user_id))
+{
+	$grab_fields = $dbl->run("SELECT `username`, `pc_info_public`, `distro` FROM `users` WHERE `user_id` = ?", array($user_id))->fetch();
 	if (!$grab_fields)
 	{
 		$core->message('That person does not exist here!');
@@ -19,10 +26,10 @@ if(isset($_GET['user_id']))
 			$templating->load('profile');
 			$templating->block('additional');
 			$templating->set('username', $grab_fields['username']);
-			$templating->set('profile_link', '/profiles/' . $_GET['user_id']);
+			$templating->set('profile_link', '/profiles/' . $user_id);
 
 			$fields_output = '';
-			$pc_info = $user->display_pc_info($_GET['user_id'], $grab_fields['distro']);
+			$pc_info = $user->display_pc_info($user_id, $grab_fields['distro']);
 			if ($pc_info['counter'] > 0)
 			{
 				foreach ($pc_info as $k => $info)
@@ -41,12 +48,12 @@ if(isset($_GET['user_id']))
 
 			$templating->block('view_full');
 			$templating->set('stats_link', "/users/statistics");
-			$templating->set('profile_link', '/profiles/' . $_GET['user_id']);
+			$templating->set('profile_link', '/profiles/' . $user_id);
 
 			$edit_link = '';
-			if (isset($_GET['user_id']))
+			if (isset($user_id))
 			{
-				if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $_GET['user_id'])
+				if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $user_id)
 				{
 					$edit_link = ' | <a href="/usercp.php?module=pcinfo">Edit your PC info</a>';
 				}

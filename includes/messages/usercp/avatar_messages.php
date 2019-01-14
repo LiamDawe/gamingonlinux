1aae88ab1fc4514e81ebc3ae76c882da1b8ee6db
liam dawe <liamdawe@gmail.com>
2019-01-14 15:06:18 +0000

    new modern messages for usercp avatar page

--- includes/messages/usercp/avatar_messages.php
+++ includes/messages/usercp/avatar_messages.php
@@ -19,5 +19,18 @@ return [
 	[
 		"text" => "No file selected to upload, dummy!",
 		"error" => 1
+	],
+	"no_gallery_picked" =>
+	[
+		"text" => "You tried to set an avatar from the gallery, but you didn't pick one!",
+		"error" => 1
+	],
+	"gallery_picked" =>
+	[
+		"text" => "Your new avatar has been set from the gallery, don't you look nice!"
+	],
+	"uploaded" =>
+	[
+		"text" => "Your shiny new avatar has been uploaded, how fancy!"
 	]
 ];

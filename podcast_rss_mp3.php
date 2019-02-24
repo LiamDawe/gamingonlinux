5647fae7d92d70ae6c3a019e644ac72a22137b80
liam dawe <liamdawe@gmail.com>
2019-02-24 16:54:16 +0000

    podcast image, fixed up podcast rss feeds

--- podcast_rss_mp3.php
+++ podcast_rss_mp3.php
@@ -1,14 +1,14 @@
 <?php
 $file_dir = dirname(__FILE__);
 
-$db_conf = include $file_dir . '/includes/config.php';
 define('golapp', TRUE);
 
-include($file_dir. '/includes/class_db_mysql.php');
-$dbl = new db_mysql("mysql:host=".$db_conf['host'].";dbname=".$db_conf['database'],$db_conf['username'],$db_conf['password'], $db_conf['table_prefix']);
-
-include($file_dir . '/includes/class_core.php');
-$core = new core($dbl, $file_dir);
+// we dont need the whole bootstrap
+require dirname(__FILE__) . "/includes/loader.php";
+include dirname(__FILE__) . '/includes/config.php';
+$dbl = new db_mysql();
+$core = new core($dbl);
+$bbcode = new bbcode($dbl, $core, $user);
 
 header("Content-Type: application/rss+xml");
 header("Cache-Control: max-age=3600");
@@ -17,7 +17,7 @@ $output = '<?xml version="1.0" encoding="UTF-8"?>
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom" xml:lang="en-GB">
 
 <channel>
-<title>Gaming On Linux Podcast</title>
+<title>Gaming On Linux Podcasts</title>
 <link>https://www.gamingonlinux.com/podcast_rss_mp3.php</link>
 <atom:link href="https://www.gamingonlinux.com/podcast_rss_mp3.php" rel="self" type="application/rss+xml"/>
 <language>en</language>
@@ -44,15 +44,17 @@ foreach ($articles as $line)
 	$title = str_replace("&", "&amp;", $title);
 	$title = $title;
 
+	preg_match("/<a href=\"(.+\.mp3)/m", $line['text'], $matches);
 
-	preg_match("/\[mp3](.+?)\[\/mp3\]/is", $line['text'], $matches);
+	$size = filesize($file_dir . str_replace("https://www.gamingonlinux.com",'',$matches[1]));
+
 	if (isset($matches[1]))
 	{
 		$output .= "
 		<item>
 		<title>$title</title>
 		<description>{$line['tagline']}</description>
-		<enclosure url=\"{$matches[1]}\" type=\"audio/mpeg\" />
+		<enclosure url=\"{$matches[1]}\" type=\"audio/mpeg\" length=\"$size\" />
 		<pubDate>$date</pubDate>
 		<guid>https://www.gamingonlinux.com/$nice_title.{$line['article_id']}/</guid>
 		</item>";

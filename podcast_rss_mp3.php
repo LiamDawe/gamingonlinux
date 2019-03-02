d45c196d168d4aee5e0a4fe327ffd21a2a136e12
liam dawe <liamdawe@gmail.com>
2019-03-02 19:46:32 +0000

    rss spec doesn't like https

--- podcast_rss_mp3.php
+++ podcast_rss_mp3.php
@@ -11,21 +11,21 @@ $core = new core($dbl);
 $bbcode = new bbcode($dbl, $core, $user);
 
 header("Content-Type: application/rss+xml");
-header("Cache-Control: max-age=3600");
+header("Cache-Control: no-cache, must-revalidate");
 
 $output = '<?xml version="1.0" encoding="UTF-8"?>
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom" xml:lang="en-GB">
 
 <channel>
 <title>Gaming On Linux Podcasts</title>
-<link>https://www.gamingonlinux.com/podcast_rss_mp3.php</link>
-<atom:link href="https://www.gamingonlinux.com/podcast_rss_mp3.php" rel="self" type="application/rss+xml"/>
+<link>http://www.gamingonlinux.com/</link>
+<atom:link href="http://www.gamingonlinux.com/podcast_rss_mp3.php" rel="self" type="application/rss+xml"/>
 <language>en</language>
 <description>Linux gaming chat and general banter</description>
 <image>
-<url>https://www.gamingonlinux.com/templates/default/images/favicons/apple-touch-icon-144x144.png</url>
-<title>Gaming On Linux Podcasts</title>
-<link>https://www.gamingonlinux.com/</link>
+	<url>http://www.gamingonlinux.com/templates/default/images/favicons/apple-touch-icon-144x144.png</url>
+	<title>Gaming On Linux Podcasts</title>
+	<link>http://www.gamingonlinux.com/</link>
 </image>
 <itunes:author>gamingonlinux.com</itunes:author>
 <itunes:summary>Talking about the latest games for Linux and general chat about Linux and gaming</itunes:summary>
@@ -52,6 +52,7 @@ foreach ($articles as $line)
 	preg_match("/<a href=\"(.+\.mp3)/m", $line['text'], $matches);
 
 	$size = filesize($file_dir . str_replace("https://www.gamingonlinux.com",'',$matches[1]));
+	$item_url = str_replace("https",'http',$matches[1]);
 
 	if (isset($matches[1]))
 	{
@@ -59,9 +60,9 @@ foreach ($articles as $line)
 		<item>
 		<title>$title</title>
 		<description>{$line['tagline']}</description>
-		<enclosure url=\"{$matches[1]}\" type=\"audio/mpeg\" length=\"$size\" />
+		<enclosure url=\"$item_url\" type=\"audio/mpeg\" length=\"$size\" />
 		<pubDate>$date</pubDate>
-		<guid>https://www.gamingonlinux.com/articles/$nice_title.{$line['article_id']}/</guid>
+		<guid>http://www.gamingonlinux.com/articles/$nice_title.{$line['article_id']}/</guid>
 		</item>";
 	}
 }

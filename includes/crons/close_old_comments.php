dce23fd129bcbffb3362c097d2507eb42b70330f
liam dawe <liamdawe@gmail.com>
2019-05-25 18:08:16 +0100

    move to newer mailer class

--- includes/crons/close_old_comments.php
+++ includes/crons/close_old_comments.php
@@ -7,20 +7,18 @@ $timeout = 31536000; // 1 year
 
 $stamp = time() - $timeout;
 
-$closed = '';
+$html_message = "Comments closed on these articles:<br />";
+$plain_message = "Comments closed on these articles:\r\n";
 $closing = $dbl->run("SELECT `title` FROM `articles` WHERE `date` < ? AND `comments_open` = 1", array($stamp))->fetch_all();
 foreach ($closing as $to_close)
 {
-	$closed .= $to_close['title'] . '<br />';
+	$html_message .= $to_close['title'] . '<br />';
+	$plain_message .= $to_close['title'] . "\r\n";
 }
 
 $dbl->run("UPDATE `articles` SET `comments_open` = 0 WHERE `date` < ?", array($stamp));
 
 $subject = 'GamingOnLinux CRON - Comments Closed';
 
-// To send HTML mail, the Content-type header must be set
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= "From: GOL Contact Us <noreply@gamingonlinux.com>\r\n";
-
-mail($core->config('contact_email'), $subject, "Comments closed on these articles: " . $closed, $headers);
+$mail = new mailer($core);
+$mail->sendMail($core->config('contact_email'), $subject, $html_message, $plain_message);

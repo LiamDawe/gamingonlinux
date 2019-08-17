1fe77cb0a278fc6b63ac44d4b8d852cdd55455ed
Liam Dawe <liamdawe@gmail.com>
2019-08-17 18:19:37 +0100

    update about us page to use author mugshots, plus add a quick view articles link to each bio

--- modules/about_us.php
+++ modules/about_us.php
@@ -6,48 +6,48 @@ if(!defined('golapp'))
 $templating->set_previous('title', 'About Us', 1);
 $templating->set_previous('meta_description', 'About Us information for GamingOnLinux', 1);
 
-if ($filecache->check_cache('about_us', 604800)) // 1 week cache, almost never needed to update
+$templating->load('about_us');
+$templating->block('top');
+
+$get_editors = $dbl->run("SELECT u.`username`, u.`user_id`, u.`supporter_link`, u.`avatar`, u.`avatar_uploaded`, u.`avatar_gallery`, u.`article_bio`, u.`author_picture` FROM `users` u INNER JOIN `user_group_membership` g ON u.user_id = g.user_id WHERE u.`user_id` != 1844 AND g.group_id = 1 OR g.group_id = 2 ORDER BY u.`user_id` = 1 DESC, u.`username` ASC")->fetch_all();
+
+foreach ($get_editors as $editors)
 {
-	$templating->get_cache('about_us');
-}
-else
-{
-	echo $templating->output();
-	$filecache->init();
-
-	$templating->load('about_us');
-	$templating->block('top');
-
-	$get_editors = $dbl->run("SELECT u.`username`, u.`user_id`, u.`supporter_link`, u.`avatar`, u.`avatar_uploaded`, u.`avatar_gallery`, u.`article_bio` FROM `users` u INNER JOIN `user_group_membership` g ON u.user_id = g.user_id WHERE u.`user_id` != 1844 AND g.group_id = 1 OR g.group_id = 2 ORDER BY u.`user_id` = 1 DESC, u.`username` ASC")->fetch_all();
-
-	foreach ($get_editors as $editors)
+	if (!empty($editors['article_bio']))
 	{
-		if (!empty($editors['article_bio']))
+		$templating->block('row');
+		$templating->set('user_id', $editors['user_id']);
+		$templating->set('username', $editors['username']);
+		$templating->set('bio', $bbcode->parse_bbcode($editors['article_bio']));
+		$author_pic = '/uploads/avatars/no_avatar.png';
+		if (isset($editors['author_picture']) && !empty($editors['author_picture']))
 		{
-			$templating->block('row');
-			$templating->set('user_id', $editors['user_id']);
-			$templating->set('username', $editors['username']);
-			$templating->set('bio', $bbcode->parse_bbcode($editors['article_bio']));
+			$author_pic = '/uploads/avatars/author_pictures/'.$editors['author_picture'];
 		}
+		$templating->set('author_picture', $author_pic);
 	}
+}
 
-	$templating->block('contributors_top');
+$templating->block('contributors_top');
 
-	$get_editors = $dbl->run("SELECT u.`username`, u.`user_id`, u.`supporter_link`, u.`avatar`, u.`avatar_uploaded`, u.`avatar_gallery`, u.`article_bio` FROM `users` u INNER JOIN `user_group_membership` g ON u.user_id = g.user_id WHERE g.group_id = 5 ORDER BY u.`username` ASC")->fetch_all();
+$get_editors = $dbl->run("SELECT u.`username`, u.`user_id`, u.`supporter_link`, u.`avatar`, u.`avatar_uploaded`, u.`avatar_gallery`, u.`article_bio`, u.`author_picture` FROM `users` u INNER JOIN `user_group_membership` g ON u.user_id = g.user_id WHERE g.group_id = 5 ORDER BY u.`username` ASC")->fetch_all();
 	
-	foreach ($get_editors as $editors)
+foreach ($get_editors as $editors)
+{
+	if (!empty($editors['article_bio']))
 	{
-		if (!empty($editors['article_bio']))
+		$templating->block('row');
+		$templating->set('user_id', $editors['user_id']);
+		$templating->set('username', $editors['username']);
+		$templating->set('bio', $bbcode->parse_bbcode($editors['article_bio']));
+		$author_pic = '/uploads/avatars/no_avatar.png';
+		if (isset($editors['author_picture']) && !empty($editors['author_picture']))
 		{
-			$templating->block('row');
-			$templating->set('user_id', $editors['user_id']);
-			$templating->set('username', $editors['username']);
-			$templating->set('bio', $bbcode->parse_bbcode($editors['article_bio']));
+			$author_pic = '/uploads/avatars/author_pictures/'.$editors['author_picture'];
 		}
+		$templating->set('author_picture', $author_pic);
 	}
-
-	echo $templating->output();
-
-	$filecache->write('about_us');
 }
+
+echo $templating->output();
 ?>

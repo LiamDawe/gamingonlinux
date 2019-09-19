4e6a3a858e9e4f3dc95ff756b5754a7a9044d698
Liam Dawe <liamdawe@gmail.com>
2019-09-19 22:25:47 +0100

    fix unsubbing from forum post, clean code up a lil bit

--- public_html/unsubscribe.php
+++ public_html/unsubscribe.php
@@ -6,85 +6,85 @@ include(APP_ROOT . '/includes/header.php');
 $user_id = '';
 if (isset($_GET['user_id']))
 {
-	$user_id = $_GET['user_id'];
+	$user_id = (int) $_GET['user_id'];
 }
 $email = '';
 if (isset($_GET['email']))
 {
 	$email = $_GET['email'];
 }
-$article_id = '';
+$post_id = '';
 if (isset($_GET['article_id']))
 {
-	$article_id = $_GET['article_id'];
+	$post_id = (int) $_GET['article_id'];
+}
+else if (isset($_GET['topic_id']))
+{
+	$post_id = (int) $_GET['topic_id'];
+}
+$key = '';
+if (isset($_GET['secret_key']))
+{
+	$key = $_GET['secret_key'];
 }
 
-$empty_check = core::mempty(compact('user_id', 'email', 'article_id'));
+$empty_check = core::mempty(compact('user_id', 'email', 'post_id', 'key'));
 if($empty_check !== true)
 {
-	$_SESSION['message'] = 'cannotunsubscribe';
+	$_SESSION['message'] = 'empty';
+	$_SESSION['message_extra'] = $empty_check;
 	header("Location: home/");
 	die();
 }
 
-if (isset($_GET['user_id']) && is_numeric($_GET['user_id']) && isset($_GET['email']) && isset($_GET['secret_key']))
+$user_exists = $dbl->run("SELECT `email` FROM `users` WHERE `user_id` = ? AND `email` = ?", array($_GET['user_id'], $_GET['email']))->fetchOne();
+if ($user_exists)
 {
-	$user_exists = $dbl->run("SELECT `email` FROM `users` WHERE `user_id` = ? AND `email` = ?", array($_GET['user_id'], $_GET['email']))->fetchOne();
-	if ($user_exists)
+	if (isset($_GET['article_id']) && is_numeric($_GET['article_id']))
 	{
-		if (isset($_GET['article_id']) && is_numeric($_GET['article_id']))
-		{
-			// check secret key
-			$sub_exists = $dbl->run("SELECT `secret_key` FROM `articles_subscriptions` WHERE `user_id` = ? AND `article_id` = ? AND `secret_key` = ?", array($_GET['user_id'], $_GET['article_id'], $_GET['secret_key']))->fetchOne();
+		// check secret key
+		$sub_exists = $dbl->run("SELECT `secret_key` FROM `articles_subscriptions` WHERE `user_id` = ? AND `article_id` = ? AND `secret_key` = ?", array($_GET['user_id'], $_GET['article_id'], $_GET['secret_key']))->fetchOne();
 
-			if ($sub_exists)
-			{
-				$dbl->run("DELETE FROM `articles_subscriptions` WHERE `user_id` = ? AND `article_id` = ?", array($_GET['user_id'], $_GET['article_id']));
-				$_SESSION['message'] = 'unsubscribed';
-				header("Location: home/");
-				die();
-			}
-			else
-			{
-				$_SESSION['message'] = 'cannotunsubscribe';
-				header("Location: home/");
-				die();
-			}
+		if ($sub_exists)
+		{
+			$dbl->run("DELETE FROM `articles_subscriptions` WHERE `user_id` = ? AND `article_id` = ?", array($_GET['user_id'], $_GET['article_id']));
+			$_SESSION['message'] = 'unsubscribed';
+			header("Location: home/");
+			die();
 		}
-
-		else if (isset($_GET['topic_id']) && is_numeric($_GET['topic_id']))
+		else
 		{
-			// check secret key
-			$sub_exists = $dbl->run("SELECT `secret_key` FROM `forum_topics_subscriptions` WHERE `user_id` = ? AND `topic_id` = ? AND `secret_key` = ?", array($_GET['user_id'], $_GET['topic_id'], $_GET['secret_key']))->fetchOne();
-
-			if ($sub_exists)
-			{
-				$dbl->run("DELETE FROM `forum_topics_subscriptions` WHERE `user_id` = ? AND `topic_id` = ?", array($_GET['user_id'], $_GET['topic_id']));
-				$_SESSION['message'] = 'unsubscribed';
-				header("Location: home/");
-				die();
-			}
-			else
-			{
-				$_SESSION['message'] = 'cannotunsubscribe';
-				header("Location: home/");
-				die();
-			}
+			$_SESSION['message'] = 'cannotunsubscribe';
+			header("Location: home/");
+			die();
 		}
 	}
 
-	else
+	else if (isset($_GET['topic_id']) && is_numeric($_GET['topic_id']))
 	{
-		$_SESSION['message'] = 'cannotunsubscribe';
-		header("Location: home/");
-		die();
+		// check secret key
+		$sub_exists = $dbl->run("SELECT `secret_key` FROM `forum_topics_subscriptions` WHERE `user_id` = ? AND `topic_id` = ? AND `secret_key` = ?", array($_GET['user_id'], $_GET['topic_id'], $_GET['secret_key']))->fetchOne();
+
+		if ($sub_exists)
+		{
+			$dbl->run("DELETE FROM `forum_topics_subscriptions` WHERE `user_id` = ? AND `topic_id` = ?", array($_GET['user_id'], $_GET['topic_id']));
+			$_SESSION['message'] = 'unsubscribed';
+			header("Location: home/");
+			die();
+		}
+		else
+		{
+			$_SESSION['message'] = 'cannotunsubscribe';
+			header("Location: home/");
+			die();
+		}
 	}
 }
+
 else
 {
 	$_SESSION['message'] = 'cannotunsubscribe';
 	header("Location: home/");
 	die();
 }
-
 ?>

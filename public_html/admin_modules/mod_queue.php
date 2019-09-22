419b842d6caeca4011f1db829f5924b90efb0104
Liam Dawe <liamdawe@gmail.com>
2019-09-22 14:52:13 +0100

    approving a comment should set the time to the time it was approved, prevent confusing users

--- public_html/admin_modules/mod_queue.php
+++ public_html/admin_modules/mod_queue.php
@@ -245,7 +245,7 @@ if (isset($_POST['action']))
 				// update the news items comment count
 				$dbl->run("UPDATE `articles` SET `comment_count` = (comment_count + 1) WHERE `article_id` = ?", [$approved['article_id']]);
 				
-				$dbl->run("UPDATE `articles_comments` SET `approved` = 1 WHERE `comment_id` = ?", [$_POST['post_id']]);
+				$dbl->run("UPDATE `articles_comments` SET `approved` = 1, `time_posted` = ? WHERE `comment_id` = ?", [core::$date, $_POST['post_id']]);
 				
 				// add 1 to their approval rating
 				$user_get = $dbl->run("SELECT `mod_approved` FROM `users` WHERE `user_id` = ?", [$_POST['author_id']])->fetch();

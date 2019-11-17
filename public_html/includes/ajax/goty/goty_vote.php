ab69577d83ab22c5612e26a683281c1a421c128b
Liam Dawe <liamdawe@gmail.com>
2019-11-17 15:24:27 +0000

    support for multiple votes

--- public_html/includes/ajax/goty/goty_vote.php
+++ public_html/includes/ajax/goty/goty_vote.php
@@ -10,16 +10,26 @@ if($_POST)
 {
 	if ($core->config('goty_voting_open') == 1 && isset($_SESSION['user_id']) && $_SESSION['user_id'] > 0)
 	{
-		$check_vote = $dbl->run("SELECT `user_id` FROM `goty_votes` WHERE `user_id` = ? AND `category_id` = ?", array($_SESSION['user_id'], $_POST['category_id']))->fetchOne();
-		if (!$check_vote)
+		$check_vote = $dbl->run("SELECT `game_id` FROM `goty_votes` WHERE `user_id` = ? AND `category_id` = ?", array($_SESSION['user_id'], $_POST['category_id']))->fetch_all();
+		$total_votes = count($check_vote);
+
+		if (!$check_vote || $total_votes < $core->config('goty_votes_per_category'))
 		{
-			$dbl->run("UPDATE `goty_games` SET `votes` = (votes + 1) WHERE `id` = ?", array($_POST['game_id']));
+			if (!in_array($_POST['game_id'], $check_vote))
+			{
+				$dbl->run("UPDATE `goty_games` SET `votes` = (votes + 1) WHERE `id` = ?", array($_POST['game_id']));
 
-			$dbl->run("UPDATE `config` SET `data_value` = (data_value + 1) WHERE `data_key` = 'goty_total_votes'");
+				$dbl->run("UPDATE `config` SET `data_value` = (data_value + 1) WHERE `data_key` = 'goty_total_votes'");
 
-			$dbl->run("INSERT INTO `goty_votes` SET `user_id` = ?, `game_id` = ?, `category_id` = ?", array($_SESSION['user_id'], $_POST['game_id'], $_POST['category_id']));
-			echo json_encode(array("result" => 1));
-			return;
+				$dbl->run("INSERT INTO `goty_votes` SET `user_id` = ?, `game_id` = ?, `category_id` = ?", array($_SESSION['user_id'], $_POST['game_id'], $_POST['category_id']));
+				echo json_encode(array("result" => 1));
+				return;
+			}
+			else
+			{
+				echo json_encode(array("result" => 4));
+				return;
+			}
 		}
 		else
 		{

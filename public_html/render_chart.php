1ef474b4dd84d46fc8ebac6b1a9bf0fa4f7f5e52
Liam Dawe <liamdawe@gmail.com>
2019-11-28 14:44:03 +0000

    chart render only needs 3 classes, use minimal bootstrap

--- public_html/render_chart.php
+++ public_html/render_chart.php
@@ -1,7 +1,7 @@
 <?php
 define("APP_ROOT", dirname(__FILE__));
 
-require APP_ROOT . "/includes/bootstrap.php";
+require APP_ROOT . "/includes/minimal_bootstrap.php";
 
 $charts = new charts($dbl);
 

258f6c99fa6779be7b0db5c762c71bdca50f4c2c
Liam Dawe <liamdawe@gmail.com>
2019-12-18 17:43:26 +0000

    when closing old comments, remove subs for them - no point

--- crons/close_old_comments.php
+++ crons/close_old_comments.php
@@ -3,22 +3,28 @@ define("APP_ROOT", dirname( dirname(__FILE__) ) . '/public_html');
 
 require APP_ROOT . "/includes/cron_bootstrap.php";
 
-$timeout = 31536000; // 1 year
-
-$stamp = time() - $timeout;
+$stamp = strtotime("-1 year", time());
 
 $html_message = "Comments closed on these articles:<br />";
 $plain_message = "Comments closed on these articles:\r\n";
-$closing = $dbl->run("SELECT `title` FROM `articles` WHERE `date` < ? AND `comments_open` = 1", array($stamp))->fetch_all();
+$id_list = array();
+$closing = $dbl->run("SELECT `article_id`, `title` FROM `articles` WHERE `date` < ? AND `comments_open` = 1", array($stamp))->fetch_all();
 foreach ($closing as $to_close)
 {
+	$id_list[] = $to_close['article_id'];
 	$html_message .= $to_close['title'] . '<br />';
 	$plain_message .= $to_close['title'] . "\r\n";
 }
 
-$dbl->run("UPDATE `articles` SET `comments_open` = 0 WHERE `date` < ?", array($stamp));
+if ($closing)
+{
+	$dbl->run("UPDATE `articles` SET `comments_open` = 0 WHERE `date` < ?", array($stamp));
 
-$subject = 'GamingOnLinux CRON - Comments Closed';
+	$in  = str_repeat('?,', count($id_list) - 1) . '?';
+	$dbl->run("DELETE FROM `articles_subscriptions` WHERE `article_id` IN ($in)", $id_list);
 
-$mail = new mailer($core);
-$mail->sendMail($core->config('contact_email'), $subject, $html_message, $plain_message);
+	$subject = 'GamingOnLinux CRON - Comments Closed';
+
+	$mail = new mailer($core);
+	$mail->sendMail($core->config('contact_email'), $subject, $html_message, $plain_message);
+}

635ec08f9e7b3950481f2ae8d758577633ed2075
Liam Dawe <liamdawe@gmail.com>
2020-01-11 20:28:08 +0000

    mod queue block should also count the upcoming article tag suggestions

--- public_html/admin_blocks/admin_block_mod_queue.php
+++ public_html/admin_blocks/admin_block_mod_queue.php
@@ -5,7 +5,7 @@ if(!defined('golapp'))
 }
 $templating->load('admin_blocks/admin_block_mod_queue');
 
-$post_counter = $dbl->run("SELECT COUNT(`id`) as count FROM `admin_notifications` WHERE `type` IN('mod_queue','mod_queue_reply', 'mod_queue_comment') AND `completed` = 0")->fetchOne();
+$post_counter = $dbl->run("SELECT COUNT(`id`) as count FROM `admin_notifications` WHERE `type` IN('mod_queue','mod_queue_reply', 'mod_queue_comment', 'submitted_article_tag_suggestion') AND `completed` = 0")->fetchOne();
 
 $templating->block('main');
 

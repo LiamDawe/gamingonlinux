14964e11d4133e73c8d051966648192968261162
Liam Dawe <liamdawe@gmail.com>
2020-01-16 16:50:29 +0000

    install tool now actually makes a config file, still mostly useless...

--- public_html/tools/install.php
+++ public_html/tools/install.php
@@ -3,8 +3,8 @@
 		<strong>Database information</strong><br />
 		Database Name: <input type="text" name="db_name" value="" /><br />
 		Database Username: <input type="text" name="db_username" value="" /><br />
-		Database Password: <input type="text" type="text" name="db_password" /><br />
-		Database Host: <input type="text" value="localhost" />
+		Database Password: <input type="password" type="text" name="db_password" /><br />
+		Database Host: <input type="text" name="db_host" value="localhost" />
 	</div>
 	<br />
 	<div>
@@ -16,14 +16,31 @@
 	<div>
 		<strong>Starting configuration</strong>
 		Site name: <input type="text" name="site_title" /><br />
-		Site url:
-		Site path: 
+		Site url: <input type="text" name="site_url" /><br />
+		Site path: <input type="text" name="site_path" /><br />
 	</div>
 	<p><button type="submit" name="go" value="1">Go for launch</button></p>
 </form>
 <?php
 if (isset($_POST['go']))
 {
+	define("APP_ROOT", dirname( dirname(__FILE__) ) );
+	// create config file
+
+	$config_file = fopen(APP_ROOT . '/includes/config.php', 'w');
+
+	$config_content = '<?php
+	define("DB", 
+	[
+		"DB_HOST_NAME" => "'.$_POST['db_host'].'",
+		"DB_USER_NAME" => "'.$_POST['db_username'].'",
+		"DB_PASSWORD" => "'.$_POST['db_password'].'",
+		"DB_DATABASE" => "'.$_POST['db_name'].'"
+	]);';
+
+	fwrite($config_file, $config_content);
+	fclose($config_file);
+
 	// setup tables
 	
 	// insert general table data
@@ -31,6 +48,6 @@ if (isset($_POST['go']))
 	// setup admin user
 	
 	// redirect to the installed app
-	header("Location: ");
+	echo 'All done, you can now use <a href="'.$_POST['site_url'].'">your website</a>. Please ensure you delete this file.';
 }
 ?>

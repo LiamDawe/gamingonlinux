ce86a1c88d34306ae74cd48d286d4f0f3f35cc47
Liam Dawe <liamdawe@gmail.com>
2020-07-26 12:24:50 +0100

    no need for phpmailer inside minimal bootstrap

--- public_html/includes/class_mailer.php
+++ public_html/includes/class_mailer.php
@@ -1,5 +1,9 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class mailer 
 {
--- public_html/includes/minimal_bootstrap.php
+++ public_html/includes/minimal_bootstrap.php
@@ -1,8 +1,5 @@
 <?php
 require dirname(__FILE__) . "/loader.php";
-require dirname(__FILE__) . '/PHPMailer/src/PHPMailer.php';
-require dirname(__FILE__) . '/PHPMailer/src/Exception.php';
-
 include (dirname(__FILE__) . '/config.php');
 
 $dbl = new db_mysql();

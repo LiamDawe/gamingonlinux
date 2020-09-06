e7706b0baf9e078240cdb20fddfea6156d2823ba
Liam Dawe <liamdawe@gmail.com>
2020-09-06 15:12:57 +0100

    only show games that support linux in the free games list, woops

--- public_html/includes/class_game_sales.php
+++ public_html/includes/class_game_sales.php
@@ -259,18 +259,18 @@ class game_sales
 
 			$merged_arrays = array_merge([$where], $genre_ids, $licenses);
 
-			$total_rows = $this->dbl->run("SELECT COUNT(Distinct id) FROM `calendar` c WHERE $sql_where c.`free_game` = 1 AND c.`also_known_as` IS NULL AND c.`is_application` = 0 AND c.`approved` = 1 AND `is_emulator` = 0 AND `is_dlc` = 0 ORDER BY c.`name` ASC", [$where])->fetchOne();
+			$total_rows = $this->dbl->run("SELECT COUNT(Distinct id) FROM `calendar` c WHERE $sql_where c.`free_game` = 1 AND c.`also_known_as` IS NULL AND c.`is_application` = 0 AND c.`approved` = 1 AND `is_emulator` = 0 AND `is_dlc` = 0 AND `supports_linux` = 1 ORDER BY c.`name` ASC", [$where])->fetchOne();
 			$pagination = $this->core->pagination_link(50, $total_rows, '/free_games.php?', $page, $link_extra);	
 
-			$games_res = $this->dbl->run("SELECT c.`id`, c.`name`, c.`license`, c.`small_picture` FROM `calendar` c $genre_join WHERE $sql_where c.`free_game` = 1 AND c.`also_known_as` IS NULL AND c.`is_application` = 0 AND c.`approved` = 1 AND `is_emulator` = 0 AND `is_dlc` = 0 $options_sql GROUP BY c.`id` ORDER BY c.`name` ASC LIMIT {$this->core->start}, 50", $merged_arrays)->fetch_all();
+			$games_res = $this->dbl->run("SELECT c.`id`, c.`name`, c.`license`, c.`small_picture` FROM `calendar` c $genre_join WHERE $sql_where c.`free_game` = 1 AND c.`also_known_as` IS NULL AND c.`is_application` = 0 AND c.`approved` = 1 AND `is_emulator` = 0 AND `is_dlc` = 0 AND `supports_linux` = 1 $options_sql GROUP BY c.`id` ORDER BY c.`name` ASC LIMIT {$this->core->start}, 50", $merged_arrays)->fetch_all();
 		}
 		else
 		{
 			$merged_arrays = array_merge($genre_ids, $licenses);
-			$total_rows = $this->dbl->run("SELECT COUNT(Distinct c.id) FROM `calendar` c $genre_join WHERE c.`free_game` = 1 AND c.`also_known_as` IS NULL AND c.`is_application` = 0 AND c.`approved` = 1 AND `is_emulator` = 0 AND `is_dlc` = 0 $options_sql  ORDER BY c.`name` ASC", $merged_arrays)->fetchOne();
+			$total_rows = $this->dbl->run("SELECT COUNT(Distinct c.id) FROM `calendar` c $genre_join WHERE c.`free_game` = 1 AND c.`also_known_as` IS NULL AND c.`is_application` = 0 AND c.`approved` = 1 AND `is_emulator` = 0 AND `is_dlc` = 0 AND `supports_linux` = 1 $options_sql  ORDER BY c.`name` ASC", $merged_arrays)->fetchOne();
 			$pagination = $this->core->pagination_link(50, $total_rows, '/free_games.php?', $page, $link_extra);
 
-			$games_res = $this->dbl->run("SELECT c.`id`, c.`name`, c.`license`, c.`small_picture` FROM `calendar` c $genre_join WHERE c.`free_game` = 1 AND c.`also_known_as` IS NULL AND c.`is_application` = 0 AND c.`approved` = 1 AND `is_emulator` = 0 AND `is_dlc` = 0 $options_sql GROUP BY c.`id` ORDER BY c.`name` ASC LIMIT {$this->core->start}, 50", $merged_arrays)->fetch_all();	
+			$games_res = $this->dbl->run("SELECT c.`id`, c.`name`, c.`license`, c.`small_picture` FROM `calendar` c $genre_join WHERE c.`free_game` = 1 AND c.`also_known_as` IS NULL AND c.`is_application` = 0 AND c.`approved` = 1 AND `is_emulator` = 0 AND `is_dlc` = 0 AND `supports_linux` = 1 $options_sql GROUP BY c.`id` ORDER BY c.`name` ASC LIMIT {$this->core->start}, 50", $merged_arrays)->fetch_all();	
 		}
 
 		$this->templating->set('filter_total', $total_rows);

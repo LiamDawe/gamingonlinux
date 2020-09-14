d9be59bcfa11107892c471807bf4e6fde0ab58fa
Liam Dawe <liamdawe@gmail.com>
2020-09-14 14:36:34 +0100

    new files for steamplay section

---
+++ public_html/includes/messages/steamplay_messages.php
@@ -0,0 +1,14 @@
+<?php
+return [
+	"wrong_proton_version" => 
+	[
+		"text"	=> 'The Proton version entered did not match one we have in the database. Please only use what is in the dropdown box.',
+		"error" => 1
+	],
+	"wrong_time_played" =>
+	[
+		"text" => "The time played entered did not match the choices. Please only use what is in the dropdown box.",
+		"error" => 1
+	]
+];
+?>

f521e3a87989241f0561b2ab1ee6f85fc196a298
Liam Dawe <liamdawe@gmail.com>
2020-09-19 11:21:17 +0100

    admin notification load adjustments

--- public_html/includes/ajax/admin/load_notifications.php
+++ public_html/includes/ajax/admin/load_notifications.php
@@ -5,28 +5,35 @@ define("APP_ROOT", dirname( dirname ( dirname ( dirname(__FILE__) ) ) ) );
 
 require APP_ROOT . "/includes/bootstrap.php";
 
-header('Content-Type: application/json');
-
-if(isset($_GET['last_id']) || isset($_GET['last_id']))
+if (isset($_SESSION) && $_SESSION['user_id'] > 0)
 {
-	if ($user->check_group([1,2,5]))
+	header('Content-Type: application/json');
+
+	if(isset($_GET['last_id']) || isset($_GET['last_id']))
 	{
-		$templating->load('admin_modules/admin_home');
-
-		$grab_notes = $notifications->load_admin_notifications($_GET['last_id']);
-		
-		foreach ($grab_notes['rows'] as $tracking)
+		if ($user->check_group([1,2,5]))
 		{
-			$templating->block('tracking_row', 'admin_modules/admin_home');
+			$templating->load('admin_modules/admin_home');
 
-			$templating->set('editor_action', $tracking);
+			$grab_notes = $notifications->load_admin_notifications($_GET['last_id']);
+			
+			foreach ($grab_notes['rows'] as $tracking)
+			{
+				$templating->block('tracking_row', 'admin_modules/admin_home');
+
+				$templating->set('editor_action', $tracking);
+			}
+
+			$last_id = $grab_notes['last_id'];
+
+			// count if there's any left
+			$total_left = $dbl->run("SELECT COUNT(*) FROM `admin_notifications` WHERE `id` < ? ORDER BY `id` DESC", array($last_id))->fetchOne();
+
+			echo json_encode(array("result" => 'done', 'text' => $templating->output(), 'last_id' => $last_id, 'total_left' => $total_left));
 		}
-
-		$last_id = $grab_notes['last_id'];
-
-		// count if there's any left
-		$total_left = $dbl->run("SELECT COUNT(*) FROM `admin_notifications` WHERE `id` < ? ORDER BY `id` DESC", array($last_id))->fetchOne();
-
-		echo json_encode(array("result" => 'done', 'text' => $templating->output(), 'last_id' => $last_id, 'total_left' => $total_left));
 	}
+}
+else
+{
+	die('Authentication needed.');
 }

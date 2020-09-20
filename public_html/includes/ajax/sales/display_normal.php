8f636f8e972bb755b1360ee379f71b023f3be3f7
Liam Dawe <liamdawe@gmail.com>
2020-09-20 09:14:07 +0100

    uses new class naming from bootstrap

--- public_html/includes/ajax/sales/display_normal.php
+++ public_html/includes/ajax/sales/display_normal.php
@@ -5,14 +5,12 @@ define("APP_ROOT", dirname( dirname ( dirname ( dirname(__FILE__) ) ) ) );
 
 require APP_ROOT . "/includes/bootstrap.php";
 
-$game_sales = new game_sales($dbl, $templating, $user, $core);
-
 $filters = NULL;
 if (isset($_GET['filters']))
 {
 	$filters = $_GET['filters'];
 }
 
-$game_sales->display_normal($filters);
+$gamedb->display_normal($filters);
 
 echo $templating->output();

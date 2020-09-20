3be565ed1aecca39bd1f3c5b8ef159d388873ab9
Liam Dawe <liamdawe@gmail.com>
2020-09-20 20:59:14 +0100

    reduce the amount of sql queries the stats page does

--- public_html/modules/statistics.php
+++ public_html/modules/statistics.php
@@ -83,23 +83,23 @@ if (!isset($_GET['view']) || isset($_GET['view']) && $_GET['view'] == 'monthly')
 		$grouping_id = $dbl->run("SELECT `grouping_id` FROM user_stats_grouping ORDER BY `grouping_id` DESC LIMIT 1")->fetchOne();
 	}
 
-	foreach($charts_list as $chart)
+	$get_charts_info = $dbl->run("SELECT `name`, `id`, `grouping_id`, `total_answers` FROM `user_stats_charts` WHERE `grouping_id` = ? ORDER BY `id` ASC", array($grouping_id))->fetch_all(PDO::FETCH_GROUP|PDO::FETCH_UNIQUE|PDO::FETCH_ASSOC);
+
+	foreach($get_charts_info as $name => $chart)
 	{
-		$get_chart_id = $dbl->run("SELECT `id`, `grouping_id`,`name`, `h_label`, `total_answers` FROM `user_stats_charts` WHERE `name` = ? AND `grouping_id` = ? ORDER BY `id` DESC LIMIT 1", array($chart['name'], $grouping_id))->fetch();
-
-		if ($get_chart_id['total_answers'] > 0)
+		if ($chart['total_answers'] > 0)
 		{
-			$previous_group = $dbl->run("SELECT `grouping_id` FROM `user_stats_charts` WHERE `grouping_id` < ? ORDER BY `id` DESC LIMIT 1", array($get_chart_id['grouping_id']))->fetch();
+			$previous_group = $dbl->run("SELECT `grouping_id` FROM `user_stats_charts` WHERE `grouping_id` < ? ORDER BY `id` DESC LIMIT 1", array($chart['grouping_id']))->fetch();
 
-			$get_last_chart_id = $dbl->run("SELECT `id` FROM `user_stats_charts` WHERE `name` = ? AND `grouping_id` = ? ORDER BY `id` DESC LIMIT 1", array($chart['name'], $previous_group['grouping_id']))->fetchOne();
+			$get_last_chart_id = $dbl->run("SELECT `id` FROM `user_stats_charts` WHERE `name` = ? AND `grouping_id` = ? ORDER BY `id` DESC LIMIT 1", array($name, $previous_group['grouping_id']))->fetchOne();
 
 			$charts = new charts($dbl);
 
 			$options = ['padding_right' => 70, 'show_top_10' => 1, 'bundle_outside_top10' => $chart['bundle_outside_top10']];
 
-			if (isset($get_chart_id['id']))
+			if (isset($chart['id']))
 			{
-				$grab_chart = $charts->stat_chart($get_chart_id['id'], $get_last_chart_id, $options);
+				$grab_chart = $charts->stat_chart($chart['id'], $get_last_chart_id, $options);
 
 				// only do this once
 				if ($counter == 0)
@@ -109,15 +109,15 @@ if (!isset($_GET['view']) || isset($_GET['view']) && $_GET['view'] == 'monthly')
 				}
 
 				$templating->block('chart_section', 'statistics');
-				$chart_id_link = str_replace(' ', '', $chart['name']); // Replaces all spaces with hyphens.
+				$chart_id_link = str_replace(' ', '', $name); // Replaces all spaces with hyphens.
 				$chart_id_link = preg_replace('/[^A-Za-z0-9\-]/', '', $chart_id_link); // Removes special chars.
 				$templating->set('title_id', $chart_id_link); 
-				$templating->set('title', $chart['name']);
+				$templating->set('title', $name);
 				$templating->set('graph', $grab_chart['graph']);
 				$download_link = '';
 				if (isset($_SESSION['user_id']) && $_SESSION['user_id'] > 0)
 				{
-					$download_link = '<div style="text-align: center;"><em>Download Graph: (<a href="/render_chart.php?id='.$get_chart_id['id'].'&type=stats&download">SVG</a>)</em> | <a href="/render_chart.php?id='.$get_chart_id['id'].'&type=stats">Graph Link</a></div>';
+					$download_link = '<div style="text-align: center;"><em>Download Graph: (<a href="/render_chart.php?id='.$chart['id'].'&type=stats&download">SVG</a>)</em> | <a href="/render_chart.php?id='.$chart['id'].'&type=stats">Graph Link</a></div>';
 				}
 				$templating->set('download_link', $download_link);
 				$templating->set('total_users', $grab_chart['total_users_answered']);

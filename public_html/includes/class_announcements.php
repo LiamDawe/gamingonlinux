4adb8fc9daa16663325011b4531686f6449b016e
Liam Dawe <liamdawe@gmail.com>
2020-09-22 13:16:54 +0100

    remove pc info check function from announcements, no runs on cron

--- public_html/includes/class_announcements.php
+++ public_html/includes/class_announcements.php
@@ -19,15 +19,6 @@ class announcements
 			$this->core->set_dbcache('index_announcements', serialize($get_announcements));  // no need for a cache, don't update often
 		}
 
-		if (isset($_SESSION) && isset($_SESSION['user_id']))
-		{
-			$pcinfo_announce = $this->check_old_pc_info($_SESSION['user_id']);
-			if (!empty($pcinfo_announce))
-			{
-				$get_announcements = array_merge($pcinfo_announce, $get_announcements);
-			}
-		}
-
 		if ($get_announcements)
 		{
 			$random_item = array_rand($get_announcements, 1);
@@ -100,31 +91,6 @@ class announcements
 		}
 
 		return $announcement_return;
-	}
-
-	function check_old_pc_info($user_id)
-	{
-		$announcements = [];
-		if (isset($user_id) && $user_id != 0)
-		{
-			$checker = $this->dbl->run("SELECT `date_updated` FROM `user_profile_info` WHERE `user_id` = ?", array($user_id))->fetch();
-
-			if ($checker && $checker['date_updated'] != NULL)
-			{
-				$minus_4months = strtotime('-4 months');
-
-				if (strtotime($checker['date_updated']) < $minus_4months)
-				{
-					if (!isset($_COOKIE['gol_announce_pc_info']))
-					{
-						$announcements['pc_info']['text'] = 'You haven\'t updated your PC information in over 4 months! <a href="/usercp.php?module=pcinfo">Click here to go and check</a>. You can simply update if nothing has changed to be included in our statistics!';
-						$announcements['pc_info']['can_dismiss'] = 1;
-						$announcements['pc_info']['id'] = 'pc_info';
-					}
-				}
-			}
-		}
-		return $announcements;
 	}
 }
 ?>

505d3cf4aa4a1ed246bfcc5b56902c0dd9bc2bda
Liam Dawe <liamdawe@gmail.com>
2020-09-25 21:31:47 +0100

    top articles listed in footer now on home page

--- public_html/includes/footer.php
+++ public_html/includes/footer.php
@@ -4,6 +4,33 @@ if(!defined('golapp'))
 	die('Direct access not permitted: footer.');
 }
 $templating->load('footer');
+
+// random hot articles
+if (!isset(core::$current_module) || isset(core::$current_module) && (core::$current_module['module_file_name'] == 'home'))
+{
+	/*
+	// top articles this month but not from the most recent 2 days to prevent showing what they've just seen on the home page
+	*/
+	$blocked_tags  = str_repeat('?,', count($user->blocked_tags) - 1) . '?';
+	$top_article_query = "SELECT a.`article_id`, a.`title`, a.`slug`, a.`date`, a.`tagline_image`, a.`gallery_tagline`, t.`filename` as gallery_tagline_filename FROM `articles` a LEFT JOIN `articles_tagline_gallery` t ON t.`id` = a.`gallery_tagline` WHERE a.`date` > UNIX_TIMESTAMP(NOW() - INTERVAL 1 MONTH) AND a.`date` < UNIX_TIMESTAMP(NOW() - INTERVAL 2 DAY) AND a.`views` > ? AND a.`show_in_menu` = 0 AND NOT EXISTS (SELECT 1 FROM article_category_reference c  WHERE a.article_id = c.article_id AND c.`category_id` IN ( $blocked_tags )) ORDER BY RAND() DESC LIMIT 6";
+
+	$fetch_top3 = $dbl->run($top_article_query, array_merge([$core->config('hot-article-viewcount')], $user->blocked_tags))->fetch_all();
+
+	if (is_array($fetch_top3))
+	{
+		$templating->block('random_top_articles', 'footer');
+		$random_list = '';
+		foreach ($fetch_top3 as $top_articles)
+		{
+			$tagline_image = $article_class->tagline_image($top_articles, 142, 250);
+
+			$random_list .= '<div class="footer-article-flex-col-item"><div>'.$tagline_image.'</div><div><a href="'.$article_class->article_link(array('date' => $top_articles['date'], 'slug' => $top_articles['slug'])).'">'.$top_articles['title'].'</a></div></div>';
+		}
+
+		$templating->set('random_list', $random_list);
+	}
+}
+
 $templating->block('footer');
 $templating->set('jsstatic', JSSTATIC);
 $templating->set('url', url);

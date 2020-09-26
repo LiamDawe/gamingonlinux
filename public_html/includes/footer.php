9deec037a64db0b2285d9b540601fc6402378146
Liam Dawe <liamdawe@gmail.com>
2020-09-26 16:41:35 +0100

    add latest OS/distro news to footer

--- public_html/includes/footer.php
+++ public_html/includes/footer.php
@@ -14,13 +14,36 @@ if (!isset(core::$current_module) || isset(core::$current_module) && (core::$cur
 	$blocked_tags  = str_repeat('?,', count($user->blocked_tags) - 1) . '?';
 	$top_article_query = "SELECT a.`article_id`, a.`title`, a.`slug`, a.`date`, a.`tagline_image`, a.`gallery_tagline`, t.`filename` as gallery_tagline_filename FROM `articles` a LEFT JOIN `articles_tagline_gallery` t ON t.`id` = a.`gallery_tagline` WHERE a.`date` > UNIX_TIMESTAMP(NOW() - INTERVAL 1 MONTH) AND a.`date` < UNIX_TIMESTAMP(NOW() - INTERVAL 2 DAY) AND a.`views` > ? AND a.`show_in_menu` = 0 AND NOT EXISTS (SELECT 1 FROM article_category_reference c  WHERE a.article_id = c.article_id AND c.`category_id` IN ( $blocked_tags )) ORDER BY RAND() DESC LIMIT 6";
 
-	$fetch_top3 = $dbl->run($top_article_query, array_merge([$core->config('hot-article-viewcount')], $user->blocked_tags))->fetch_all();
+	$fetch_top = $dbl->run($top_article_query, array_merge([$core->config('hot-article-viewcount')], $user->blocked_tags))->fetch_all();
 
-	if (is_array($fetch_top3))
+	if (is_array($fetch_top))
 	{
 		$templating->block('random_top_articles', 'footer');
 		$random_list = '';
-		foreach ($fetch_top3 as $top_articles)
+		foreach ($fetch_top as $top_articles)
+		{
+			$tagline_image = $article_class->tagline_image($top_articles, 142, 250);
+
+			$random_list .= '<div class="footer-article-flex-col-item"><div>'.$tagline_image.'</div><div><a href="'.$article_class->article_link(array('date' => $top_articles['date'], 'slug' => $top_articles['slug'])).'">'.$top_articles['title'].'</a></div></div>';
+		}
+
+		$templating->set('random_list', $random_list);
+	}
+}
+
+// latest open source and linux distro news
+if (!isset(core::$current_module) || isset(core::$current_module) && (core::$current_module['module_file_name'] == 'home'))
+{
+	$blocked_tags  = str_repeat('?,', count($user->blocked_tags) - 1) . '?';
+	$top_article_query = "SELECT a.`article_id`, a.`title`, a.`slug`, a.`date`, a.`tagline_image`, a.`gallery_tagline`, t.`filename` as gallery_tagline_filename FROM `article_category_reference` r JOIN `articles` a ON a.`article_id` = r.`article_id` LEFT JOIN `articles_tagline_gallery` t ON t.`id` = a.`gallery_tagline` WHERE NOT EXISTS (SELECT 1 FROM article_category_reference c  WHERE a.article_id = c.article_id AND c.`category_id` IN ( $blocked_tags )) AND r.category_id IN (11,192,170) ORDER BY a.`date` DESC LIMIT 6";
+
+	$fetch_top_os = $dbl->run($top_article_query, array_merge($user->blocked_tags))->fetch_all();
+
+	if (is_array($fetch_top_os))
+	{
+		$templating->block('recent_open_source', 'footer');
+		$random_list = '';
+		foreach ($fetch_top_os as $top_articles)
 		{
 			$tagline_image = $article_class->tagline_image($top_articles, 142, 250);
 

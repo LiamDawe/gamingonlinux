bca2b84c76952f72192cfc39aba7ad5a3633cf39
Liam Dawe <liamdawe@gmail.com>
2020-10-01 23:34:25 +0100

    podcast rss feeds now use one single file

--- public_html/podcast_rss.php
+++ public_html/podcast_rss.php
@@ -13,13 +13,19 @@ $bbcode = new bbcode($dbl, $core, $user);
 header("Content-Type: application/rss+xml");
 header("Cache-Control: no-cache, must-revalidate");
 
+$format = '?format=mp3';
+if (isset($_GET['format']) && $_GET['format'] == 'ogg')
+{
+	$format = '?format=ogg';
+}
+
 $output = '<?xml version="1.0" encoding="UTF-8"?>
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom" xml:lang="en-GB">
 
 <channel>
 <title>Gaming On Linux Podcasts</title>
 <link>http://www.gamingonlinux.com/</link>
-<atom:link href="http://www.gamingonlinux.com/podcast_rss_mp3.php" rel="self" type="application/rss+xml"/>
+<atom:link href="http://www.gamingonlinux.com/podcast_rss.php'.$format.'" rel="self" type="application/rss+xml"/>
 <language>en</language>
 <description>Linux gaming chat and general banter</description>
 <image>
@@ -37,7 +43,22 @@ $output = '<?xml version="1.0" encoding="UTF-8"?>
 <itunes:category text="Technology">
 </itunes:category>';
 
-$articles = $dbl->run("SELECT a.* FROM `articles` a LEFT JOIN `article_category_reference` c ON c.article_id = a.article_id WHERE a.`active` = 1 AND c.`category_id` = 97 ORDER BY a.`date` DESC LIMIT 30")->fetch_all();
+$articles = $dbl->run("SELECT a.`date`, a.`title`, a.`slug`, a.`tagline`, a.`text` FROM `articles` a LEFT JOIN `article_category_reference` c ON c.article_id = a.article_id WHERE a.`active` = 1 AND c.`category_id` = 97 ORDER BY a.`date` DESC LIMIT 30")->fetch_all();
+
+function retrieve_remote_file_size($url){
+	$ch = curl_init($url);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_HEADER, TRUE);
+	curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+	$data = curl_exec($ch);
+	$size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+
+	curl_close($ch);
+	return $size;
+}
 
 foreach ($articles as $line)
 {
@@ -49,10 +70,19 @@ foreach ($articles as $line)
 	$title = str_replace("&", "&amp;", $title);
 	$title = $title;
 
-	preg_match("/<a href=\"(.+\.mp3)/m", $line['text'], $matches);
+	if ($format == '?format=mp3')
+	{
+		preg_match("/<a href=\"(.+\.mp3)/m", $line['text'], $matches);
+	}
+	else if ($format == '?format=ogg')
+	{
+		preg_match("/<a href=\"(.+\.ogg)/m", $line['text'], $matches);
+	}
 
-	$size = filesize($file_dir . str_replace("https://www.gamingonlinux.com",'',$matches[1]));
 	$item_url = str_replace("https",'http',$matches[1]);
+	$size = retrieve_remote_file_size($item_url);
+
+	$article_link = $core->config('website_url') . date('Y', $line['date']) . '/' . date('m', $line['date']) . '/' . $line['slug'];
 
 	if (isset($matches[1]))
 	{
@@ -62,7 +92,7 @@ foreach ($articles as $line)
 		<description>{$line['tagline']}</description>
 		<enclosure url=\"$item_url\" type=\"audio/mpeg\" length=\"$size\" />
 		<pubDate>$date</pubDate>
-		<guid>http://www.gamingonlinux.com/articles/$nice_title.{$line['article_id']}/</guid>
+		<guid>".$article_link."</guid>
 		</item>";
 	}
 }

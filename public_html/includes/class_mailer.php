8015a0acc96626e93c4c9e70d8a6b1e306d79d2c
Liam Dawe <liamdawe@gmail.com>
2020-10-18 13:29:48 +0100

    overhaul html email with some style, also fixes broken logo with new dedicated email logo

--- public_html/includes/class_mailer.php
+++ public_html/includes/class_mailer.php
@@ -24,23 +24,39 @@ class mailer
     public function sendMail($to, $subject, $body, $plainText = NULL, $reply_to = NULL)
 	{
 		// add in logo
-		$body = '<img src="' . $this->core->config('static_image_url') . 'logos/' . 'icon.svg" width="35" height="35" alt="' . $this->core->config('site_title') . '"><br />' . $body;
+		$body = '<head><style>
+		body {background-color: #313131;
+			color: #eeeeee;}
+		table {background-color: #313131;
+			color: #eeeeee; padding: 5px;}
+		td {padding: 5px;}
+		a, a:link, a:active, a:visited, a[href] { color: #428BCA !important; pointer-events: auto; cursor: default;}
+		.center {
+			display: block;
+			margin-left: auto;
+			margin-right: auto;
+		  }
+		  </style></head><body id="body" bgcolor="#313131"><table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#313131">
+		  <tr>
+			<td bgcolor="#313131">
+			<img src="' . $this->core->config('static_image_url') . 'logos/' . 'email_logo.png" width="450" height="250" alt="' . $this->core->config('site_title') . '" class="center">
+			</td>
+		  </tr>
+		  <tr><td>
+		<br />' . $body;
 
 		// add in footer
-		$body = $body . '<div>
+		$body = $body . '</td></tr>
+		<tr><td><div>
 		<hr>
 		<p><em>If you think you shouldn\'t be getting this email, please forward this email to '. $this->core->config('contact_email') .' so that we can take action.</em></p>
 		<p>You can find us on: <a href="https://twitter.com/gamingonlinux">Twitter</a>, <a href="https://mastodon.social/@gamingonlinux">Mastodon</a>, <a href="https://t.me/linuxgaming">Telegram</a>, <a href="https://www.twitch.tv/gamingonlinux">Twitch</a>, <a href="https://discord.gg/0rxBtcSOonvGzXr4">Discord</a>, <a href="https://www.gamingonlinux.com/irc/">IRC</a> and <a href="https://www.youtube.com/gamingonlinux">YouTube</a>.</p>
-		</div>';
+		</div></tr></td></table></body>';
 
         $this->mail->addAddress($to);
 		if ($reply_to != NULL)
 		{
 			$this->mail->addReplyTo($reply_to['email'], $reply_to['name']);
-		}
-		else
-		{
-			$this->mail->addReplyTo($this->core->config('contact_email'), $this->core->config('site_title') . ' Admin');
 		}
         $this->mail->Subject = $subject;
         $this->mail->Body = $body;

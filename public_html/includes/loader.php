22677263ea49339ebe1cc4c1eadd71ee21f4e0f6
Liam Dawe <liamdawe@gmail.com>
2020-10-25 16:33:09 +0000

    uncomplicate the auto loader, we don't use any of this

--- public_html/includes/loader.php
+++ public_html/includes/loader.php
@@ -3,30 +3,12 @@
 * Attempt to automatically load a class based on name
 * @param string $classname The name of class to load
 **/
-function loadClass($classname)
+spl_autoload_register(function ($class)
 {
-	$basePath = realpath(dirname(__FILE__));
-	$incDir = $basePath .  DIRECTORY_SEPARATOR;
-	
-	if (strpos($classname, '\\') !== false)
-	{
-		if (file_exists( $incDir . str_replace("\\", DIRECTORY_SEPARATOR, $classname) . ".php" )) 
-		{
-			include $incDir . str_replace("\\", DIRECTORY_SEPARATOR, $classname) . ".php";
-			return true;
-		}
-	}
-	$modelDir = $incDir .  "models" . DIRECTORY_SEPARATOR;
-	if (file_exists( $incDir . "class_" . $classname . ".php" ))
-	{
-		include $incDir . "class_" . $classname . ".php";
-		return true;
-	} 
-	else if (file_exists( $modelDir . "model_" . $classname . ".php" )) 
-	{
-		include $modelDir . "model_" . $classname . ".php";
-		return true;
-	}
-}
+	$base_dir = realpath(dirname(__FILE__));
+	$file = $base_dir . DIRECTORY_SEPARATOR . 'class_' . str_replace('\\', '/', $class) . '.php';
 
-spl_autoload_register("loadClass");
+    if (file_exists($file)) {
+        require $file;
+    }
+});
